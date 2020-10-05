53fcc5234fbc74ee428c281c3892cd0b44e3210d
VisualAcademy <redplus@redplus.net>
2020-10-05 08:20:12 +0000

    UseStaticFiles

--- DotNetNote/DotNetNote/Startup.cs
+++ DotNetNote/DotNetNote/Startup.cs
@@ -1,12 +1,8 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DotNetNote
 {
@@ -16,6 +12,7 @@ namespace DotNetNote
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,13 +23,18 @@ namespace DotNetNote
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles(); 
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    //await context.Response.WriteAsync("Hello World!");
+                    // 한글 출력
+                    context.Response.Headers["Content-Type"] = "text/html; charset=utf-8";
+                    await context.Response.WriteAsync("안녕하세요.", System.Text.Encoding.UTF8);
                 });
             });
         }
