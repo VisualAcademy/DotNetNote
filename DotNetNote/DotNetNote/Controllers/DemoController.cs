f69f1f0857e3b0ef47c271bd030e087f406c4a2e
VisualAcademy <redplus@redplus.net>
2020-10-05 08:46:20 +0000

    Model, View, Controller

---
+++ DotNetNote/DotNetNote/Controllers/DemoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return Content("Demo Page");
+            return View();
+        }
+    }
+}
--- DotNetNote/DotNetNote/Startup.cs
+++ DotNetNote/DotNetNote/Startup.cs
@@ -12,7 +12,7 @@ namespace DotNetNote
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddControllersWithViews(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,6 +29,8 @@ namespace DotNetNote
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapDefaultControllerRoute(); 
+
                 endpoints.MapGet("/", async context =>
                 {
                     //await context.Response.WriteAsync("Hello World!");
