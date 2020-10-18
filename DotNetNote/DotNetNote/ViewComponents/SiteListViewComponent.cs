349fb6c32c5422a59c328f8f5b1cc15303c7cf2e
RedPlus <redplus@redplus.net>
2020-10-18 20:26:29 +0900

    Carousel

--- DotNetNote/DotNetNote/ViewComponents/SiteListViewComponent.cs
+++ DotNetNote/DotNetNote/ViewComponents/SiteListViewComponent.cs
@@ -11,7 +11,7 @@ namespace DotNetNote.ViewComponents
             var siteLists = new List<Site>() {
                 new Site { Id = 1, Title = "길벗출판사",
                     Url = "http://www.gilbut.co.kr",
-                    Description = "ASP.NET Core 서적 출간" },
+                    Description = "C# 교과서 및 ASP.NET Core 다루는 기술 책 출간" },
                 new Site { Id = 2, Title = "데브렉",
                     Url = "http://www.devlec.com",
                     Description = "DotNetNote 사이트 제작 관련 동영상 강의 제공" },
