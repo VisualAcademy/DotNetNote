a4792b2c3482497ceb18bbaa0031e5860aa73e40
RedPlus <redplus@redplus.net>
2021-02-21 21:42:16 +0000

    AddHttpContextAccessor

--- DotNetNote/DotNetNote/Startup.cs
+++ DotNetNote/DotNetNote/Startup.cs
@@ -35,6 +35,9 @@ namespace DotNetNote
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddHttpContextAccessor(); //[1]
+
             services.AddControllersWithViews();
 
             //services.AddDbContext<ApplicationDbContext>(options =>
