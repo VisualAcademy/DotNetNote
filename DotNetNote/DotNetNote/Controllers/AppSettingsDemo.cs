40c34b74eddc34ee9ec15f29cdd67eeb23dce44f
RedPlus <redplus@redplus.net>
2021-03-22 06:14:19 +0900

    appsettings 파일의 데이터를 IConfiguration 개체로 읽어오기 및 secrets 파일로 보안된 설정값 관리하기

---
+++ DotNetNote/DotNetNote/Controllers/AppSettingsDemo.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace DotNetNote.Controllers
+{
+    [Authorize(Roles = "Administrators")]
+    public class AppSettingsDemo : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public AppSettingsDemo(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            string con1 = _configuration.GetSection("StorageConnectionString1").Value;
+
+            string site1 = _configuration
+                .GetSection("BlogStorageConnectionString")
+                .GetSection("Site1").Value;
+
+            string site2 = _configuration
+                .GetValue<string>("BlogStorageConnectionString:Site2");
+
+            return Content($"{con1}, {site1}, {site2}");
+        }
+    }
+}
