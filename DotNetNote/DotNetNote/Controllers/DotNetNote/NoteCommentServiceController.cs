f19f14c1db0302ab632a073b41e7e962f7352dca
RedPlus <redplus@redplus.net>
2021-06-26 12:16:42 +0900

    Code Cleanup

--- DotNetNote/DotNetNote/Controllers/DotNetNote/NoteCommentServiceController.cs
+++ DotNetNote/DotNetNote/Controllers/DotNetNote/NoteCommentServiceController.cs
@@ -14,11 +14,8 @@ namespace DotNetNote.Controllers
             _repository = repository;
         }
 
+        // 최근 댓글 리스트 반환
         [HttpGet]
-        public IEnumerable<NoteComment> Get()
-        {
-            // 최근 댓글 리스트 반환
-            return _repository.GetRecentComments();
-        }
+        public IEnumerable<NoteComment> Get() => _repository.GetRecentComments();
     }
 }
