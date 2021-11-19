7cef3a958c9c17263b60cd3610653008c400fe30
Park YongJune <redplus@redplus.net>
2021-11-19 17:27:22 +0000

    Code Cleanup

--- DotNetNote/DotNetNote/Controllers/VariableTestController.cs
+++ DotNetNote/DotNetNote/Controllers/VariableTestController.cs
@@ -1,29 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DotNetNote.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace DotNetNote.Controllers
+namespace DotNetNote.Controllers;
+
+public class VariableTestController : Controller
 {
-    public class VariableTestController : Controller
+    private readonly IVariableRepository _repository;
+
+    public VariableTestController(IVariableRepository repository)
     {
-        private readonly IVariableRepository _repository;
+        _repository = repository;
+    }
 
-        public VariableTestController(IVariableRepository repository)
-        {
-            _repository = repository;
-        }
-
-        // GET: /<controller>/
-        public IActionResult Index()
-        {
-            var variables = _repository.GetAll();
-            ViewBag.Variables = variables; 
-            return View();
-        }
+    // GET: /<controller>/
+    public IActionResult Index()
+    {
+        var variables = _repository.GetAll();
+        ViewBag.Variables = variables;
+        return View();
     }
 }
--- DotNetNote/DotNetNote/Controllers/ViewComponentDemoController.cs
+++ DotNetNote/DotNetNote/Controllers/ViewComponentDemoController.cs
@@ -1,46 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetNote.Controllers
+namespace DotNetNote.Controllers;
+
+public class ViewComponentDemoController : Controller
 {
-    public class ViewComponentDemoController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        return View();
+    }
 
-        /// <summary>
-        /// CopyrightViewComponent 출력 데모
-        /// </summary>
-        public IActionResult CopyrightDemo()
-        {
-            return View();
-        }
-        
-        /// <summary>
-        /// TechListViewComponent 사용 데모
-        /// </summary>
-        public IActionResult TechListDemo()
-        {
-            return View();
-        }
+    /// <summary>
+    /// CopyrightViewComponent 출력 데모
+    /// </summary>
+    public IActionResult CopyrightDemo()
+    {
+        return View();
+    }
+
+    /// <summary>
+    /// TechListViewComponent 사용 데모
+    /// </summary>
+    public IActionResult TechListDemo()
+    {
+        return View();
+    }
 
 
-        /// <summary>
-        /// SiteListViewComponent 사용 데모
-        /// </summary>
-        public IActionResult SiteListDemo()
-        {
-            return View();
-        }
+    /// <summary>
+    /// SiteListViewComponent 사용 데모
+    /// </summary>
+    public IActionResult SiteListDemo()
+    {
+        return View();
+    }
 
 
-        /// <summary>
-        /// 최근 댓글 리스트 출력 데모
-        /// </summary>
-        public IActionResult RecentlyCommentListDemo()
-        {
-            return View(); 
-        }
+    /// <summary>
+    /// 최근 댓글 리스트 출력 데모
+    /// </summary>
+    public IActionResult RecentlyCommentListDemo()
+    {
+        return View();
     }
 }
