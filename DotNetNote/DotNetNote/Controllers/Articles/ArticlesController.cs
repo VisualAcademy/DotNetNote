f5ac63468c2b0e55bcad1442e4d37f05a8cd9b9b
Park YongJune <redplus@redplus.net>
2021-11-20 02:42:34 +0900

    Code Cleanup

--- DotNetNote/DotNetNote/Controllers/Articles/ArticlesController.cs
+++ DotNetNote/DotNetNote/Controllers/Articles/ArticlesController.cs
@@ -2,23 +2,22 @@ using MemoEngineCore.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 
-namespace DotNetNote.Controllers
+namespace DotNetNote.Controllers;
+
+public class ArticlesController : Controller
 {
-    public class ArticlesController : Controller
+    private readonly IBlogService _service;
+
+    public ArticlesController(IBlogService service)
     {
-        private readonly IBlogService _service;
+        this._service = service;
+    }
 
-        public ArticlesController(IBlogService service)
-        {
-            this._service = service;
-        }
+    // GET: Articles
+    public IActionResult Index()
+    {
+        var posts = _service.GetPosts().ToList();
 
-        // GET: Articles
-        public IActionResult Index()
-        {
-            var posts = _service.GetPosts().ToList();
-
-            return View(posts);
-        }
+        return View(posts);
     }
 }
