8f81a4a1e5be8178798bfc72e069ea43b2253af4
Park YongJune <redplus@redplus.net>
2021-11-19 17:31:06 +0000

    Code Cleanup

--- DotNetNote/DotNetNote/Controllers/TabManagerController.cs
+++ DotNetNote/DotNetNote/Controllers/TabManagerController.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetNote.Controllers
+namespace DotNetNote.Controllers;
+
+public class TabManagerController : Controller
 {
-    public class TabManagerController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        return View();
     }
 }
