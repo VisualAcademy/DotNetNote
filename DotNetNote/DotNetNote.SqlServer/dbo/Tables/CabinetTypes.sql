60bee37dff32e34c6322e5d48577e9dd413871f0
Park YongJune <redplus@redplus.net>
2022-02-22 15:16:08 +0900

    .

---
+++ DotNetNote/DotNetNote.SqlServer/dbo/Tables/CabinetTypes.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[CabinetTypes] (
+    [Id]             BIGINT         IDENTITY (1, 1) NOT NULL Primary Key,
+    [Identification] NVARCHAR (MAX) NULL,
+    [Show]           BIT            NULL,
+    [Adjusted]       BIT            NOT NULL
+);
