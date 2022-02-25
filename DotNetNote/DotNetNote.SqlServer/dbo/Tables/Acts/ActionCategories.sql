694e6ddcf0b9c2b56cc32bebc76259f192bdf2df
Park YongJune <redplus@redplus.net>
2022-02-25 14:55:25 +0900

    .

---
+++ DotNetNote/DotNetNote.SqlServer/dbo/Tables/Acts/ActionCategories.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ActionCategories] (
+    [ID]         BIGINT         IDENTITY (1, 1) NOT NULL,
+    [gbAdjusted] BIT            CONSTRAINT [DF_ActionCategories_gbAdjusted] DEFAULT ((0)) NOT NULL,
+    [Category]   NVARCHAR (255) NULL,
+    [Active]     BIT            NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
