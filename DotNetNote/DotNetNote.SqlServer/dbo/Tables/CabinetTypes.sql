270a46eaf012553a8085052e8d2d04aaeddd1acd
Park YongJune <redplus@redplus.net>
2022-03-02 23:01:00 +0900

    .

--- DotNetNote/DotNetNote.SqlServer/dbo/Tables/CabinetTypes.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[CabinetTypes] (
-    [Id]             BIGINT         IDENTITY (1, 1) NOT NULL Primary Key,
-    [Identification] NVARCHAR (MAX) NULL,
-    [Show]           BIT            NULL,
-    [Adjusted]       BIT            NOT NULL
-);
---
+++ DotNetNote/DotNetNote.SqlServer/dbo/Tables/CabinetTypes/CabinetTypes.sql
@@ -0,0 +1,7 @@
+﻿-- 캐비넷 타입 관리용 테이블 
+CREATE TABLE [dbo].[CabinetTypes] (
+    [Id]             BIGINT         IDENTITY (1, 1) NOT NULL Primary Key,
+    [Identification] NVARCHAR (MAX) NULL, -- 이름
+    [Show]           BIT            NULL, -- 표시 여부 
+    [Adjusted]       BIT            NOT NULL -- 백업 여부 
+);
