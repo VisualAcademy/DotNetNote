0a6a8fbd149be349fb8685ba8819d6a47e29c9aa
Park YongJune <redplus@redplus.net>
2022-03-02 23:03:20 +0900

    .

--- DotNetNote/DotNetNote/Models/CabinetTypes/CabinetType.cs
+++ DotNetNote/DotNetNote/Models/CabinetTypes/CabinetType.cs
@@ -1,10 +1,16 @@
 ﻿namespace DotNetNote.Models
 {
+    /// <summary>
+    /// 캐비넷 타입 관리 모델 클래스 
+    /// </summary>
     public class CabinetType
     {
         public long Id { get; set; }
+
         public string? Identification { get; set; }
+
         public bool? Show { get; set; }
+
         public bool Adjusted { get; set; } = false;
     }
 }
