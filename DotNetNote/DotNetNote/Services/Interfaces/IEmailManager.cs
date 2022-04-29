4c9b22ed1057760a20dc7a4516571a4fdfc56f3c
Park YongJune <redplus@redplus.net>
2022-04-30 05:09:29 +0900

    .

--- DotNetNote/DotNetNote/Services/Interfaces/IEmailManager.cs
+++ DotNetNote/DotNetNote/Services/Interfaces/IEmailManager.cs
@@ -2,6 +2,8 @@
 
 namespace DotNetNoteCore.Services
 {
+    // Abstractions: Interfaces => IEmailSender
+    // Implementations: Classes => EmailSender, SendGridEmailSender, ...
     public interface IEmailManager
     {
         Task SendEmailAsync(string email, string subject, string message, string recipient = "", string callbackUrl = "");
