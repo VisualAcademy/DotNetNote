49fe34668023dfac19e93ad63001b5c8e4c2721d
Park YongJune <redplus@redplus.net>
2022-05-07 18:30:54 +0900

    .

--- DotNetNote/DotNetNote/Tests/LoginController.cs
+++ DotNetNote/DotNetNote/Tests/LoginController.cs
@@ -1,28 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetNote
+namespace DotNetNote.Tests;
+
+[Authorize(Roles = "Administrators")]
+public class LoginController : Controller
 {
-    public class LoginController : Controller
+    public LoginController()
     {
-        public LoginController()
-        {
-        }
+    }
 
-        public string Name { get; set; }
-        public string Password { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
 
-        public IActionResult Login()
-        {
-            ViewData["Title"] = "Login Page";
-            return View(); 
-        }
+    public IActionResult Login()
+    {
+        ViewData["Title"] = "Login Page";
+        return View();
+    }
 
-        public IActionResult FormSubmit(string name, string password)
-        {
-            // TODO: Authenticate user.
-            ViewData["Name"] = name;
-            ViewData["Title"] = "Welcome";
-            return View("Welcome");
-        }
+    public IActionResult FormSubmit(string name, string password)
+    {
+        // TODO: Authenticate user.
+        ViewData["Name"] = name;
+        ViewData["Title"] = "Welcome";
+        return View("Welcome");
     }
 }
