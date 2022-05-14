5f1b237f565fdc0b7c5dbecf57c7b4b2196dbc0e
Park YongJune <redplus@redplus.net>
2022-05-14 21:21:00 +0900

    .

--- DotNetNote/DotNetNote/Controllers/Users/AdminController.cs
+++ DotNetNote/DotNetNote/Controllers/Users/AdminController.cs
@@ -1,38 +1,37 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetNote.Controllers
+namespace DotNetNote.Controllers;
+
+[Authorize("Administrators")]
+public class AdminController : Controller
 {
-    [Authorize("Administrators")]
-    public class AdminController : Controller
+    //[Authorize(Roles = "Users")]
+    //[Authorize("Users")] // Startup.cs AddPolicy() 설정 필요
+    //[Authorize("Administrators")]
+    //public string Index()
+    //{
+    //    return "Admin 사용자만 볼 수 있음";
+    //}
+
+    public IActionResult Index()
     {
-        //[Authorize(Roles = "Users")]
-        //[Authorize("Users")] // Startup.cs AddPolicy() 설정 필요
-        //[Authorize("Administrators")]
-        //public string Index()
-        //{
-        //    return "Admin 사용자만 볼 수 있음";
-        //}
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    /// <summary>
+    /// TODO: 게시판 관리자 페이지
+    /// </summary>
+    public IActionResult NoteManager()
+    {
+        return View();
+    }
 
-        /// <summary>
-        /// TODO: 게시판 관리자 페이지
-        /// </summary>
-        public IActionResult NoteManager()
-        {
-            return View();
-        }
-
-        /// <summary>
-        /// TODO: 사용자 관리자 페이지
-        /// </summary>
-        public IActionResult UserManager()
-        {
-            return View();
-        }
+    /// <summary>
+    /// TODO: 사용자 관리자 페이지
+    /// </summary>
+    public IActionResult UserManager()
+    {
+        return View();
     }
 }
