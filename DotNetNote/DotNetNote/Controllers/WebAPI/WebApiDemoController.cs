56698c36fee8f78b1f55e6fdcfeaf1f0942dc371
Park YongJune <redplus@redplus.net>
2022-05-16 00:03:08 +0900

    .

--- DotNetNote/DotNetNote/Controllers/WebAPI/WebApiDemoController.cs
+++ DotNetNote/DotNetNote/Controllers/WebAPI/WebApiDemoController.cs
@@ -2,35 +2,34 @@
 using System.ComponentModel.DataAnnotations;
 using System.Net;
 
-namespace DotNetNote.Controllers
+namespace DotNetNote.Controllers;
+
+[Route("api/[controller]")]
+public class WebApiDemoController : Controller
 {
-    [Route("api/[controller]")]
-    public class WebApiDemoController : Controller
+    [HttpGet]
+    public JsonResult Get()
     {
-        [HttpGet]
-        public JsonResult Get()
-        {
-            return Json(new { Name = "박용준" });
-        }
-
-        [HttpPost]
-        public JsonResult Post([FromBody]WebApiDemoClass name)
-        {
-            if (ModelState.IsValid)
-            {
-                Response.StatusCode = (int)HttpStatusCode.Created;
-                return Json(true);
-            }
-            Response.StatusCode = (int)HttpStatusCode.BadRequest;
-            return Json("실패");
-        }
+        return Json(new { Name = "박용준" });
     }
 
-    public class WebApiDemoClass
+    [HttpPost]
+    public JsonResult Post([FromBody]WebApiDemoClass name)
     {
-        public int Id { get; set; }
-        [Required]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "3자 이상")]
-        public string Name { get; set; }
+        if (ModelState.IsValid)
+        {
+            Response.StatusCode = (int)HttpStatusCode.Created;
+            return Json(true);
+        }
+        Response.StatusCode = (int)HttpStatusCode.BadRequest;
+        return Json("실패");
     }
+}
+
+public class WebApiDemoClass
+{
+    public int Id { get; set; }
+    [Required]
+    [StringLength(50, MinimumLength = 3, ErrorMessage = "3자 이상")]
+    public string Name { get; set; }
 }
