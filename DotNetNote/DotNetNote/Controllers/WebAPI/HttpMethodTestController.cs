56807f0e5776cbe4871721aaca6e560cadf3a542
Park YongJune <redplus@redplus.net>
2022-05-15 23:59:40 +0900

    .

--- DotNetNote/DotNetNote/Controllers/WebAPI/HttpMethodTestController.cs
+++ DotNetNote/DotNetNote/Controllers/WebAPI/HttpMethodTestController.cs
@@ -1,44 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetNote.Controllers
+namespace DotNetNote.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HttpMethodTestController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class HttpMethodTestController : ControllerBase
+    // GET: api/<HttpMethodTestController>
+    [HttpGet]
+    public IEnumerable<string> Get()
     {
-        // GET: api/<HttpMethodTestController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        return new string[] { "value1", "value2" };
+    }
 
-        // GET api/<HttpMethodTestController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return $"value {id}";
-        }
+    // GET api/<HttpMethodTestController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return $"value {id}";
+    }
 
-        // POST api/<HttpMethodTestController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
+    // POST api/<HttpMethodTestController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
 
-        }
+    }
 
-        // PUT api/<HttpMethodTestController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
+    // PUT api/<HttpMethodTestController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
 
-        }
+    }
 
-        // DELETE api/<HttpMethodTestController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+    // DELETE api/<HttpMethodTestController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
 
-        }
     }
 }
