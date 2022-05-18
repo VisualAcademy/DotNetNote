447864de9dbfb92875e8a0027dbefbbcc1c7f34a
Park YongJune <redplus@redplus.net>
2022-05-19 02:08:14 +0900

    file-scoped namespace

--- DotNetNote/DotNetNote/Controllers/CompaniesController.cs
+++ DotNetNote/DotNetNote/Controllers/CompaniesController.cs
@@ -1,43 +1,42 @@
 ﻿using DotNetNote.Models.Companies;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetNote.Controllers
+namespace DotNetNote.Controllers;
+
+public class CompaniesController : Controller
 {
-    public class CompaniesController : Controller
+    private ICompanyRepository _repository;
+
+    public CompaniesController(ICompanyRepository repository)
     {
-        private ICompanyRepository _repository;
+        _repository = repository;
+    }
 
-        public CompaniesController(ICompanyRepository repository)
-        {
-            _repository = repository;
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    [HttpPost]
+    public IActionResult Index(string name)
+    {
+        ViewBag.Message = $"{name}을 입력했습니다.";
 
-        [HttpPost]
-        public IActionResult Index(string name)
-        {
-            ViewBag.Message = $"{name}을 입력했습니다.";
+        return View();
+    }
 
-            return View();
-        }
+    [HttpGet]
+    public IActionResult Manage()
+    {
+        var companies = _repository.Read();
 
-        [HttpGet]
-        public IActionResult Manage()
-        {
-            var companies = _repository.Read();
+        return View(companies);
+    }
 
-            return View(companies);
-        }
-
-        [HttpPost]
-        public IActionResult Manage(string name)
-        {
-            _repository.Add(new CompanyModel() { Name = name });
-            return RedirectToAction(nameof(Manage));
-        }
+    [HttpPost]
+    public IActionResult Manage(string name)
+    {
+        _repository.Add(new CompanyModel() { Name = name });
+        return RedirectToAction(nameof(Manage));
     }
 }
