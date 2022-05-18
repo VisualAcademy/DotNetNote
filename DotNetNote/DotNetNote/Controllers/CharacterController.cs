9fa629758f98ba5c48cf876de75195b9b23eb182
Park YongJune <redplus@redplus.net>
2022-05-18 14:00:33 +0900

    .

--- DotNetNote/DotNetNote/Controllers/CharacterController.cs
+++ DotNetNote/DotNetNote/Controllers/CharacterController.cs
@@ -1,50 +1,48 @@
-﻿using DotNetNote.Models;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 /// <summary>
 /// 캐릭터(Heroes) 선택: 로그인한 회원만 접근 가능
 /// </summary>
-namespace DotNetNote.Controllers
+namespace DotNetNote.Controllers;
+
+[Authorize]
+public class CharacterController : Controller
 {
-    [Authorize]
-    public class CharacterController : Controller
+    private IHeroRepository _hero;
+    private ICharacterRepository _character;
+
+    public CharacterController(IHeroRepository hero, ICharacterRepository character)
     {
-        private IHeroRepository _hero;
-        private ICharacterRepository _character;
+        _hero = hero;
+        _character = character;
+    }
 
-        public CharacterController(IHeroRepository hero, ICharacterRepository character)
+    public IActionResult Index()
+    {
+        var heroes = _hero.GetAllHeroes();
+
+        var username = User.FindFirst("UserId").Value; // 사용자 아이디 
+        var myCharacter = _character.GetCharacterByUsername(username);
+        if (myCharacter != null)
         {
-            _hero = hero;
-            _character = character;
+            ViewBag.MyCharacter = myCharacter;
         }
 
-        public IActionResult Index()
-        {
-            var heroes = _hero.GetAllHeroes();
+        return View(heroes);
+    }
 
-            var username = User.FindFirst("UserId").Value; // 사용자 아이디 
-            var myCharacter = _character.GetCharacterByUsername(username);
-            if (myCharacter != null)
-            {
-                ViewBag.MyCharacter = myCharacter;
-            }
+    public IActionResult Choice(int hero)
+    {
+        // DB에 저장
+        var username = User.FindFirst("UserId").Value; // 사용자 아이디
+        var heroId = hero; // HeroId
 
-            return View(heroes);
-        }
+        var model = new CharacterModel() { Username = username, HeroId = heroId };
 
-        public IActionResult Choice(int hero)
-        {
-            // DB에 저장
-            var username = User.FindFirst("UserId").Value; // 사용자 아이디
-            var heroId = hero; // HeroId
+        _character.SetCharacter(model);
 
-            var model = new CharacterModel() { Username = username, HeroId = heroId };
-
-            _character.SetCharacter(model);
-
-            ViewBag.ChoiceHero = hero.ToString();
-            return View();
-        }
+        ViewBag.ChoiceHero = hero.ToString();
+        return View();
     }
 }
