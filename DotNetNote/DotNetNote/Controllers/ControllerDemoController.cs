ff420b5751d37ef809a7e246bf36ecc8f83be7f2
Park YongJune <redplus@redplus.net>
2022-05-20 11:10:43 +0900

    file-scoped namespace

--- DotNetNote/DotNetNote/Controllers/ControllerDemoController.cs
+++ DotNetNote/DotNetNote/Controllers/ControllerDemoController.cs
@@ -1,32 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 
-namespace DotNetNote.Controllers
+namespace DotNetNote.Controllers;
+
+// 컨트롤러: 클래스
+// /ControllerDemo/
+public class ControllerDemoController : Controller
 {
-    // 컨트롤러: 클래스
-    // /ControllerDemo/
-    public class ControllerDemoController : Controller
+    // 액션: 메서드
+    // /ControllerDemo/Index
+    public void Index()
     {
-        // 액션: 메서드
-        // /ControllerDemo/Index
-        public void Index()
-        {
-            // 아무런 값도 출력하지 않음
-        }
+        // 아무런 값도 출력하지 않음
+    }
 
-        public string StringAction()
-        {
-            return "String을 반환하는 액션 메서드";
-        }
+    public string StringAction()
+    {
+        return "String을 반환하는 액션 메서드";
+    }
 
-        public DateTime DateTimeAction()
-        {
-            return DateTime.Now;
-        }
+    public DateTime DateTimeAction()
+    {
+        return DateTime.Now;
+    }
 
-        public IActionResult DefaultAction()
-        {
-            return View(); // 컨트롤러/액션메서드
-        }
+    public IActionResult DefaultAction()
+    {
+        return View(); // 컨트롤러/액션메서드
     }
 }
