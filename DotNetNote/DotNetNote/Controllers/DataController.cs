0e0e665ca2e9afcff5b423c1cb59e3a47437aa45
Park YongJune <redplus@redplus.net>
2022-05-21 06:41:57 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Controllers/DataController.cs
+++ DotNetNote/DotNetNote/Controllers/DataController.cs
@@ -1,16 +1,15 @@
 ﻿using DotNetNote.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetNote.Controllers
+namespace DotNetNote.Controllers;
+
+public class DataController : Controller
 {
-    public class DataController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            // 모든 데이터를 읽어서 View 페이지에 전달
-            DataService demoService = new DataService();
-            var data = demoService.GetAll();
-            return View(data);
-        }
+        // 모든 데이터를 읽어서 View 페이지에 전달
+        DataService demoService = new DataService();
+        var data = demoService.GetAll();
+        return View(data);
     }
 }
