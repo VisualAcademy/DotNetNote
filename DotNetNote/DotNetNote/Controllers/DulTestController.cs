8697133dff0c48a5c3ce0a68a18a9105af21fbd9
Park YongJune <redplus@redplus.net>
2022-05-22 07:13:06 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Controllers/DulTestController.cs
+++ DotNetNote/DotNetNote/Controllers/DulTestController.cs
@@ -1,18 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetNote.Controllers
-{
-    public class DulTestController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
+namespace DotNetNote.Controllers;
 
-        public string IsPhotoTest()
-        {
-            string file = "a.png";
-            return Dul.BoardLibrary.IsPhoto(file).ToString(); // True
-        }
+public class DulTestController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public string IsPhotoTest()
+    {
+        string file = "a.png";
+        return Dul.BoardLibrary.IsPhoto(file).ToString(); // True
     }
 }
