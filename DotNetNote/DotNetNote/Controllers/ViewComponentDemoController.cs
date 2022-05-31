e08e678d1ad8f5fb880f7407c6c2cc12db3c9066
Park YongJune <redplus@redplus.net>
2022-05-31 11:20:52 +0900

    Use expression body for methods

--- DotNetNote/DotNetNote/Controllers/ViewComponentDemoController.cs
+++ DotNetNote/DotNetNote/Controllers/ViewComponentDemoController.cs
@@ -4,10 +4,7 @@ namespace DotNetNote.Controllers;
 
 public class ViewComponentDemoController : Controller
 {
-    public IActionResult Index()
-    {
-        return View();
-    }
+    public IActionResult Index() => View();
 
     /// <summary>
     /// CopyrightViewComponent 출력 데모
