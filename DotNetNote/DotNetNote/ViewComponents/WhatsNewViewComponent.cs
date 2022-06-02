453342729d370d0477ac36e1c78a1742beefb3b9
Park YongJune <redplus@redplus.net>
2022-06-02 21:22:22 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/ViewComponents/WhatsNewViewComponent.cs
+++ DotNetNote/DotNetNote/ViewComponents/WhatsNewViewComponent.cs
@@ -1,23 +1,22 @@
 ﻿using DotNetNote.Models.Notes;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetNote.ViewComponents
+namespace DotNetNote.ViewComponents;
+
+public class WhatsNewViewComponent : ViewComponent
 {
-    public class WhatsNewViewComponent : ViewComponent
+    // 게시판 리포지토리 개체 
+    private INoteRepository _repository;
+
+    public WhatsNewViewComponent(INoteRepository repository)
     {
-        // 게시판 리포지토리 개체 
-        private INoteRepository _repository;
+        _repository = repository;
+    }
 
-        public WhatsNewViewComponent(INoteRepository repository)
-        {
-            _repository = repository;
-        }
-
-        public IViewComponentResult Invoke()
-        {
-            //return View("Details", _repository.GetRecentPosts()); // 이름 지정
-            //return View(_repository.GetRecentPosts()); // 기본값: Default
-            return View(_repository.GetRecentPostsCache()); // 기본값: Default
-        }
+    public IViewComponentResult Invoke()
+    {
+        //return View("Details", _repository.GetRecentPosts()); // 이름 지정
+        //return View(_repository.GetRecentPosts()); // 기본값: Default
+        return View(_repository.GetRecentPostsCache()); // 기본값: Default
     }
 }
