e8afaed1ada023df2aeb50f31b0f45b89b2a293f
Park YongJune <redplus@redplus.net>
2022-06-03 05:23:27 +0900

    Use expression body for constructors

--- DotNetNote/DotNetNote/ViewComponents/WhatsNew2ViewComponent.cs
+++ DotNetNote/DotNetNote/ViewComponents/WhatsNew2ViewComponent.cs
@@ -8,10 +8,7 @@ public class WhatsNew2ViewComponent : ViewComponent
     // 게시판 리포지토리 개체 
     private INoteRepository _repository;
 
-    public WhatsNew2ViewComponent(INoteRepository repository)
-    {
-        _repository = repository;
-    }
+    public WhatsNew2ViewComponent(INoteRepository repository) => _repository = repository;
 
     public IViewComponentResult Invoke()
     {
