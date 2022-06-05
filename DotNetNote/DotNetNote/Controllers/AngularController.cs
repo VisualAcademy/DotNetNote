002923c917b04c91e33808839ede4a991d59475b
Park YongJune <redplus@redplus.net>
2022-06-05 21:35:49 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Controllers/AngularController.cs
+++ DotNetNote/DotNetNote/Controllers/AngularController.cs
@@ -1,17 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetNote.Controllers
-{
-    public class AngularController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
+namespace DotNetNote.Controllers;
 
-        public IActionResult Error()
-        {
-            return View();
-        }
+public class AngularController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Error()
+    {
+        return View();
     }
 }
