40d32606873915a8582fea19feb757228e57bb3f
Park YongJune <redplus@redplus.net>
2022-06-13 04:04:53 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Controllers/Cascading/SelectListDemoController.cs
+++ DotNetNote/DotNetNote/Controllers/Cascading/SelectListDemoController.cs
@@ -3,27 +3,26 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using DotNetNote.Data;
 using System.Linq;
 
-namespace DotNetNote.Controllers.Cascading
+namespace DotNetNote.Controllers.Cascading;
+
+public class SelectListDemoController : Controller
 {
-    public class SelectListDemoController : Controller
+    private readonly ApplicationDbContext _context;
+
+    public SelectListDemoController(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public SelectListDemoController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+    public IActionResult Index()
+    {
+        ViewData["PropertyId"] =
+            new SelectList(_context.Properties.OrderBy(it => it.Name), "Id", "Name");
+        ViewData["LocationId"] =
+            new SelectList(_context.Locations.OrderBy(it => it.Name), "Id", "Name");
+        ViewData["SublocationId"] =
+            new SelectList(_context.Sublocations.OrderBy(it => it.SublocationName), "Id", "SublocationName");
 
-        public IActionResult Index()
-        {
-            ViewData["PropertyId"] =
-                new SelectList(_context.Properties.OrderBy(it => it.Name), "Id", "Name");
-            ViewData["LocationId"] =
-                new SelectList(_context.Locations.OrderBy(it => it.Name), "Id", "Name");
-            ViewData["SublocationId"] =
-                new SelectList(_context.Sublocations.OrderBy(it => it.SublocationName), "Id", "SublocationName");
-
-            return View();
-        }
+        return View();
     }
 }
