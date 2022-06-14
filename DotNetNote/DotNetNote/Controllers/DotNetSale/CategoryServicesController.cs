b35bbf2e7b126b56e3e0b7d5aa87bb4e39d99f66
Park YongJune <redplus@redplus.net>
2022-06-15 07:48:44 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Controllers/DotNetSale/CategoryServicesController.cs
+++ DotNetNote/DotNetNote/Controllers/DotNetSale/CategoryServicesController.cs
@@ -1,24 +1,23 @@
 ﻿using DotNetNote.Models.Categories;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetNote.Controllers.DotNetSale
+namespace DotNetNote.Controllers.DotNetSale;
+
+[Route("api/[controller]")]
+public class CategoryServicesController : Controller
 {
-    [Route("api/[controller]")]
-    public class CategoryServicesController : Controller
+    private readonly ICategoryRepository _repository;
+
+    public CategoryServicesController(ICategoryRepository repository)
     {
-        private readonly ICategoryRepository _repository;
+        _repository = repository;
+    }
 
-        public CategoryServicesController(ICategoryRepository repository)
-        {
-            _repository = repository;
-        }
-
-        [HttpGet]
-        public IEnumerable<Category> Get()
-        {
-            //return (new CategoryRepositoryInMemory()).GetCategories();
-            //return (new CategoryRepositorySqlServer()).GetCategories();
-            return _repository.GetCategories();
-        }
+    [HttpGet]
+    public IEnumerable<Category> Get()
+    {
+        //return (new CategoryRepositoryInMemory()).GetCategories();
+        //return (new CategoryRepositorySqlServer()).GetCategories();
+        return _repository.GetCategories();
     }
 }
