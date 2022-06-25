5809af7f747bee5f246ecb8cf1842d5306ba9a08
Park YongJune <redplus@redplus.net>
2022-06-26 02:18:14 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Controllers/Guide/GuideController.cs
+++ DotNetNote/DotNetNote/Controllers/Guide/GuideController.cs
@@ -1,12 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetNote.Controllers
+namespace DotNetNote.Controllers;
+
+/// <summary>
+/// ASP.NET Core 책 학습 가이드 
+/// </summary>
+public class GuideController : Controller
 {
-    /// <summary>
-    /// ASP.NET Core 책 학습 가이드 
-    /// </summary>
-    public class GuideController : Controller
-    {
-        public IActionResult Index() => View();
-    }
+    public IActionResult Index() => View();
 }
