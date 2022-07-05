63e30615ce1b4c6d36e797b6800ba5320163f9c4
Park YongJune <redplus@redplus.net>
2022-07-05 09:08:27 +0900

    Remove Unnecessary Usings

--- DotNetNote/DotNetNote/Models/Four/Four.cs
+++ DotNetNote/DotNetNote/Models/Four/Four.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Data;
 using Microsoft.Data.SqlClient;
