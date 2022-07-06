71ceb662c0167f5f802914e86738a07973c8fd2a
Park YongJune <redplus@redplus.net>
2022-07-06 11:09:46 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Models/Goods/01_GoodsBase.cs
+++ DotNetNote/DotNetNote/Models/Goods/01_GoodsBase.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 
-namespace DotNetNote.Models
+namespace DotNetNote.Models;
+
+/// <summary>
+/// [1] 모델 클래스
+/// Goods 테이블과 일대일로 매핑되는 모델 클래스: 
+/// Goods, GoodsModel, GoodsViewModel, GoodsBase, GoodsDto, GoodsEntity, ...
+/// </summary>
+public class GoodsBase
 {
-    /// <summary>
-    /// [1] 모델 클래스
-    /// Goods 테이블과 일대일로 매핑되는 모델 클래스: 
-    /// Goods, GoodsModel, GoodsViewModel, GoodsBase, GoodsDto, GoodsEntity, ...
-    /// </summary>
-    public class GoodsBase
-    {
-        public int GoodsId { get; set; }
-        public string GoodsName { get; set; }
-        public string GoodsDescription { get; set; }
-        public DateTimeOffset CreatedDate { get; set; }
-    }
+    public int GoodsId { get; set; }
+    public string GoodsName { get; set; }
+    public string GoodsDescription { get; set; }
+    public DateTimeOffset CreatedDate { get; set; }
 }
