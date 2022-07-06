4748d937ca9bff4d9babde7d27cc7bdfca79a504
Park YongJune <redplus@redplus.net>
2022-07-07 00:42:19 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Models/Goods/03_IGoodsRepository.cs
+++ DotNetNote/DotNetNote/Models/Goods/03_IGoodsRepository.cs
@@ -1,18 +1,17 @@
-﻿namespace DotNetNote.Models
-{
-    /// <summary>
-    /// [3] 인터페이스
-    /// Goods 테이블에 대한 CURD API 명세서 작성
-    /// </summary>
-    public interface IGoodsRepository
-    {
-        GoodsBase AddGoods(GoodsBase model);        // 입력: T Add(T model); 
-        List<GoodsBase> GetAllGoods();              // 출력: List<T> GetAll(); 
-        GoodsBase GetGoodsById(int goodsId);        // 상세: T GetById(int id); 
-        GoodsBase UpdateGoods(GoodsBase model);     // 수정: T Update(T model); 
-        void RemoveGoods(int id);                   // 삭제: void Remove(int id);
+﻿namespace DotNetNote.Models;
 
-        //int GetTotalRecordCountGoods();             // 총 레코드 수
-        GoodsSet GetAllGoodsWithPaging(int pageNumber = 1, int pageSize = 10); // 페이징
-    }
+/// <summary>
+/// [3] 인터페이스
+/// Goods 테이블에 대한 CURD API 명세서 작성
+/// </summary>
+public interface IGoodsRepository
+{
+    GoodsBase AddGoods(GoodsBase model);        // 입력: T Add(T model); 
+    List<GoodsBase> GetAllGoods();              // 출력: List<T> GetAll(); 
+    GoodsBase GetGoodsById(int goodsId);        // 상세: T GetById(int id); 
+    GoodsBase UpdateGoods(GoodsBase model);     // 수정: T Update(T model); 
+    void RemoveGoods(int id);                   // 삭제: void Remove(int id);
+
+    //int GetTotalRecordCountGoods();             // 총 레코드 수
+    GoodsSet GetAllGoodsWithPaging(int pageNumber = 1, int pageSize = 10); // 페이징
 }
