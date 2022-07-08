c4162e74f22a587e6e83bc67631857263aa677af
Park YongJune <redplus@redplus.net>
2022-07-08 12:44:23 +0900

    Code Cleanup

--- DotNetNote/DotNetNote/Models/One/IOneRepository.cs
+++ DotNetNote/DotNetNote/Models/One/IOneRepository.cs
@@ -1,8 +1,11 @@
-﻿namespace DotNetNote.Models
+﻿namespace DotNetNote.Models;
+
+/// <summary>
+/// 리포지토리 인터페이스 
+/// </summary>
+public interface IOneRepository
 {
-    public interface IOneRepository
-    {
-        One Add(One model);
-        List<One> GetAll();
-    }
+    One Add(One model);
+
+    List<One> GetAll();
 }
--- DotNetNote/DotNetNote/Models/One/OneRepository.cs
+++ DotNetNote/DotNetNote/Models/One/OneRepository.cs
@@ -3,37 +3,39 @@ using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using System.Linq;
 
-namespace DotNetNote.Models
+namespace DotNetNote.Models;
+
+/// <summary>
+/// 리포지토리 클래스 
+/// </summary>
+public class OneRepository : IOneRepository
 {
-    public class OneRepository : IOneRepository
+    private SqlConnection db;
+    private IConfiguration _config;
+
+    public OneRepository(IConfiguration config)
     {
-        private SqlConnection db;
-        private IConfiguration _config;
+        _config = config;
+        db = new SqlConnection(
+                _config
+                    .GetSection("ConnectionStrings")
+                        .GetSection("DefaultConnection").Value);
+    }
 
-        public OneRepository(IConfiguration config)
-        {
-            _config = config;
-            db = new SqlConnection(
-                    _config
-                        .GetSection("ConnectionStrings")
-                            .GetSection("DefaultConnection").Value);
-        }
+    public List<One> GetAll()
+    {
+        string sql = "Select * From Ones Order By Id Asc";
+        return db.Query<One>(sql).ToList();
+    }
 
-        public List<One> GetAll()
-        {
-            string sql = "Select * From Ones Order By Id Asc";
-            return db.Query<One>(sql).ToList();
-        }
-
-        public One Add(One model)
-        {
-            string sql = @"
+    public One Add(One model)
+    {
+        string sql = @"
                 Insert Into Ones (Note) Values (@Note);
                 Select Cast(SCOPE_IDENTITY() As Int);
             ";
-            var id = db.Query<int>(sql, model).Single();
-            model.Id = id;
-            return model;
-        }
+        var id = db.Query<int>(sql, model).Single();
+        model.Id = id;
+        return model;
     }
 }
