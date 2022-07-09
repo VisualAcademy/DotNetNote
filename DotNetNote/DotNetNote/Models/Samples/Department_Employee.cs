9e049fe098c7e0cc4c0f6659c60711fe9a0ced3f
Park YongJune <redplus@redplus.net>
2022-07-10 03:00:36 +0900

    Code Cleanup

--- DotNetNote/DotNetNote/Models/Samples/Department_Employee.cs
+++ DotNetNote/DotNetNote/Models/Samples/Department_Employee.cs
@@ -11,7 +11,9 @@ namespace DotNetNote.Models.Samples
     public class Department
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         public ICollection<Employee> Type { get; set; }
     }
 
