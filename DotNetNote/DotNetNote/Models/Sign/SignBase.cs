8aac671f4386c5ac0e27a6600d67e424188a2d9b
Park YongJune <redplus@redplus.net>
2022-07-11 09:37:33 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Models/Sign/SignBase.cs
+++ DotNetNote/DotNetNote/Models/Sign/SignBase.cs
@@ -1,15 +1,14 @@
-﻿namespace DotNetNote.Models
+﻿namespace DotNetNote.Models;
+
+public class SignBase
 {
-    public class SignBase
-    {
-        public int SignId { get; set; }         // 일련번호
+    public int SignId { get; set; }         // 일련번호
 
-        public string Username { get; set; }    // 아이디
+    public string Username { get; set; }    // 아이디
 
-        public string Password { get; set; }    // 암호
+    public string Password { get; set; }    // 암호
 
-        public string Name { get; set; }        // 이름
+    public string Name { get; set; }        // 이름
 
-        public string Email { get; set; }       // 이메일
-    }
+    public string Email { get; set; }       // 이메일
 }
