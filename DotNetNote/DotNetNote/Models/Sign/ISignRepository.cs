f9c779b098d69b4fe305bd88261447c0010c05bd
Park YongJune <redplus@redplus.net>
2022-07-11 09:27:52 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Models/Sign/ISignRepository.cs
+++ DotNetNote/DotNetNote/Models/Sign/ISignRepository.cs
@@ -1,20 +1,19 @@
-﻿namespace DotNetNote.Models
+﻿namespace DotNetNote.Models;
+
+public interface ISignRepository
 {
-    public interface ISignRepository
-    {
-        /// <summary>
-        /// 로그인 되었는지 확인
-        /// </summary>
-        bool IsAuthenticated(SignViewModel model);
+    /// <summary>
+    /// 로그인 되었는지 확인
+    /// </summary>
+    bool IsAuthenticated(SignViewModel model);
 
-        /// <summary>
-        /// 회원 가입
-        /// </summary>
-        SignBase AddSign(SignViewModel model);
+    /// <summary>
+    /// 회원 가입
+    /// </summary>
+    SignBase AddSign(SignViewModel model);
 
-        /// <summary>
-        /// 회원 정보
-        /// </summary>
-        SignBase GetSignByEmail(string email);
-    }
+    /// <summary>
+    /// 회원 정보
+    /// </summary>
+    SignBase GetSignByEmail(string email);
 }
