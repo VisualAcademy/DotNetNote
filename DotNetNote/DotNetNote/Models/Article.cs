f2487aa4c3e0a93c9bafa0ddecdb53629af13432
Park YongJune <redplus@redplus.net>
2022-07-13 01:32:24 +0900

    Code Cleanup

--- DotNetNote/DotNetNote/Models/Article.cs
+++ DotNetNote/DotNetNote/Models/Article.cs
@@ -5,8 +5,11 @@ namespace DotNetNote.Models
     public class Article
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         public string Title { get; set; }
+
         public DateTime PostDate { get; set; }
     }
 }
--- DotNetNote/DotNetNote/Models/User/DotNetNoteUser.cs
+++ DotNetNote/DotNetNote/Models/User/DotNetNoteUser.cs
@@ -3,8 +3,11 @@
     public class DotNetNoteUser
     {
         public string Id { get; set; }
+
         public string UserName { get; set; }
+
         public string NormalizedUserName { get; set; }
+
         public string PasswordHash { get; set; }
     }
 }
