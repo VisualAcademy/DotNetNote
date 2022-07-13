00d19559c73f5470c154b78c4ca6091dd74fd9a6
Park YongJune <redplus@redplus.net>
2022-07-14 00:16:35 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Models/User/DotNetNoteUserRegisterModel.cs
+++ DotNetNote/DotNetNote/Models/User/DotNetNoteUserRegisterModel.cs
@@ -1,16 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace DotNetNote
+namespace DotNetNote;
+
+public class DotNetNoteUserRegisterModel
 {
-    public class DotNetNoteUserRegisterModel
-    {
-        public string UserName { get; set; }
+    public string UserName { get; set; }
 
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
 
-        [Compare("Password")]
-        [DataType(DataType.Password)]
-        public string ConfirmPassword { get; set; }
-    }
+    [Compare("Password")]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; }
 }
