abb549ebebf98b0ce059f584ee0a317dd4c06dfa
Park YongJune <redplus@redplus.net>
2022-07-14 00:15:22 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Models/User/DotNetNoteLoginModel.cs
+++ DotNetNote/DotNetNote/Models/User/DotNetNoteLoginModel.cs
@@ -1,12 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace DotNetNoteCore
-{
-    public class DotNetNoteLoginModel
-    {
-        public string UserName { get; set; }
+namespace DotNetNoteCore;
 
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
-    }
+public class DotNetNoteLoginModel
+{
+    public string UserName { get; set; }
+
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
 }
