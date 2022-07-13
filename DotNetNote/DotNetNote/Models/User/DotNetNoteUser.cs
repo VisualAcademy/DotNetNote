e9bff3027098fbe6e8e08bbd22244febd7f81d8b
Park YongJune <redplus@redplus.net>
2022-07-14 00:15:55 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Models/User/DotNetNoteUser.cs
+++ DotNetNote/DotNetNote/Models/User/DotNetNoteUser.cs
@@ -1,13 +1,12 @@
-﻿namespace DotNetNoteCore
+﻿namespace DotNetNoteCore;
+
+public class DotNetNoteUser
 {
-    public class DotNetNoteUser
-    {
-        public string Id { get; set; }
+    public string Id { get; set; }
 
-        public string UserName { get; set; }
+    public string UserName { get; set; }
 
-        public string NormalizedUserName { get; set; }
+    public string NormalizedUserName { get; set; }
 
-        public string PasswordHash { get; set; }
-    }
+    public string PasswordHash { get; set; }
 }
