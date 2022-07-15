514645750fbf771b79b5100dd34872873651a814
Park YongJune <redplus@redplus.net>
2022-07-16 02:21:54 +0900

    .

--- DotNetNote/DotNetNote/Models/DemoClass.cs
+++ DotNetNote/DotNetNote/Models/DemoClass.cs
@@ -3,6 +3,7 @@
     public class DemoClass
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
     }
 }
--- DotNetNote/DotNetNote/Models/DemoModel.cs
+++ DotNetNote/DotNetNote/Models/DemoModel.cs
@@ -3,6 +3,7 @@
     public class DemoModel
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
     }
 }
