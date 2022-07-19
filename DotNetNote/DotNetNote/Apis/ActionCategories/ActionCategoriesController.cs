300bab27f839096a1bfb0a8eb6a5194e6b9cef25
Park YongJune <redplus@redplus.net>
2022-07-19 10:59:40 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Apis/ActionCategories/ActionCategoriesController.cs
+++ DotNetNote/DotNetNote/Apis/ActionCategories/ActionCategoriesController.cs
@@ -5,101 +5,100 @@ using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Acts.Controllers.ActionCategories
+namespace Acts.Controllers.ActionCategories;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ActionCategoriesController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ActionCategoriesController : ControllerBase
+    private readonly ActContext _context;
+
+    public ActionCategoriesController(ActContext context)
     {
-        private readonly ActContext _context;
+        _context = context;
+    }
 
-        public ActionCategoriesController(ActContext context)
+    // GET: api/ActionCategories
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ActionCategory>>> GetActionCategories()
+    {
+        return await _context.ActionCategories.ToListAsync();
+    }
+
+    // GET: api/ActionCategories/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ActionCategory>> GetActionCategory(long id)
+    {
+        var actionCategory = await _context.ActionCategories.FindAsync(id);
+
+        if (actionCategory == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        // GET: api/ActionCategories
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<ActionCategory>>> GetActionCategories()
+        return actionCategory;
+    }
+
+    // PUT: api/ActionCategories/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutActionCategory(long id, ActionCategory actionCategory)
+    {
+        if (id != actionCategory.Id)
         {
-            return await _context.ActionCategories.ToListAsync();
+            return BadRequest();
         }
 
-        // GET: api/ActionCategories/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<ActionCategory>> GetActionCategory(long id)
-        {
-            var actionCategory = await _context.ActionCategories.FindAsync(id);
+        _context.Entry(actionCategory).State = EntityState.Modified;
 
-            if (actionCategory == null)
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!ActionCategoryExists(id))
             {
                 return NotFound();
             }
-
-            return actionCategory;
-        }
-
-        // PUT: api/ActionCategories/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutActionCategory(long id, ActionCategory actionCategory)
-        {
-            if (id != actionCategory.Id)
+            else
             {
-                return BadRequest();
+                throw;
             }
-
-            _context.Entry(actionCategory).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ActionCategoryExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
         }
 
-        // POST: api/ActionCategories
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<ActionCategory>> PostActionCategory(ActionCategory actionCategory)
+        return NoContent();
+    }
+
+    // POST: api/ActionCategories
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<ActionCategory>> PostActionCategory(ActionCategory actionCategory)
+    {
+        _context.ActionCategories.Add(actionCategory);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetActionCategory", new { id = actionCategory.Id }, actionCategory);
+    }
+
+    // DELETE: api/ActionCategories/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteActionCategory(long id)
+    {
+        var actionCategory = await _context.ActionCategories.FindAsync(id);
+        if (actionCategory == null)
         {
-            _context.ActionCategories.Add(actionCategory);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetActionCategory", new { id = actionCategory.Id }, actionCategory);
+            return NotFound();
         }
 
-        // DELETE: api/ActionCategories/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteActionCategory(long id)
-        {
-            var actionCategory = await _context.ActionCategories.FindAsync(id);
-            if (actionCategory == null)
-            {
-                return NotFound();
-            }
+        _context.ActionCategories.Remove(actionCategory);
+        await _context.SaveChangesAsync();
 
-            _context.ActionCategories.Remove(actionCategory);
-            await _context.SaveChangesAsync();
+        return NoContent();
+    }
 
-            return NoContent();
-        }
-
-        private bool ActionCategoryExists(long id)
-        {
-            return _context.ActionCategories.Any(e => e.Id == id);
-        }
+    private bool ActionCategoryExists(long id)
+    {
+        return _context.ActionCategories.Any(e => e.Id == id);
     }
 }
