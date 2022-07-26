9410e33b6613efc6c2481ad69d440298d075caed
Park YongJune <redplus@redplus.net>
2022-07-26 09:39:29 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Controllers/CustomersController.cs
+++ DotNetNote/DotNetNote/Controllers/CustomersController.cs
@@ -2,21 +2,19 @@
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetNote.Controllers
+namespace DotNetNote.Controllers;
+
+[Route("api/[controller]")]
+[EnableCors("AllowAnyOrigin")]
+public class CustomersController : Controller
 {
-    [Route("api/[controller]")]
-    [EnableCors("AllowAnyOrigin")]
-    public class CustomersController : Controller
+    [HttpGet]
+    public IEnumerable<Customer> Get()
     {
-        [HttpGet]
-        public IEnumerable<Customer> Get()
-        {
-            return new List<Customer> {
-                new Customer { Id = 1, Name = "김태영", City = "서울" },
-                new Customer { Id = 2, Name = "박용준", City = "인천" },
-                new Customer { Id = 3, Name = "한상훈", City = "경기" }
-            };
-        }
+        return new List<Customer> {
+            new Customer { Id = 1, Name = "김태영", City = "서울" },
+            new Customer { Id = 2, Name = "박용준", City = "인천" },
+            new Customer { Id = 3, Name = "한상훈", City = "경기" }
+        };
     }
 }
-
