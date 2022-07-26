9add5ae8e48fd42758a722869d0a0e21e7a9072a
Park YongJune <redplus@redplus.net>
2022-07-26 09:37:57 +0900

    .

--- DotNetNote/DotNetNote.SqlServer/dbo/Tables/Notifications/MyNotifications.sql
+++ DotNetNote/DotNetNote.SqlServer/dbo/Tables/Notifications/MyNotifications.sql
@@ -1,19 +1,12 @@
-﻿CREATE TABLE [dbo].[MyNotifications](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[Timestamp] [datetimeoffset](7) NOT NULL,
+﻿CREATE TABLE [dbo].[MyNotifications] 
+(
+	[Id] [int] IDENTITY(1,1) Primary Key NOT NULL,
+	[Timestamp] [datetimeoffset](7) NOT NULL Default(sysdatetimeoffset()),
 	[Message] [nvarchar](max) NULL,
 	[Type] [nvarchar](50) NULL,
 	[Url] [nvarchar](max) NULL,
 	--------------------------------------------------------
 	UserId Int Null,
 	IsComplete Bit Default(0) Not Null
-
- CONSTRAINT [PK_MyNotifications] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[MyNotifications] ADD  CONSTRAINT [DF_MyNotifications_Timestamp]  DEFAULT (sysdatetimeoffset()) FOR [Timestamp]
+)
 GO
