c856c07aef7de114a46b68a8fd441fb079710321
Park YongJune <redplus@redplus.net>
2022-08-03 04:15:59 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/TagHelpers/EmailLinkTagHelper.cs
+++ DotNetNote/DotNetNote/TagHelpers/EmailLinkTagHelper.cs
@@ -1,27 +1,26 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
 using System.Threading.Tasks;
 
-namespace DotNetNote.TagHelpers
+namespace DotNetNote.TagHelpers;
+
+// <el>Help<el>
+//[HtmlTargetElement("email-link")]
+[HtmlTargetElement("el")]
+public class EmailLinkTagHelper : TagHelper
 {
-    // <el>Help<el>
-    //[HtmlTargetElement("email-link")]
-    [HtmlTargetElement("el")]
-    public class EmailLinkTagHelper : TagHelper
+    const string domain = "dotnetkorea.com";
+    public override async Task ProcessAsync(
+        TagHelperContext context, TagHelperOutput output)
     {
-        const string domain = "dotnetkorea.com";
-        public override async Task ProcessAsync(
-            TagHelperContext context, TagHelperOutput output)
-        {
-            output.TagName = "a";
+        output.TagName = "a";
 
-            string originText = 
-                (await output.GetChildContentAsync()).GetContent();
+        string originText = 
+            (await output.GetChildContentAsync()).GetContent();
 
-            string emailString = $"{originText}@{domain}";
+        string emailString = $"{originText}@{domain}";
 
-            output.Attributes.Add("href", $"mailto:{emailString}");
+        output.Attributes.Add("href", $"mailto:{emailString}");
 
-            output.Content.SetContent(emailString);
-        }
+        output.Content.SetContent(emailString);
     }
 }
