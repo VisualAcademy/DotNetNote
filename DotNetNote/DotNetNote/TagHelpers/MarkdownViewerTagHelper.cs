9edb0fae756c891a370ee7ceb17fea96a9eb2c4f
Park YongJune <redplus@redplus.net>
2022-08-03 04:16:17 +0900

    .

--- DotNetNote/DotNetNote/TagHelpers/MarkdownViewerTagHelper.cs
+++ DotNetNote/DotNetNote/TagHelpers/MarkdownViewerTagHelper.cs
@@ -1,21 +1,20 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
 using System.Threading.Tasks;
 
-namespace DotNetNote.TagHelpers
+namespace DotNetNote.TagHelpers;
+
+[HtmlTargetElement("markdown-viewer")]
+public class MarkdownViewerTagHelper : TagHelper
 {
-    [HtmlTargetElement("markdown-viewer")]
-    public class MarkdownViewerTagHelper : TagHelper
+    public async override Task ProcessAsync(
+        TagHelperContext context, TagHelperOutput output)
     {
-        public async override Task ProcessAsync(
-            TagHelperContext context, TagHelperOutput output)
-        {
-            // 태그 헬퍼 안의 텍스트 가져오기
-            var content = await output.GetChildContentAsync();
-            var result =
-                CommonMark.CommonMarkConverter.Convert(
-                    content.GetContent()); // HTML 결과
-            output.Content.SetHtmlContent(result); // HTML 출력
-            output.TagName = null; // 따로 특정 태그로 묶이지 않음
-        }
+        // 태그 헬퍼 안의 텍스트 가져오기
+        var content = await output.GetChildContentAsync();
+        var result =
+            CommonMark.CommonMarkConverter.Convert(
+                content.GetContent()); // HTML 결과
+        output.Content.SetHtmlContent(result); // HTML 출력
+        output.TagName = null; // 따로 특정 태그로 묶이지 않음
     }
 }
