2f1f974be2d8ed73b69885c6a4696b895191885c
Park YongJune <redplus@redplus.net>
2022-08-08 07:07:36 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Controllers/Form/FormPostDemoController.cs
+++ DotNetNote/DotNetNote/Controllers/Form/FormPostDemoController.cs
@@ -1,26 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetNote.Controllers
+namespace DotNetNote.Controllers;
+
+public class FormPostDemoController : Controller
 {
-    public class FormPostDemoController : Controller
+    /// <summary>
+    /// 폼 데이터 전송 및 받기
+    /// </summary>
+    [HttpGet] // /FormPostDemo/Index
+    public IActionResult Index()
     {
-        /// <summary>
-        /// 폼 데이터 전송 및 받기
-        /// </summary>
-        [HttpGet] // /FormPostDemo/Index
-        public IActionResult Index()
-        {
-            return View();
-        }
+        return View();
+    }
 
-        [HttpPost] // /FormPostDemo/Index
-        public IActionResult Index(string name, string content)
-        {
-            //ViewBag.Result = $"이름: {Request.Form["name"]}, " 
-            //    + $"내용: {Request.Form["content"]}";
-            ViewBag.Result = $"이름: {name}, 내용: {content}";
+    [HttpPost] // /FormPostDemo/Index
+    public IActionResult Index(string name, string content)
+    {
+        //ViewBag.Result = $"이름: {Request.Form["name"]}, " 
+        //    + $"내용: {Request.Form["content"]}";
+        ViewBag.Result = $"이름: {name}, 내용: {content}";
 
-            return View();
-        }
+        return View();
     }
 }
