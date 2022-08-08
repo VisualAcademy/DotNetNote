939257d0693e49c68f21371ebcf509719048867b
Park YongJune <redplus@redplus.net>
2022-08-09 08:22:00 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Controllers/FluctuationStates/FluctuationStateServicesController.cs
+++ DotNetNote/DotNetNote/Controllers/FluctuationStates/FluctuationStateServicesController.cs
@@ -1,28 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetNote.Controllers
+namespace DotNetNote.Controllers;
+
+public class FluctuationStateServicesController : Controller
 {
-    public class FluctuationStateServicesController : Controller
+    private readonly IFluctuationStateRepository _repository;
+
+    public FluctuationStateServicesController() => _repository = new FluctuationStateRepositoryInMemory();
+
+    [HttpPost]
+    [Route("api/FluctuationStateServices/GetFluctuationState")]
+    public List<FluctuationStateDto> GetFluctuationState()
     {
-        private readonly IFluctuationStateRepository _repository;
+        #region +
+        //List<FluctuationStateDto> r = new List<FluctuationStateDto>()
+        //{
+        //    new FluctuationStateDto { Text = "증가", Value = "0"},
+        //    new FluctuationStateDto { Text = "감소", Value = "1"},
+        //    new FluctuationStateDto { Text = "고정", Value = "2"},
+        //};
 
-        public FluctuationStateServicesController() => _repository = new FluctuationStateRepositoryInMemory();
-
-        [HttpPost]
-        [Route("api/FluctuationStateServices/GetFluctuationState")]
-        public List<FluctuationStateDto> GetFluctuationState()
-        {
-            #region +
-            //List<FluctuationStateDto> r = new List<FluctuationStateDto>()
-            //{
-            //    new FluctuationStateDto { Text = "증가", Value = "0"},
-            //    new FluctuationStateDto { Text = "감소", Value = "1"},
-            //    new FluctuationStateDto { Text = "고정", Value = "2"},
-            //};
-
-            //return r; 
-            #endregion
-            return _repository.GetFluctuationStates();
-        }
+        //return r; 
+        #endregion
+        return _repository.GetFluctuationStates();
     }
 }
