bf6590a0a62671b21711fa8164e9bf20736cd712
Park YongJune <redplus@redplus.net>
2022-08-21 23:02:05 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Controllers/DotNetNote/NoteCommentServiceController.cs
+++ DotNetNote/DotNetNote/Controllers/DotNetNote/NoteCommentServiceController.cs
@@ -1,17 +1,16 @@
 ﻿using DotNetNote.Models.Notes;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetNote.Controllers
+namespace DotNetNote.Controllers;
+
+[Route("api/[controller]")]
+public class NoteCommentServiceController : Controller
 {
-    [Route("api/[controller]")]
-    public class NoteCommentServiceController : Controller
-    {
-        private INoteCommentRepository _repository;
+    private INoteCommentRepository _repository;
 
-        public NoteCommentServiceController(INoteCommentRepository repository) => _repository = repository;
+    public NoteCommentServiceController(INoteCommentRepository repository) => _repository = repository;
 
-        // 최근 댓글 리스트 반환
-        [HttpGet]
-        public IEnumerable<NoteComment> Get() => _repository.GetRecentComments();
-    }
+    // 최근 댓글 리스트 반환
+    [HttpGet]
+    public IEnumerable<NoteComment> Get() => _repository.GetRecentComments();
 }
