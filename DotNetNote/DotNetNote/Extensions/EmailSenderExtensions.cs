95265b345e6aa2e17ac0589dfacaa2cc1f939e8b
Park YongJune <redplus@redplus.net>
2022-09-18 02:38:51 +0900

    User expression body for methods

--- DotNetNote/DotNetNote/Extensions/EmailSenderExtensions.cs
+++ DotNetNote/DotNetNote/Extensions/EmailSenderExtensions.cs
@@ -5,9 +5,6 @@ namespace DotNetNote.Services;
 
 public static class EmailSenderExtensions
 {
-    public static Task SendEmailConfirmationAsync(this IEmailSender emailSender, string email, string link)
-    {
-        return emailSender.SendEmailAsync(email, "Confirm your email",
+    public static Task SendEmailConfirmationAsync(this IEmailSender emailSender, string email, string link) => emailSender.SendEmailAsync(email, "Confirm your email",
             $"Please confirm your account by clicking this link: <a href='{HtmlEncoder.Default.Encode(link)}'>link</a>");
-    }
 }
