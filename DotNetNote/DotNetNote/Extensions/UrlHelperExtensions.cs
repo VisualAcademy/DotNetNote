b0e093a28193df98a8392c4c08f6d0b93f1873c2
Park YongJune <redplus@redplus.net>
2022-09-20 05:30:07 +0900

    Use expression body for methods

--- DotNetNote/DotNetNote/Extensions/UrlHelperExtensions.cs
+++ DotNetNote/DotNetNote/Extensions/UrlHelperExtensions.cs
@@ -4,14 +4,11 @@ namespace Microsoft.AspNetCore.Mvc;
 
 public static class UrlHelperExtensions
 {
-    public static string EmailConfirmationLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
-    {
-        return urlHelper.Action(
+    public static string EmailConfirmationLink(this IUrlHelper urlHelper, string userId, string code, string scheme) => urlHelper.Action(
             action: nameof(AccountController.ConfirmEmail),
             controller: "Account",
             values: new { userId, code },
             protocol: scheme);
-    }
 
     public static string ResetPasswordCallbackLink(this IUrlHelper urlHelper, string userId, string code, string scheme)
     {
