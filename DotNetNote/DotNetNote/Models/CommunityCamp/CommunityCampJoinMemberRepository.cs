0b6c8beef03d65ddd591dcc112bac0e4bfcf9a2e
Park YongJune <redplus@redplus.net>
2022-09-23 07:51:51 +0900

    .

--- DotNetNote/DotNetNote/Models/CommunityCamp/CommunityCampJoinMemberRepository.cs
+++ DotNetNote/DotNetNote/Models/CommunityCamp/CommunityCampJoinMemberRepository.cs
@@ -9,26 +9,26 @@ public class CommunityCampJoinMemberRepository :
     ICommunityCampJoinMemberRepository
 {
     private readonly IConfiguration _config;
-    private SqlConnection con;
+    private SqlConnection _db;
 
     public CommunityCampJoinMemberRepository(IConfiguration config)
     {
         _config = config;
-        con = new SqlConnection(_config.GetSection("Data")
+        _db = new SqlConnection(_config.GetSection("Data")
             .GetSection("DefaultConnection").GetSection("ConnectionString").Value);
     }
 
-    public List<CommunityCampJoinMember> GetAll() => con.Query<CommunityCampJoinMember>(
+    public List<CommunityCampJoinMember> GetAll() => _db.Query<CommunityCampJoinMember>(
             "Select * From CommunityCampJoinMembers Order By Id Asc").ToList();
 
-    public void AddMember(CommunityCampJoinMember model) => con.Execute("Insert Into CommunityCampJoinMembers "
+    public void AddMember(CommunityCampJoinMember model) => _db.Execute("Insert Into CommunityCampJoinMembers "
             + " (CommunityName, Name, Mobile, Email, Size, CreationDate) "
             + " Values (@CommunityName, @Name, @Mobile, @Email, @Size, GetDate())",
             model);
 
     public void DeleteMember(CommunityCampJoinMember model)
     {
-        con.Execute("Delete CommunityCampJoinMembers Where " 
+        _db.Execute("Delete CommunityCampJoinMembers Where " 
             + " CommunityName = @CommunityName And Name = @Name And " 
             + "Mobile = @Mobile And Email = @Email", model);
     }
