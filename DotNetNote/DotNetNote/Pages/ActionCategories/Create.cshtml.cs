1c2e2074ff0ba2460a88dee36c54195806d5c1eb
Park YongJune <redplus@redplus.net>
2022-09-24 11:14:41 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Pages/ActionCategories/Create.cshtml.cs
+++ DotNetNote/DotNetNote/Pages/ActionCategories/Create.cshtml.cs
@@ -4,31 +4,30 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Acts.Models;
 
-namespace Acts.Pages.ActionCategories
+namespace Acts.Pages.ActionCategories;
+
+public class CreateModel : PageModel
 {
-    public class CreateModel : PageModel
+    private readonly Acts.Models.ActContext _context;
+
+    public CreateModel(ActContext context) => _context = context;
+
+    public IActionResult OnGet() => Page();
+
+    [BindProperty]
+    public ActionCategory ActionCategory { get; set; }
+
+    // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+    public async Task<IActionResult> OnPostAsync()
     {
-        private readonly Acts.Models.ActContext _context;
-
-        public CreateModel(ActContext context) => _context = context;
-
-        public IActionResult OnGet() => Page();
-
-        [BindProperty]
-        public ActionCategory ActionCategory { get; set; }
-
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
+        if (!ModelState.IsValid)
         {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            _context.ActionCategories.Add(ActionCategory);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
+            return Page();
         }
+
+        _context.ActionCategories.Add(ActionCategory);
+        await _context.SaveChangesAsync();
+
+        return RedirectToPage("./Index");
     }
 }
