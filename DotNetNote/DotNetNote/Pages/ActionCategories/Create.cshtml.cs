a166e8b42d13a1c69b4fe350eca71e1966b4f167
Park YongJune <redplus@redplus.net>
2022-09-24 11:12:51 +0900

    Remove Unnecessary Usings

--- DotNetNote/DotNetNote/Pages/ActionCategories/Create.cshtml.cs
+++ DotNetNote/DotNetNote/Pages/ActionCategories/Create.cshtml.cs
@@ -1,11 +1,7 @@
 ﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Acts.Models;
 
 namespace Acts.Pages.ActionCategories
