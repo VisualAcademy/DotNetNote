6a76e013b19bde456299244a346e8c56240c09a3
Park YongJune <redplus@redplus.net>
2022-09-25 13:55:38 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Pages/ActionCategories/Delete.cshtml.cs
+++ DotNetNote/DotNetNote/Pages/ActionCategories/Delete.cshtml.cs
@@ -5,52 +5,51 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Acts.Models;
 
-namespace Acts.Pages.ActionCategories
+namespace Acts.Pages.ActionCategories;
+
+public class DeleteModel : PageModel
 {
-    public class DeleteModel : PageModel
+    private readonly Acts.Models.ActContext _context;
+
+    public DeleteModel(Acts.Models.ActContext context)
     {
-        private readonly Acts.Models.ActContext _context;
+        _context = context;
+    }
 
-        public DeleteModel(Acts.Models.ActContext context)
+    [BindProperty]
+    public ActionCategory ActionCategory { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(long? id)
+    {
+        if (id == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        [BindProperty]
-        public ActionCategory ActionCategory { get; set; }
+        ActionCategory = await _context.ActionCategories.FirstOrDefaultAsync(m => m.Id == id);
 
-        public async Task<IActionResult> OnGetAsync(long? id)
+        if (ActionCategory == null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            return NotFound();
+        }
+        return Page();
+    }
 
-            ActionCategory = await _context.ActionCategories.FirstOrDefaultAsync(m => m.Id == id);
-
-            if (ActionCategory == null)
-            {
-                return NotFound();
-            }
-            return Page();
+    public async Task<IActionResult> OnPostAsync(long? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
         }
 
-        public async Task<IActionResult> OnPostAsync(long? id)
+        ActionCategory = await _context.ActionCategories.FindAsync(id);
+
+        if (ActionCategory != null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            ActionCategory = await _context.ActionCategories.FindAsync(id);
-
-            if (ActionCategory != null)
-            {
-                _context.ActionCategories.Remove(ActionCategory);
-                await _context.SaveChangesAsync();
-            }
-
-            return RedirectToPage("./Index");
+            _context.ActionCategories.Remove(ActionCategory);
+            await _context.SaveChangesAsync();
         }
+
+        return RedirectToPage("./Index");
     }
 }
