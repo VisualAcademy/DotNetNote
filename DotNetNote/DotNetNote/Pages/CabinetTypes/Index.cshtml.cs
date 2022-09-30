edaa1cde6e910897a6773cabdbc4539a02728eac
Park YongJune <redplus@redplus.net>
2022-10-01 01:50:57 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Pages/CabinetTypes/Index.cshtml.cs
+++ DotNetNote/DotNetNote/Pages/CabinetTypes/Index.cshtml.cs
@@ -3,16 +3,15 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotNetNote.Pages.CabinetTypes
+namespace DotNetNote.Pages.CabinetTypes;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
-    {
-        private readonly Data.ApplicationDbContext _context;
+    private readonly Data.ApplicationDbContext _context;
 
-        public IndexModel(DotNetNote.Data.ApplicationDbContext context) => _context = context;
+    public IndexModel(DotNetNote.Data.ApplicationDbContext context) => _context = context;
 
-        public IList<CabinetType> CabinetType { get;set; }
+    public IList<CabinetType> CabinetType { get;set; }
 
-        public async Task OnGetAsync() => CabinetType = await _context.CabinetTypes.ToListAsync();
-    }
+    public async Task OnGetAsync() => CabinetType = await _context.CabinetTypes.ToListAsync();
 }
