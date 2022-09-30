baa63da3d7fbea7e8a35bb7eaddd5637e6555b38
Park YongJune <redplus@redplus.net>
2022-10-01 01:49:12 +0900

    Remove Unnecessary Usings

--- DotNetNote/DotNetNote/Pages/CabinetTypes/Index.cshtml.cs
+++ DotNetNote/DotNetNote/Pages/CabinetTypes/Index.cshtml.cs
@@ -1,13 +1,7 @@
 ﻿#nullable disable
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using DotNetNote.Data;
-using DotNetNote.Models;
 
 namespace DotNetNote.Pages.CabinetTypes
 {
