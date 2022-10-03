099c1a4709f5fa12e43453667430856d11601b91
Park YongJune <redplus@redplus.net>
2022-10-04 01:09:24 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Pages/Cascading/Properties/Create.cshtml.cs
+++ DotNetNote/DotNetNote/Pages/Cascading/Properties/Create.cshtml.cs
@@ -4,37 +4,36 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using VisualAcademy.Models;
 
-namespace VisualAcademy.Pages.Cascading.Properties
+namespace VisualAcademy.Pages.Cascading.Properties;
+
+public class CreateModel : PageModel
 {
-    public class CreateModel : PageModel
+    private readonly DotNetNote.Data.ApplicationDbContext _context;
+
+    public CreateModel(DotNetNote.Data.ApplicationDbContext context)
     {
-        private readonly DotNetNote.Data.ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public CreateModel(DotNetNote.Data.ApplicationDbContext context)
-        {
-            _context = context;
-        }
+    public IActionResult OnGet()
+    {
+        return Page();
+    }
 
-        public IActionResult OnGet()
+    [BindProperty]
+    public Property Property { get; set; }
+
+    // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
         {
             return Page();
         }
 
-        [BindProperty]
-        public Property Property { get; set; }
+        _context.Properties.Add(Property);
+        await _context.SaveChangesAsync();
 
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-
-            _context.Properties.Add(Property);
-            await _context.SaveChangesAsync();
-
-            return RedirectToPage("./Index");
-        }
+        return RedirectToPage("./Index");
     }
 }
