c5ae5e7fdb39d95fb576a02430e1f32a8cc7fbc3
Park YongJune <redplus@redplus.net>
2022-10-06 02:00:17 +0900

    Use expression body for constructors

--- DotNetNote/DotNetNote/Pages/Cascading/Properties/Details.cshtml.cs
+++ DotNetNote/DotNetNote/Pages/Cascading/Properties/Details.cshtml.cs
@@ -11,10 +11,7 @@ public class DetailsModel : PageModel
 {
     private readonly DotNetNote.Data.ApplicationDbContext _context;
 
-    public DetailsModel(DotNetNote.Data.ApplicationDbContext context)
-    {
-        _context = context;
-    }
+    public DetailsModel(DotNetNote.Data.ApplicationDbContext context) => _context = context;
 
     public Property Property { get; set; }
 
