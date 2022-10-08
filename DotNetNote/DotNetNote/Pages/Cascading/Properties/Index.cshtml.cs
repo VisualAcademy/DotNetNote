d59da65d6cd33fbe0c444de88a5def897d0e652e
Park YongJune <redplus@redplus.net>
2022-10-08 09:34:11 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Pages/Cascading/Properties/Index.cshtml.cs
+++ DotNetNote/DotNetNote/Pages/Cascading/Properties/Index.cshtml.cs
@@ -4,22 +4,21 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using VisualAcademy.Models;
 
-namespace VisualAcademy.Pages.Cascading.Properties
+namespace VisualAcademy.Pages.Cascading.Properties;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly DotNetNote.Data.ApplicationDbContext _context;
+
+    public IndexModel(DotNetNote.Data.ApplicationDbContext context)
     {
-        private readonly DotNetNote.Data.ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public IndexModel(DotNetNote.Data.ApplicationDbContext context)
-        {
-            _context = context;
-        }
+    public IList<Property> Property { get;set; }
 
-        public IList<Property> Property { get;set; }
-
-        public async Task OnGetAsync()
-        {
-            Property = await _context.Properties.ToListAsync();
-        }
+    public async Task OnGetAsync()
+    {
+        Property = await _context.Properties.ToListAsync();
     }
 }
