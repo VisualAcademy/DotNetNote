ac451229262ed734dfa2822834e3a75c6349045e
Park YongJune <redplus@redplus.net>
2022-10-11 00:11:33 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Pages/Cascading/Sublocations/Edit.cshtml.cs
+++ DotNetNote/DotNetNote/Pages/Cascading/Sublocations/Edit.cshtml.cs
@@ -7,71 +7,70 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using VisualAcademy.Models;
 
-namespace VisualAcademy.Pages.Cascading.Sublocations
-{
-    public class EditModel : PageModel
-    {
-        private readonly DotNetNote.Data.ApplicationDbContext _context;
+namespace VisualAcademy.Pages.Cascading.Sublocations;
 
-        public EditModel(DotNetNote.Data.ApplicationDbContext context)
+public class EditModel : PageModel
+{
+    private readonly DotNetNote.Data.ApplicationDbContext _context;
+
+    public EditModel(DotNetNote.Data.ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [BindProperty]
+    public Sublocation Sublocation { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+        if (id == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        [BindProperty]
-        public Sublocation Sublocation { get; set; }
+        Sublocation = await _context.Sublocations
+            .Include(s => s.LocationRef).FirstOrDefaultAsync(m => m.Id == id);
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+        if (Sublocation == null)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            return NotFound();
+        }
+       ViewData["LocationId"] = new SelectList(_context.Locations, "Id", "Id");
+        return Page();
+    }
 
-            Sublocation = await _context.Sublocations
-                .Include(s => s.LocationRef).FirstOrDefaultAsync(m => m.Id == id);
-
-            if (Sublocation == null)
-            {
-                return NotFound();
-            }
-           ViewData["LocationId"] = new SelectList(_context.Locations, "Id", "Id");
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see https://aka.ms/RazorPagesCRUD.
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        _context.Attach(Sublocation).State = EntityState.Modified;
+
+        try
         {
-            if (!ModelState.IsValid)
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!SublocationExists(Sublocation.Id))
             {
-                return Page();
+                return NotFound();
             }
-
-            _context.Attach(Sublocation).State = EntityState.Modified;
-
-            try
+            else
             {
-                await _context.SaveChangesAsync();
+                throw;
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!SublocationExists(Sublocation.Id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return RedirectToPage("./Index");
         }
 
-        private bool SublocationExists(int id)
-        {
-            return _context.Sublocations.Any(e => e.Id == id);
-        }
+        return RedirectToPage("./Index");
+    }
+
+    private bool SublocationExists(int id)
+    {
+        return _context.Sublocations.Any(e => e.Id == id);
     }
 }
