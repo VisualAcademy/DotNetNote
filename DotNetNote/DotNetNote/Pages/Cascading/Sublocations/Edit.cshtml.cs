a9d688dfdcd313522ac8f912ed8d3bbd6685e1cf
Park YongJune <redplus@redplus.net>
2022-10-11 00:10:56 +0900

    Remove Unnecessary Usings

--- DotNetNote/DotNetNote/Pages/Cascading/Sublocations/Edit.cshtml.cs
+++ DotNetNote/DotNetNote/Pages/Cascading/Sublocations/Edit.cshtml.cs
@@ -1,13 +1,10 @@
 ﻿#nullable disable
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using DotNetNote.Data;
 using VisualAcademy.Models;
 
 namespace VisualAcademy.Pages.Cascading.Sublocations
