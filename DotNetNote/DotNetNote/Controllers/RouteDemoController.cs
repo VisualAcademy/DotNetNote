ad76a525c4a79e027ca86513a4765f0a355261a8
Park YongJune <redplus@redplus.net>
2022-10-17 13:03:04 +0900

    .

--- DotNetNote/DotNetNote/Controllers/RouteDemoController.cs
+++ DotNetNote/DotNetNote/Controllers/RouteDemoController.cs
@@ -2,10 +2,10 @@
 
 namespace DotNetNote.Controllers;
 
-// [Route()] 특성을 사용한 어트리뷰트 라우팅
+// [Route()] 특성을 사용한 특성(어트리뷰트, 애트리뷰트) 라우팅
 [Route("RouteDemo")]
 public class RouteDemoController
 {
     [Route(""), Route("Index")]
-    public string Index() => "어트리뷰트 라우팅";
+    public string Index() => "특성(어트리뷰트) 라우팅";
 }
