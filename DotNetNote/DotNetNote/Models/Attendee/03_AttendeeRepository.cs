63192f9cdaedfb4b001f5132835bf269b6efff7f
Park YongJune <redplus@redplus.net>
2022-10-18 12:26:35 +0900

    .

--- DotNetNote/DotNetNote/Models/Attendee/03_AttendeeRepository.cs
+++ DotNetNote/DotNetNote/Models/Attendee/03_AttendeeRepository.cs
@@ -10,6 +10,9 @@ public class AttendeeRepository : IAttendeeRepository
     private readonly IConfiguration _config;
     private SqlConnection con;
 
+    /// <summary>
+    /// 생성자의 매개 변수 주입 방식으로 Configuration 개체 주입
+    /// </summary>
     public AttendeeRepository(IConfiguration config)
     {
         _config = config;
@@ -18,6 +21,9 @@ public class AttendeeRepository : IAttendeeRepository
                 .GetSection("DefaultConnection").Value);
     }
 
+    /// <summary>
+    /// 생성자의 매개 변수로 데이터베이스 연결 문자열 직접 받기 
+    /// </summary>
     public AttendeeRepository(string connectionString) => con = new SqlConnection(connectionString);
 
     public void Add(Attendee model)
