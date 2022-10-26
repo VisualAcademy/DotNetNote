d870d570f7c881c285001c3fd619c2665813d073
Park YongJune <redplus@redplus.net>
2022-10-27 01:44:16 +0900

    Use expression body for methods

--- DotNetNote/DotNetNote/Areas/DotNetSale/Controllers/CategoryController.cs
+++ DotNetNote/DotNetNote/Areas/DotNetSale/Controllers/CategoryController.cs
@@ -5,11 +5,9 @@ namespace DotNetSale.Controllers
     [Area("DotNetSale")]
     public class CategoryController : Controller
     {
-        public IActionResult Index()
-        {
+        public IActionResult Index() =>
             //return RedirectToAction("CategoryList");
-            return RedirectToAction(nameof(CategoryList));
-        }
+            RedirectToAction(nameof(CategoryList));
 
         public IActionResult CategoryAdd() => View();
 
