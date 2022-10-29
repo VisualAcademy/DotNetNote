fba81d8c3e75eb1032a02a60ed32833847506a5b
Park YongJune <redplus@redplus.net>
2022-10-30 03:20:52 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Areas/DotNetSale/Controllers/HomeController.cs
+++ DotNetNote/DotNetNote/Areas/DotNetSale/Controllers/HomeController.cs
@@ -1,38 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace DotNetSale.Controllers
+namespace DotNetSale.Controllers;
+
+[Area("DotNetSale")]
+public class HomeController : Controller
 {
-    [Area("DotNetSale")]
-    public class HomeController : Controller
+    private ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger) => _logger = logger;
+
+    public IActionResult Index()
     {
-        private ILogger<HomeController> _logger;
+        _logger.LogInformation("HOME - Index 페이지가 로드되었습니다.");
 
-        public HomeController(ILogger<HomeController> logger) => _logger = logger;
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            _logger.LogInformation("HOME - Index 페이지가 로드되었습니다.");
+    public IActionResult About()
+    {
+        _logger.LogInformation("HOME - About 페이지가 로드되었습니다.");
 
-            return View();
-        }
+        ViewData["Message"] = "Your application description page.";
 
-        public IActionResult About()
-        {
-            _logger.LogInformation("HOME - About 페이지가 로드되었습니다.");
+        return View();
+    }
 
-            ViewData["Message"] = "Your application description page.";
+    public IActionResult Contact()
+    {
+        _logger.LogInformation("HOME - Contact 페이지가 로드되었습니다.");
 
-            return View();
-        }
+        ViewData["Message"] = "Your contact page.";
 
-        public IActionResult Contact()
-        {
-            _logger.LogInformation("HOME - Contact 페이지가 로드되었습니다.");
-
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
+        return View();
     }
 }
