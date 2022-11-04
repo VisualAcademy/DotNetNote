2603130cc8957776f363fcb981a5cc0aa126e815
Park YongJune <redplus@redplus.net>
2022-11-04 11:47:00 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Areas/DotNetSale/Models/CategoryRepositoryInMemory.cs
+++ DotNetNote/DotNetNote/Areas/DotNetSale/Models/CategoryRepositoryInMemory.cs
@@ -1,34 +1,33 @@
 ﻿using Microsoft.Data.SqlClient;
 
-namespace DotNetSale.Models
+namespace DotNetSale.Models;
+
+public class CategoryRepositoryInMemory : ICategoryRepository
 {
-    public class CategoryRepositoryInMemory : ICategoryRepository
+    public void AddCategory(string categoryName)
     {
-        public void AddCategory(string categoryName)
-        {
-            throw new System.NotImplementedException();
-        }
+        throw new System.NotImplementedException();
+    }
 
-        public List<Category> GetAll()
-        {
-            var categories = new List<Category>(){
-                new Category { CategoryId = 1, CategoryName = "책" },
-                new Category { CategoryId = 2, CategoryName = "강의" },
-                new Category { CategoryId = 3, CategoryName = "컴퓨터" },
-                new Category { CategoryId = 4, CategoryName = "생활용품" }
-            };
+    public List<Category> GetAll()
+    {
+        var categories = new List<Category>(){
+            new Category { CategoryId = 1, CategoryName = "책" },
+            new Category { CategoryId = 2, CategoryName = "강의" },
+            new Category { CategoryId = 3, CategoryName = "컴퓨터" },
+            new Category { CategoryId = 4, CategoryName = "생활용품" }
+        };
 
-            return categories;
-        }
+        return categories;
+    }
 
-        public List<Category> GetCategories()
-        {
-            throw new System.NotImplementedException();
-        }
+    public List<Category> GetCategories()
+    {
+        throw new System.NotImplementedException();
+    }
 
-        public SqlDataReader GetProductCategories()
-        {
-            throw new System.NotImplementedException();
-        }
+    public SqlDataReader GetProductCategories()
+    {
+        throw new System.NotImplementedException();
     }
 }
