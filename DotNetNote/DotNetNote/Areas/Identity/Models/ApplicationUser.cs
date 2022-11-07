d5abcfc3abb5def6079f097f0acf0295af77486a
Park YongJune <redplus@redplus.net>
2022-11-07 12:20:30 +0900

    ApplicationUser

---
+++ DotNetNote/DotNetNote/Areas/Identity/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DotNetNote.Areas.Identity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Timezone { get; set; }
+
+        // TODO: 
+
+    }
+}
