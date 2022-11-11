730b23711ef146a5478555c51911cda3aa706311
Park YongJune <redplus@redplus.net>
2022-11-11 18:48:57 +0900

    ApplicationRole

---
+++ DotNetNote/DotNetNote/Areas/Identity/Models/ApplicationRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace DotNetNote.Areas.Identity.Models
+{
+    public class ApplicationRole : IdentityRole
+    {
+        public string? Description { get; set; }
+    }
+}
