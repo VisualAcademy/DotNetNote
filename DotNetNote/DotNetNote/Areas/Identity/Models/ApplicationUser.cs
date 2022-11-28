2baa072eed0dc4239019db53982235db4964d123
Park YongJune <redplus@redplus.net>
2022-11-29 03:24:24 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Areas/Identity/Models/ApplicationUser.cs
+++ DotNetNote/DotNetNote/Areas/Identity/Models/ApplicationUser.cs
@@ -1,16 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
-namespace DotNetNote.Areas.Identity.Models
+namespace DotNetNote.Areas.Identity.Models;
+
+public class ApplicationUser : IdentityUser
 {
-    public class ApplicationUser : IdentityUser
-    {
-        public string? FirstName { get; set; }
+    public string? FirstName { get; set; }
 
-        public string? LastName { get; set; }
+    public string? LastName { get; set; }
 
-        public string? Timezone { get; set; }
+    public string? Timezone { get; set; }
 
-        // TODO: 
+    // TODO: 
 
-    }
 }
