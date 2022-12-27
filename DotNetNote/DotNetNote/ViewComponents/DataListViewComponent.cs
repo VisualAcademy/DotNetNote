1f777a21c9a01df42ccc20193b11799a96367b2d
Park YongJune <redplus@redplus.net>
2022-12-28 01:39:54 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/ViewComponents/DataListViewComponent.cs
+++ DotNetNote/DotNetNote/ViewComponents/DataListViewComponent.cs
@@ -1,28 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
-namespace DotNetNote.ViewComponents
+namespace DotNetNote.ViewComponents;
+
+public class DataListViewComponent : ViewComponent
 {
-    public class DataListViewComponent : ViewComponent
+    public async Task<IViewComponentResult> InvokeAsync(string name)
     {
-        public async Task<IViewComponentResult> InvokeAsync(string name)
-        {
-            var data = await GetByNameAsync(name);
-            return View(data);
-        }
+        var data = await GetByNameAsync(name);
+        return View(data);
+    }
 
-        /// <summary>
-        /// 비동기 메서드 
-        /// </summary>
-        private Task<IEnumerable<DataModel>> GetByNameAsync(string name) => Task.FromResult(GetByName(name));
+    /// <summary>
+    /// 비동기 메서드 
+    /// </summary>
+    private Task<IEnumerable<DataModel>> GetByNameAsync(string name) => Task.FromResult(GetByName(name));
 
-        /// <summary>
-        /// 동기 메서드
-        /// </summary>
-        private IEnumerable<DataModel> GetByName(string name)
-        {
-            DataService service = new DataService();
-            return service.GetDataByName(name);
-        }
+    /// <summary>
+    /// 동기 메서드
+    /// </summary>
+    private IEnumerable<DataModel> GetByName(string name)
+    {
+        DataService service = new DataService();
+        return service.GetDataByName(name);
     }
 }
