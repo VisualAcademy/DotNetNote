c310241b0ddb391ab4175a6ea14a02cb157a8337
Park YongJune <redplus@redplus.net>
2022-12-29 11:30:08 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Models/CabinetTypes/CabinetType.cs
+++ DotNetNote/DotNetNote/Models/CabinetTypes/CabinetType.cs
@@ -1,16 +1,15 @@
-﻿namespace DotNetNote.Models
+﻿namespace DotNetNote.Models;
+
+/// <summary>
+/// 캐비넷 타입 관리 모델 클래스 
+/// </summary>
+public class CabinetType
 {
-    /// <summary>
-    /// 캐비넷 타입 관리 모델 클래스 
-    /// </summary>
-    public class CabinetType
-    {
-        public long Id { get; set; }
+    public long Id { get; set; }
 
-        public string? Identification { get; set; } = null!;
+    public string? Identification { get; set; } = null!;
 
-        public bool? Show { get; set; }
+    public bool? Show { get; set; }
 
-        public bool Adjusted { get; set; } = false;
-    }
+    public bool Adjusted { get; set; } = false;
 }
