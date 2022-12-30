be1b3de4a8cb6f3e39db365d7bc5bf97c6d80b94
Park YongJune <redplus@redplus.net>
2022-12-31 08:35:12 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Models/ExamManager/Choice.cs
+++ DotNetNote/DotNetNote/Models/ExamManager/Choice.cs
@@ -1,17 +1,16 @@
-﻿namespace DotNetNote.Models
-{
-    public class Choice
-    {
-        public int Id { get; set; }
-        public string ChoiceText { get; set; }
-        public bool IsAnswer { get; set; }
-        public bool IsSelected { get; set; }
+﻿namespace DotNetNote.Models;
 
-        private Question _question = new Question();
-        public Question Question
-        {
-            get { return _question; }
-            set { _question = value;}
-        }
+public class Choice
+{
+    public int Id { get; set; }
+    public string ChoiceText { get; set; }
+    public bool IsAnswer { get; set; }
+    public bool IsSelected { get; set; }
+
+    private Question _question = new Question();
+    public Question Question
+    {
+        get { return _question; }
+        set { _question = value;}
     }
 }
