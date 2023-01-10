208835676ba92c2e5f8819bbd392b5bc1df93cba
Park YongJune <redplus@redplus.net>
2023-01-10 23:58:47 +0900

    .

--- DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/Disable2fa.cshtml.cs
+++ DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/Disable2fa.cshtml.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using DotNetNote.Models;
+using System;
+using System.Threading.Tasks;
 
 namespace VisualAcademy.Areas.Identity.Pages.Account.Manage
 {
