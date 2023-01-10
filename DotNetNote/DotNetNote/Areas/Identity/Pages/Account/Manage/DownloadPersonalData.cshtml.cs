418d120e16266acf5c9cf2cccf3ac34785e5e649
Park YongJune <redplus@redplus.net>
2023-01-11 00:02:27 +0900

    .

--- DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/DownloadPersonalData.cshtml.cs
+++ DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/DownloadPersonalData.cshtml.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using DotNetNote.Models;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace VisualAcademy.Areas.Identity.Pages.Account.Manage
 {
