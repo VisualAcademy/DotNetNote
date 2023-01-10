f3ff488aed63772cd9bb97f2e02580e95dee2a59
Park YongJune <redplus@redplus.net>
2023-01-11 00:02:55 +0900

    .

--- DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
+++ DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.Encodings.Web;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using DotNetNote.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
 
 namespace VisualAcademy.Areas.Identity.Pages.Account.Manage
 {
