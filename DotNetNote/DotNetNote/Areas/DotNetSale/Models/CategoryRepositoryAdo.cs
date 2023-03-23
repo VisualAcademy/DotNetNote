c0e46a8b9662282cb3d109bf01aa0d017d4f261e
Park YongJune <redplus@redplus.net>
2023-03-23 21:31:28 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Areas/DotNetSale/Models/CategoryRepositoryAdo.cs
+++ DotNetNote/DotNetNote/Areas/DotNetSale/Models/CategoryRepositoryAdo.cs
@@ -1,28 +1,27 @@
 ﻿using Microsoft.Data.SqlClient;
 using System;
 
-namespace DotNetSale.Models
+namespace DotNetSale.Models;
+
+public class CategoryRepositoryAdo : ICategoryRepository
 {
-    public class CategoryRepositoryAdo : ICategoryRepository
+    public void AddCategory(string categoryName)
     {
-        public void AddCategory(string categoryName)
-        {
-            throw new NotImplementedException();
-        }
+        throw new NotImplementedException();
+    }
 
-        public List<Category> GetAll()
-        {
-            throw new NotImplementedException();
-        }
+    public List<Category> GetAll()
+    {
+        throw new NotImplementedException();
+    }
 
-        public List<Category> GetCategories()
-        {
-            throw new NotImplementedException();
-        }
+    public List<Category> GetCategories()
+    {
+        throw new NotImplementedException();
+    }
 
-        public SqlDataReader GetProductCategories()
-        {
-            throw new NotImplementedException();
-        }
+    public SqlDataReader GetProductCategories()
+    {
+        throw new NotImplementedException();
     }
 }
