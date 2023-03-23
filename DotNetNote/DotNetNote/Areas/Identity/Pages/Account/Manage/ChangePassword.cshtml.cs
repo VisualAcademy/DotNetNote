ee6f44172d24b018cff1ea2054a423ba9a7fec7e
Park YongJune <redplus@redplus.net>
2023-03-23 21:32:21 +0900

    .

--- DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+
 namespace VisualAcademy.Areas.Identity.Pages.Account.Manage
 {
     public class ChangePasswordModel : PageModel
