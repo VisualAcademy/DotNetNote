81f9c12d8a052532371d1e1b01eb4c027d681378
Park YongJune <redplus@redplus.net>
2023-12-12 18:33:26 +0900

    .

--- DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/DownloadPersonalData.cshtml.cs
+++ DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/DownloadPersonalData.cshtml.cs
@@ -42,7 +42,9 @@ namespace VisualAcademy.Areas.Identity.Pages.Account.Manage
                 personalData.Add(p.Name, p.GetValue(user)?.ToString() ?? "null");
             }
 
-            Response.Headers.Add("Content-Disposition", "attachment; filename=PersonalData.json");
+            //Response.Headers.Add("Content-Disposition", "attachment; filename=PersonalData.json");
+            Response.Headers["Content-Disposition"] = "attachment; filename=PersonalData.json";
+
             return new FileContentResult(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(personalData)), "text/json");
         }
     }
