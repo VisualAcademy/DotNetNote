3150fca0469ba2f0cfa583bf9cd36d3c02351a71
Park YongJune <redplus@redplus.net>
2023-12-25 11:16:51 +0900

    Use expression body for methods

--- DotNetNote/DotNetNote/Controllers/DotNetNote/NoteServiceController.cs
+++ DotNetNote/DotNetNote/Controllers/DotNetNote/NoteServiceController.cs
@@ -7,10 +7,8 @@ namespace DotNetNote.Controllers;
 public class NoteServiceController(INoteRepository repository) : Controller
 {
     [HttpGet]
-    public IEnumerable<Note> Get()
-    {
+    public IEnumerable<Note> Get() =>
         // 최근 글 리스트 반환
         //return _repository.GetRecentPosts();      // 캐싱 적용 전
-        return repository.GetRecentPostsCache();   // 캐싱 적용 후
-    }
+        repository.GetRecentPostsCache();   // 캐싱 적용 후
 }
