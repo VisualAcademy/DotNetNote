2a1e3092ac88f577df168d9df50c5fb9fabee7fe
Park YongJune <redplus@redplus.net>
2023-12-25 11:16:19 +0900

    Use primary constructor (and remove fields)

--- DotNetNote/DotNetNote/Controllers/DotNetNote/NoteServiceController.cs
+++ DotNetNote/DotNetNote/Controllers/DotNetNote/NoteServiceController.cs
@@ -4,17 +4,13 @@ using Microsoft.AspNetCore.Mvc;
 namespace DotNetNote.Controllers;
 
 [Route("api/[controller]")]
-public class NoteServiceController : Controller
+public class NoteServiceController(INoteRepository repository) : Controller
 {
-    private readonly INoteRepository _repository;
-
-    public NoteServiceController(INoteRepository repository) => _repository = repository;
-
     [HttpGet]
     public IEnumerable<Note> Get()
     {
         // 최근 글 리스트 반환
         //return _repository.GetRecentPosts();      // 캐싱 적용 전
-        return _repository.GetRecentPostsCache();   // 캐싱 적용 후
+        return repository.GetRecentPostsCache();   // 캐싱 적용 후
     }
 }
