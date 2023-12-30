e81f8533d53a3fa513f7b2cc2cbe1b42a591fc49
Park YongJune <redplus@redplus.net>
2023-12-30 17:29:31 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote.Models/Categories/CategoryRepositorySqlServer.cs
+++ DotNetNote/DotNetNote.Models/Categories/CategoryRepositorySqlServer.cs
@@ -1,19 +1,18 @@
 ﻿using System.Collections.Generic;
 
-namespace DotNetNote.Models.Categories
-{
-    public class CategoryRepositorySqlServer : ICategoryRepository
-    {
-        public List<Category> GetCategories()
-        {
-            var categories = new List<Category>()
-            {
-                new Category() { CategoryId = 1, CategoryName = "[DB에서] 좋은 책" },
-                new Category() { CategoryId = 2, CategoryName = "[DB에서] 좋은 강의" },
-                new Category() { CategoryId = 3, CategoryName = "[DB에서] 좋은 컴퓨터" }
-            };
+namespace DotNetNote.Models.Categories;
 
-            return categories;
-        }
+public class CategoryRepositorySqlServer : ICategoryRepository
+{
+    public List<Category> GetCategories()
+    {
+        var categories = new List<Category>()
+        {
+            new Category() { CategoryId = 1, CategoryName = "[DB에서] 좋은 책" },
+            new Category() { CategoryId = 2, CategoryName = "[DB에서] 좋은 강의" },
+            new Category() { CategoryId = 3, CategoryName = "[DB에서] 좋은 컴퓨터" }
+        };
+
+        return categories;
     }
 }
