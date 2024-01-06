54af231c396042eae7e66bbcbf57c2c0113e54e0
Park YongJune <redplus@redplus.net>
2024-01-07 01:28:02 +0900

    .

--- DotNetNote/DotNetNote/Apis/Cascading/LocationsController.cs
+++ DotNetNote/DotNetNote/Apis/Cascading/LocationsController.cs
@@ -10,26 +10,22 @@ namespace VisualAcademy.Apis;
 
 [Route("api/[controller]")]
 [ApiController]
-public class LocationsController : ControllerBase
+public class LocationsController(ApplicationDbContext context) : ControllerBase
 {
-    private readonly ApplicationDbContext _context;
-
-    public LocationsController(ApplicationDbContext context) => _context = context;
-
     // GET: api/Locations
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Location>>> GetLocations() => await _context.Locations.ToListAsync();
+    public async Task<ActionResult<IEnumerable<Location>>> GetLocations() => await context.Locations.ToListAsync();
 
     [HttpGet("Properties/{parentId}")]
     public async Task<ActionResult<IEnumerable<Location>>> GetLocationsByProperty(int parentId) =>
         // 특정 Property에 해당하는 Locations 정보만 읽어오기 
-        await _context.Locations.Where(l => l.PropertyId == parentId).ToListAsync();
+        await context.Locations.Where(l => l.PropertyId == parentId).ToListAsync();
 
     // GET: api/Locations/5
     [HttpGet("{id}")]
     public async Task<ActionResult<Location>> GetLocation(int id)
     {
-        var location = await _context.Locations.FindAsync(id);
+        var location = await context.Locations.FindAsync(id);
 
         if (location == null)
         {
@@ -49,11 +45,11 @@ public class LocationsController : ControllerBase
             return BadRequest();
         }
 
-        _context.Entry(location).State = EntityState.Modified;
+        context.Entry(location).State = EntityState.Modified;
 
         try
         {
-            await _context.SaveChangesAsync();
+            await context.SaveChangesAsync();
         }
         catch (DbUpdateConcurrencyException)
         {
@@ -75,8 +71,8 @@ public class LocationsController : ControllerBase
     [HttpPost]
     public async Task<ActionResult<Location>> PostLocation(Location location)
     {
-        _context.Locations.Add(location);
-        await _context.SaveChangesAsync();
+        context.Locations.Add(location);
+        await context.SaveChangesAsync();
 
         return CreatedAtAction("GetLocation", new { id = location.Id }, location);
     }
@@ -85,17 +81,17 @@ public class LocationsController : ControllerBase
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteLocation(int id)
     {
-        var location = await _context.Locations.FindAsync(id);
+        var location = await context.Locations.FindAsync(id);
         if (location == null)
         {
             return NotFound();
         }
 
-        _context.Locations.Remove(location);
-        await _context.SaveChangesAsync();
+        context.Locations.Remove(location);
+        await context.SaveChangesAsync();
 
         return NoContent();
     }
 
-    private bool LocationExists(int id) => _context.Locations.Any(e => e.Id == id);
+    private bool LocationExists(int id) => context.Locations.Any(e => e.Id == id);
 }
