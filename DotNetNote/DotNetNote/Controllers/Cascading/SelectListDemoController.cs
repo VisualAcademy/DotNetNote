f253894d7335ba8f6611e8697b65fdf32560c965
Park YongJune <redplus@redplus.net>
2024-01-19 08:44:03 +0900

    Use primary constructor (and remove fields)

--- DotNetNote/DotNetNote/Controllers/Cascading/SelectListDemoController.cs
+++ DotNetNote/DotNetNote/Controllers/Cascading/SelectListDemoController.cs
@@ -5,20 +5,16 @@ using System.Linq;
 
 namespace DotNetNote.Controllers.Cascading;
 
-public class SelectListDemoController : Controller
+public class SelectListDemoController(ApplicationDbContext context) : Controller
 {
-    private readonly ApplicationDbContext _context;
-
-    public SelectListDemoController(ApplicationDbContext context) => _context = context;
-
     public IActionResult Index()
     {
         ViewData["PropertyId"] =
-            new SelectList(_context.Properties.OrderBy(it => it.Name), "Id", "Name");
+            new SelectList(context.Properties.OrderBy(it => it.Name), "Id", "Name");
         ViewData["LocationId"] =
-            new SelectList(_context.Locations.OrderBy(it => it.Name), "Id", "Name");
+            new SelectList(context.Locations.OrderBy(it => it.Name), "Id", "Name");
         ViewData["SublocationId"] =
-            new SelectList(_context.Sublocations.OrderBy(it => it.SublocationName), "Id", "SublocationName");
+            new SelectList(context.Sublocations.OrderBy(it => it.SublocationName), "Id", "SublocationName");
 
         return View();
     }
