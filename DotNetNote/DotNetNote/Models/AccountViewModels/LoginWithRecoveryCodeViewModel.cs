158ab0938f0995bb93b422822ec6d1ad694f15a2
Park YongJune <redplus@redplus.net>
2024-01-21 10:51:49 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Models/AccountViewModels/LoginWithRecoveryCodeViewModel.cs
+++ DotNetNote/DotNetNote/Models/AccountViewModels/LoginWithRecoveryCodeViewModel.cs
@@ -1,12 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace DotNetNote.Models.AccountViewModels
+namespace DotNetNote.Models.AccountViewModels;
+
+public class LoginWithRecoveryCodeViewModel
 {
-    public class LoginWithRecoveryCodeViewModel
-    {
-        [Required]
-        [DataType(DataType.Text)]
-        [Display(Name = "Recovery Code")]
-        public string RecoveryCode { get; set; }
-    }
+    [Required]
+    [DataType(DataType.Text)]
+    [Display(Name = "Recovery Code")]
+    public string RecoveryCode { get; set; }
 }
