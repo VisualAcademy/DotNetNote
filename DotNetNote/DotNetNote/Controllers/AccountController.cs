2c8eb9c8e7a1e63b3e12cbf0da2d535fcea7cb93
Park YongJune <redplus@redplus.net>
2024-01-25 14:31:35 +0900

    Global Usings

--- DotNetNote/DotNetNote/Controllers/AccountController.cs
+++ DotNetNote/DotNetNote/Controllers/AccountController.cs
@@ -1,16 +1,4 @@
-﻿using DotNetNote.Data;
-using DotNetNote.Models.AccountViewModels;
-using DotNetNote.Services;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Security.Claims;
-using System.Threading.Tasks;
-
-namespace DotNetNote.Controllers;
+﻿namespace DotNetNote.Controllers;
 
 [Authorize]
 [Route("[controller]/[action]")]
