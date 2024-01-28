dd77522b8903955220b3100034f6b588a620585c
Park YongJune <redplus@redplus.net>
2024-01-28 18:56:02 +0900

    Use primary constructor (and remove fields)

--- DotNetNote/DotNetNote/Controllers/AppSettingsDemo.cs
+++ DotNetNote/DotNetNote/Controllers/AppSettingsDemo.cs
@@ -3,22 +3,17 @@
 namespace DotNetNote.Controllers;
 
 [Authorize(Roles = "Administrators")]
-public class AppSettingsDemo : Controller
+public class AppSettingsDemo(IConfiguration configuration) : Controller
 {
-    // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-5.0
-    private readonly IConfiguration _configuration;
-
-    public AppSettingsDemo(IConfiguration configuration) => this._configuration = configuration;
-
     public IActionResult Index()
     {
-        string con1 = _configuration.GetSection("StorageConnectionString1").Value;
+        string con1 = configuration.GetSection("StorageConnectionString1").Value;
 
-        string site1 = _configuration
+        string site1 = configuration
             .GetSection("BlogStorageConnectionString")
             .GetSection("Site1").Value;
 
-        string site2 = _configuration
+        string site2 = configuration
             .GetValue<string>("BlogStorageConnectionString:Site2");
 
         return Content($"{con1}, {site1}, {site2}");
