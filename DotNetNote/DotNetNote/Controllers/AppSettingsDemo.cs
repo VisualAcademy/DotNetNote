cc56af621b13e78415d63599aeee2c1599d91280
Park YongJune <redplus@redplus.net>
2024-01-28 19:01:26 +0900

    .

--- DotNetNote/DotNetNote/Controllers/AppSettingsDemo.cs
+++ DotNetNote/DotNetNote/Controllers/AppSettingsDemo.cs
@@ -10,11 +10,11 @@ public class AppSettingsDemo(IConfiguration configuration) : Controller
         string con1 = configuration.GetSection("StorageConnectionString1").Value;
 
         string site1 = configuration
-            .GetSection("BlogStorageConnectionString")
+            .GetSection("BlobStorageConnectionString")
             .GetSection("Site1").Value;
 
         string site2 = configuration
-            .GetValue<string>("BlogStorageConnectionString:Site2");
+            .GetValue<string>("BlobStorageConnectionString:Site2");
 
         return Content($"{con1}, {site1}, {site2}");
     }
