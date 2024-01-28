4f9167d13e81b1c1b05aa26622d173f993648f38
Park YongJune <redplus@redplus.net>
2024-01-28 15:56:55 +0900

    .

--- DotNetNote/DotNetNote/Controllers/VariableTestController.cs
+++ DotNetNote/DotNetNote/Controllers/VariableTestController.cs
@@ -1,8 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace DotNetNote.Controllers;
+﻿namespace DotNetNote.Controllers;
 
 public class VariableTestController(IVariableRepository repository) : Controller
 {
