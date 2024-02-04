3e8bf24a1d5298ca76c82f8bba607cf46b8fce95
Park YongJune <redplus@redplus.net>
2024-02-04 13:20:04 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Models/Constants.cs
+++ DotNetNote/DotNetNote/Models/Constants.cs
@@ -1,21 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace DotNetNote.Models
-{
-    /// <summary>
-    /// 공통 상수 저장소
-    /// </summary>
-    public partial class Constants
-    {
-        public const string DotNetNoteSiteUrl = "http://www.dotnetnote.com";
-    }
+namespace DotNetNote.Models;
 
-    public partial class Constants
+/// <summary>
+/// 공통 상수 저장소
+/// </summary>
+public partial class Constants
+{
+    public const string DotNetNoteSiteUrl = "http://www.dotnetnote.com";
+}
+
+public partial class Constants
+{
+    public static List<SelectListItem> YesOrNo = new List<SelectListItem>
     {
-        public static List<SelectListItem> YesOrNo = new List<SelectListItem>
-        {
-            new SelectListItem() { Value = "Yes", Text = "Yes" },
-            new SelectListItem() { Value = "No", Text = "No" }
-        };
-    }
+        new SelectListItem() { Value = "Yes", Text = "Yes" },
+        new SelectListItem() { Value = "No", Text = "No" }
+    };
 }
