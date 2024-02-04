247f296afe73c0fe1885449d658eafa9fe57a4b9
Park YongJune <redplus@redplus.net>
2024-02-05 00:50:27 +0900

    Use primary constructor

--- DotNetNote/DotNetNote/Data/ApplicationDbContext.cs
+++ DotNetNote/DotNetNote/Data/ApplicationDbContext.cs
@@ -2,13 +2,8 @@
 
 namespace DotNetNote.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
-    {
-    }
-
     public DbSet<CabinetType> CabinetTypes { get; set; } = null!;
 
     #region Cascading DropDownList 
