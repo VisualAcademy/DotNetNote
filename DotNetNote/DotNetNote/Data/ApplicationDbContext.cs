1a4d3c6ce1c424ed493b4a92bc159ca445d74647
Park YongJune <redplus@redplus.net>
2024-02-05 00:50:04 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Data/ApplicationDbContext.cs
+++ DotNetNote/DotNetNote/Data/ApplicationDbContext.cs
@@ -1,20 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-namespace DotNetNote.Data
+namespace DotNetNote.Data;
+
+public class ApplicationDbContext : IdentityDbContext
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<CabinetType> CabinetTypes { get; set; } = null!;
-
-        #region Cascading DropDownList 
-        public DbSet<Property> Properties { get; set; } = null!;
-        public DbSet<Location> Locations { get; set; } = null!;
-        public DbSet<Sublocation> Sublocations { get; set; } = null!; 
-        #endregion
     }
+
+    public DbSet<CabinetType> CabinetTypes { get; set; } = null!;
+
+    #region Cascading DropDownList 
+    public DbSet<Property> Properties { get; set; } = null!;
+    public DbSet<Location> Locations { get; set; } = null!;
+    public DbSet<Sublocation> Sublocations { get; set; } = null!; 
+    #endregion
 }
