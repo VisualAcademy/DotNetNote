6d00ea8d68f7f9b3b007c25def80caf6d1323236
Park YongJune <redplus@redplus.net>
2024-02-05 00:50:49 +0900

    Remove Unnecessary Usings

--- DotNetNote/DotNetNote/Data/ApplicationUser.cs
+++ DotNetNote/DotNetNote/Data/ApplicationUser.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace DotNetNote.Data
+﻿namespace DotNetNote.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
