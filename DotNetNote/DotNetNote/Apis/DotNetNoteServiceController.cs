7e31759988bb888549228e7175aa64f9f48cb876
Park YongJune <redplus@redplus.net>
2024-02-05 08:22:32 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Apis/DotNetNoteServiceController.cs
+++ DotNetNote/DotNetNote/Apis/DotNetNoteServiceController.cs
@@ -1,29 +1,28 @@
 ﻿using DotNetNote.Models.Notes;
 
-namespace DotNetNote.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class DotNetNoteServiceController(INoteRepository repository) : ControllerBase
-    {
-        [HttpPost("Delete/{id}")]
-        public IActionResult DeletePost(int id, [FromBody] DeleteViewModel model)
-        {
-            if (model == null || string.IsNullOrWhiteSpace(model.Password))
-            {
-                return BadRequest(new { Message = "비밀번호가 제공되지 않았습니다." });
-            }
+namespace DotNetNote.Controllers;
 
-            if (repository.DeleteNote(id, model.Password) > 0)
-            {
-                return Ok(new { Message = "데이터가 삭제되었습니다." });
-            }
-            else
-            {
-                return BadRequest(new { Message = "삭제되지 않았습니다. 비밀번호를 확인하세요." });
-            }
+[Route("api/[controller]")]
+[ApiController]
+public class DotNetNoteServiceController(INoteRepository repository) : ControllerBase
+{
+    [HttpPost("Delete/{id}")]
+    public IActionResult DeletePost(int id, [FromBody] DeleteViewModel model)
+    {
+        if (model == null || string.IsNullOrWhiteSpace(model.Password))
+        {
+            return BadRequest(new { Message = "비밀번호가 제공되지 않았습니다." });
+        }
+
+        if (repository.DeleteNote(id, model.Password) > 0)
+        {
+            return Ok(new { Message = "데이터가 삭제되었습니다." });
+        }
+        else
+        {
+            return BadRequest(new { Message = "삭제되지 않았습니다. 비밀번호를 확인하세요." });
         }
     }
-
-    public record DeleteViewModel(string Password);
 }
+
+public record DeleteViewModel(string Password);
