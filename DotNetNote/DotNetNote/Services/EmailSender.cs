1d29dd878d7656d41aaf5682d438adc52c30c409
Park YongJune <redplus@redplus.net>
2024-02-08 17:28:30 +0900

    Remove Unnecessary Usings

--- DotNetNote/DotNetNote/Services/EmailSender.cs
+++ DotNetNote/DotNetNote/Services/EmailSender.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading.Tasks;
-
-namespace DotNetNote.Services;
+﻿namespace DotNetNote.Services;
 
 // This class is used by the application to send email for account confirmation and password reset.
 // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
