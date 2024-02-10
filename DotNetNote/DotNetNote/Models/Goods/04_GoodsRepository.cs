73aba0cbb0faf6363e5ff170e2d734c4d9c18554
Park YongJune <redplus@redplus.net>
2024-02-10 19:30:27 +0900

    Remove Unnecessary Usings

--- DotNetNote/DotNetNote/Models/Goods/04_GoodsRepository.cs
+++ DotNetNote/DotNetNote/Models/Goods/04_GoodsRepository.cs
@@ -1,8 +1,4 @@
-﻿using Dapper;
-using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Configuration;
-using System.Data;
-using System.Linq;
+﻿using Microsoft.Extensions.Configuration;
 
 namespace DotNetNote.Models;
 
