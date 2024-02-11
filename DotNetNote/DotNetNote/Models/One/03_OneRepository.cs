6b934aac93236598fbb1142917c309344c4d6b25
Park YongJune <redplus@redplus.net>
2024-02-12 06:55:12 +0900

    Remove Unnecessary Usings

--- DotNetNote/DotNetNote/Models/One/03_OneRepository.cs
+++ DotNetNote/DotNetNote/Models/One/03_OneRepository.cs
@@ -1,9 +1,4 @@
-﻿using Dapper;
-using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Configuration;
-using System.Linq;
-
-namespace DotNetNote.Models;
+﻿namespace DotNetNote.Models;
 
 /// <summary>
 /// 리포지토리 클래스 
