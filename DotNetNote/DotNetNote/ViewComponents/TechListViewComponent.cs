2d6cc5cd55082b5d8b137c420b0d7745c7618090
Park YongJune <redplus@redplus.net>
2024-02-13 06:39:14 +0900

    Use 'new(...)'

--- DotNetNote/DotNetNote/ViewComponents/TechListViewComponent.cs
+++ DotNetNote/DotNetNote/ViewComponents/TechListViewComponent.cs
@@ -8,11 +8,11 @@ public class TechListViewComponent : ViewComponent
         {
             new() { Id = 1, Title = "ASP.NET Core" },
             new Tech { Id = 2, Title = "Bootstrap" },
-            new Tech { Id = 3, Title = "C#" },
+            new() { Id = 3, Title = "C#" },
             new Tech { Id = 4, Title = "Dapper" },
-            new Tech { Id = 5, Title = "Azure" },
+            new() { Id = 5, Title = "Azure" },
             new Tech { Id = 6, Title = "jQuery" },
-            new Tech { Id = 7, Title = "Angular" }
+            new() { Id = 7, Title = "Angular" }
         };
 
         return View(techLists);
