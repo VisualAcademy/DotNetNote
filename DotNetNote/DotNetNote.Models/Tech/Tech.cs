b0ae0b7bcf84afe0021417e3cef228db603501a4
Park YongJune <redplus@redplus.net>
2024-02-13 06:17:59 +0900

    .

--- DotNetNote/DotNetNote.Models/Tech/Tech.cs
+++ DotNetNote/DotNetNote.Models/Tech/Tech.cs
@@ -1,11 +1,10 @@
-﻿namespace DotNetNote.Models
+﻿namespace DotNetNote.Models;
+
+/// <summary>
+/// Teches 테이블과 일대일로 연결되는 클래스
+/// </summary>
+public class Tech
 {
-    /// <summary>
-    /// Teches 테이블과 일대일로 연결되는 클래스
-    /// </summary>
-    public class Tech
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-    }
+    public int Id { get; set; }
+    public string Title { get; set; }
 }
