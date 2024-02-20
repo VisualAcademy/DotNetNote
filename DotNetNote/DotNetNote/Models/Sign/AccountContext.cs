105203a80a76b6d05d2930d92195cf14ab36106c
Park YongJune <redplus@redplus.net>
2024-02-21 08:24:35 +0900

    Remove unnecessary usings

--- DotNetNote/DotNetNote/Models/Sign/AccountContext.cs
+++ DotNetNote/DotNetNote/Models/Sign/AccountContext.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-
-namespace DotNetNote
+﻿namespace DotNetNote
 {
     public class AccountContext(DbContextOptions<AccountContext> options) : DbContext(options)
     {
