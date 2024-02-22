6506035be17aafa5d8c9bc6fa02d2305581e6333
Park YongJune <redplus@redplus.net>
2024-02-22 13:11:57 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Models/Samples/Department_Employee.cs
+++ DotNetNote/DotNetNote/Models/Samples/Department_Employee.cs
@@ -1,30 +1,29 @@
-﻿namespace DotNetNote.Models.Samples
+﻿namespace DotNetNote.Models.Samples;
+
+public class Department_Employee
 {
-    public class Department_Employee
-    {
-        // Empty
-    }
+    // Empty
+}
 
-    public class Department
-    {
-        public int Id { get; set; }
+public class Department
+{
+    public int Id { get; set; }
 
-        public string Name { get; set; }
+    public string Name { get; set; }
 
-        public ICollection<Employee> Type { get; set; }
-    }
+    public ICollection<Employee> Type { get; set; }
+}
 
-    public class Employee
-    {
-        public virtual int Id { get; set; }
+public class Employee
+{
+    public virtual int Id { get; set; }
 
-        public virtual string Name { get; set; }
-    }
+    public virtual string Name { get; set; }
+}
 
-    public interface IDepartmentDataSource
-    {
-        IQueryable<Employee> Employees { get; }
+public interface IDepartmentDataSource
+{
+    IQueryable<Employee> Employees { get; }
 
-        IQueryable<Department> Departments { get; }
-    }
+    IQueryable<Department> Departments { get; }
 }
