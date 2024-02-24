0759d6d58ed6a35bdb7ee691de7f4ec997a522e0
Park YongJune <redplus@redplus.net>
2024-02-24 19:33:37 +0900

    .

---
+++ DotNetNote/DotNetNote/Models/UserInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNetNote.Models
+{
+    public sealed class UserInfo
+    {
+        public required string UserId { get; set; }
+        public required string Name { get; set; }
+
+        public const string UserIdClaimType = "sub";
+        public const string NameClaimType = "name";
+    }
+}
