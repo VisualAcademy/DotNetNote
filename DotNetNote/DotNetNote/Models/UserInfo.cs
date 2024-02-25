b686a195f04247fad55ed8eafd99423d41128833
Park YongJune <redplus@redplus.net>
2024-02-25 20:01:13 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Models/UserInfo.cs
+++ DotNetNote/DotNetNote/Models/UserInfo.cs
@@ -1,11 +1,10 @@
-﻿namespace DotNetNote.Models
-{
-    public sealed class UserInfo
-    {
-        public required string UserId { get; set; }
-        public required string Name { get; set; }
+﻿namespace DotNetNote.Models;
 
-        public const string UserIdClaimType = "sub";
-        public const string NameClaimType = "name";
-    }
+public sealed class UserInfo
+{
+    public required string UserId { get; set; }
+    public required string Name { get; set; }
+
+    public const string UserIdClaimType = "sub";
+    public const string NameClaimType = "name";
 }
