d26a4531679d6abfecb5ffb2b26a7b3b4dc774ee
Park YongJune <redplus@redplus.net>
2024-02-27 13:30:27 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote.Models/TechContext.cs
+++ DotNetNote/DotNetNote.Models/TechContext.cs
@@ -1,44 +1,43 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Configuration;
 
-namespace DotNetNote.Models
+namespace DotNetNote.Models;
+
+public class TechContext : DbContext
 {
-    public class TechContext : DbContext
+    public TechContext()
     {
-        public TechContext()
+        // Empty
+    }
+
+    public TechContext(DbContextOptions<TechContext> options)
+        : base(options)
+    {
+        // 공식과 같은 코드 
+    }
+
+    protected override void OnConfiguring(
+        DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
         {
-            // Empty
+            string connectionString = ConfigurationManager
+                .ConnectionStrings["ConnectionString"].ConnectionString;
+            optionsBuilder.UseSqlServer(connectionString);
         }
+    }
 
-        public TechContext(DbContextOptions<TechContext> options)
-            : base(options)
-        {
-            // 공식과 같은 코드 
-        }
+    /// <summary>
+    /// 기술 리스트: [실습] Teches 테이블부터 Angular 앱 또는 Blazor 앱까지
+    /// </summary>
+    public DbSet<Tech> Teches { get; set; }
 
-        protected override void OnConfiguring(
-            DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                string connectionString = ConfigurationManager
-                    .ConnectionStrings["ConnectionString"].ConnectionString;
-                optionsBuilder.UseSqlServer(connectionString);
-            }
-        }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
 
-        /// <summary>
-        /// 기술 리스트: [실습] Teches 테이블부터 Angular 앱 또는 Blazor 앱까지
-        /// </summary>
-        public DbSet<Tech> Teches { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<Tech>().HasData(
-                new Tech { Id = 1, Title = "ASP.NET" }, 
-                new Tech { Id = 2, Title = "Blazor" });
-        }
+        modelBuilder.Entity<Tech>().HasData(
+            new Tech { Id = 1, Title = "ASP.NET" }, 
+            new Tech { Id = 2, Title = "Blazor" });
     }
 }
