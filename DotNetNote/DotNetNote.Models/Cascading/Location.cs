754f3ca40fe9398668e83f655d8ccf871d2db1bb
Park YongJune <redplus@redplus.net>
2024-02-29 21:38:51 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote.Models/Cascading/Location.cs
+++ DotNetNote/DotNetNote.Models/Cascading/Location.cs
@@ -2,23 +2,22 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace VisualAcademy.Models
+namespace VisualAcademy.Models;
+
+[Table("Location")]
+public partial class Location
 {
-    [Table("Location")]
-    public partial class Location
-    {
-        [Key]
-        public int Id { get; set; }
+    [Key]
+    public int Id { get; set; }
 
-        [Display(Name = "Location Name")]
-        public string Name { get; set; } = "";
+    [Display(Name = "Location Name")]
+    public string Name { get; set; } = "";
 
-        public bool Active { get; set; } = false;
+    public bool Active { get; set; } = false;
 
-        public string? Property { get; set; } = "";
+    public string? Property { get; set; } = "";
 
-        [Display(Name = "Property")]
-        public Property? PropertyRef { get; set; }
-        public int? PropertyId { get; set; }
-    }
+    [Display(Name = "Property")]
+    public Property? PropertyRef { get; set; }
+    public int? PropertyId { get; set; }
 }
