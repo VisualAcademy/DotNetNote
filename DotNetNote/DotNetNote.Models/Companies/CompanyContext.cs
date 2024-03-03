b1abdf2d551ecd1b1db633217daf18a25628eb41
Park YongJune <redplus@redplus.net>
2024-03-03 19:20:48 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote.Models/Companies/CompanyContext.cs
+++ DotNetNote/DotNetNote.Models/Companies/CompanyContext.cs
@@ -6,39 +6,38 @@
 using Microsoft.EntityFrameworkCore;
 using System.Configuration;
 
-namespace DotNetNote.Models.Companies
+namespace DotNetNote.Models.Companies;
+
+public class CompanyContext : DbContext
 {
-    public class CompanyContext : DbContext
+    public CompanyContext()
     {
-        public CompanyContext()
-        {
-            // Empty
-        }
-
-        public CompanyContext(DbContextOptions<CompanyContext> options)
-            : base(options)
-        {
-            // 공식과 같은 코드 
-        }
-
-        //// 강의에서 사용한 형태 
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DotNetNote;Integrated Security=True;");
-        //}
-        protected override void OnConfiguring(
-            DbContextOptionsBuilder optionsBuilder)
-        {
-            // 닷넷 프레임워크 기반에서 호출되는 코드 영역: 
-            // App.Config 또는 Web.Config의 연결 문자열 사용
-            if (!optionsBuilder.IsConfigured)
-            {
-                string connectionString = ConfigurationManager.ConnectionStrings[
-                    "DefaultConnection"].ConnectionString;
-                optionsBuilder.UseSqlServer(connectionString);
-            }
-        }
-
-        public DbSet<CompanyModel> Companies { get; set; }
+        // Empty
     }
+
+    public CompanyContext(DbContextOptions<CompanyContext> options)
+        : base(options)
+    {
+        // 공식과 같은 코드 
+    }
+
+    //// 강의에서 사용한 형태 
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+    //    optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DotNetNote;Integrated Security=True;");
+    //}
+    protected override void OnConfiguring(
+        DbContextOptionsBuilder optionsBuilder)
+    {
+        // 닷넷 프레임워크 기반에서 호출되는 코드 영역: 
+        // App.Config 또는 Web.Config의 연결 문자열 사용
+        if (!optionsBuilder.IsConfigured)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings[
+                "DefaultConnection"].ConnectionString;
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+
+    public DbSet<CompanyModel> Companies { get; set; }
 }
