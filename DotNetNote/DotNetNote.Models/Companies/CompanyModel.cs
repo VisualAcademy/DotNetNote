37030a590b45abef930c03b2c5830304d6f47729
Park YongJune <redplus@redplus.net>
2024-03-03 19:21:27 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote.Models/Companies/CompanyModel.cs
+++ DotNetNote/DotNetNote.Models/Companies/CompanyModel.cs
@@ -1,11 +1,10 @@
-﻿namespace DotNetNote.Models.Companies
+﻿namespace DotNetNote.Models.Companies;
+
+/// <summary>
+/// Company, CompanyDomain, ...
+/// </summary>
+public class CompanyModel
 {
-    /// <summary>
-    /// Company, CompanyDomain, ...
-    /// </summary>
-    public class CompanyModel
-    {
-        public long Id { get; set; }
-        public string Name { get; set; }
-    }
+    public long Id { get; set; }
+    public string Name { get; set; }
 }
