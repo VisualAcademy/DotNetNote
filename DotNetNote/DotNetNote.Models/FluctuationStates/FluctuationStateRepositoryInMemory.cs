01e538a97216590579df1a52896b082c590aaf45
Park YongJune <redplus@redplus.net>
2024-03-05 09:50:52 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote.Models/FluctuationStates/FluctuationStateRepositoryInMemory.cs
+++ DotNetNote/DotNetNote.Models/FluctuationStates/FluctuationStateRepositoryInMemory.cs
@@ -1,20 +1,19 @@
 ﻿using System.Collections.Generic;
 
-namespace DotNetNote.Models
-{
-    public class FluctuationStateRepositoryInMemory : IFluctuationStateRepository
-    {
-        public List<FluctuationStateDto> GetFluctuationStates()
-        {
-            List<FluctuationStateDto> r = new List<FluctuationStateDto>()
-            {
-                new FluctuationStateDto { Text = "증가", Value = "0"},
-                new FluctuationStateDto { Text = "감소", Value = "1"},
-                new FluctuationStateDto { Text = "고정", Value = "2"},
-                new FluctuationStateDto { Text = "예측", Value = "3"},
-            };
+namespace DotNetNote.Models;
 
-            return r;
-        }
+public class FluctuationStateRepositoryInMemory : IFluctuationStateRepository
+{
+    public List<FluctuationStateDto> GetFluctuationStates()
+    {
+        List<FluctuationStateDto> r = new List<FluctuationStateDto>()
+        {
+            new FluctuationStateDto { Text = "증가", Value = "0"},
+            new FluctuationStateDto { Text = "감소", Value = "1"},
+            new FluctuationStateDto { Text = "고정", Value = "2"},
+            new FluctuationStateDto { Text = "예측", Value = "3"},
+        };
+
+        return r;
     }
 }
