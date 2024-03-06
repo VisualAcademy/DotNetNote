4c9366c66ec475bc4b936da5625ee847013c4200
Park YongJune <redplus@redplus.net>
2024-03-07 00:24:00 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote.Models/Ideas/IdeaRepository.cs
+++ DotNetNote/DotNetNote.Models/Ideas/IdeaRepository.cs
@@ -5,37 +5,36 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 
-namespace DotNetNote.Models.Ideas
+namespace DotNetNote.Models.Ideas;
+
+/// <summary>
+/// [4] 리포지토리 클래스
+/// </summary>
+public class IdeaRepository : IIdeaRepository
 {
-    /// <summary>
-    /// [4] 리포지토리 클래스
-    /// </summary>
-    public class IdeaRepository : IIdeaRepository
+    private IConfiguration _config;
+    private IDbConnection db;
+
+    public IdeaRepository(IConfiguration config)
     {
-        private IConfiguration _config;
-        private IDbConnection db;
+        _config = config;
 
-        public IdeaRepository(IConfiguration config)
-        {
-            _config = config;
+        db = new SqlConnection(_config.GetSection("ConnectionString").Value);
+    }
 
-            db = new SqlConnection(_config.GetSection("ConnectionString").Value);
-        }
+    public List<Idea> GetAll() =>
+        db.Query<Idea>("Select * From Ideas").ToList();
 
-        public List<Idea> GetAll() =>
-            db.Query<Idea>("Select * From Ideas").ToList();
+    public Idea Add(Idea model)
+    {
+        //[!] 새로운 데이터 저장 후 일련번호(Identity) 값을 반환 받아 모델 클래스에 채움
+        var sql =
+            "Insert Into Ideas (Note) Values (@Note); " +
+            "Select Cast(SCOPE_IDENTITY() As Int);";
 
-        public Idea Add(Idea model)
-        {
-            //[!] 새로운 데이터 저장 후 일련번호(Identity) 값을 반환 받아 모델 클래스에 채움
-            var sql =
-                "Insert Into Ideas (Note) Values (@Note); " +
-                "Select Cast(SCOPE_IDENTITY() As Int);";
+        var id = db.Query<int>(sql, model).Single();
 
-            var id = db.Query<int>(sql, model).Single();
-
-            model.Id = id;
-            return model;
-        }
+        model.Id = id;
+        return model;
     }
 }
