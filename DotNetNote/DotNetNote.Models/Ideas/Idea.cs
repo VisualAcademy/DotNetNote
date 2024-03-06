286bbeb8a8edf1eb94bb6d5f3a0f24590b391a06
Park YongJune <redplus@redplus.net>
2024-03-06 11:45:24 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote.Models/Ideas/Idea.cs
+++ DotNetNote/DotNetNote.Models/Ideas/Idea.cs
@@ -1,22 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace DotNetNote.Models.Ideas
+namespace DotNetNote.Models.Ideas;
+
+/// <summary>
+/// [2] 모델 클래스
+/// Idea 모델 클래스: Ideas 테이블과 일대일로 매핑
+/// </summary>
+public class Idea
 {
     /// <summary>
-    /// [2] 모델 클래스
-    /// Idea 모델 클래스: Ideas 테이블과 일대일로 매핑
+    /// 일련번호
     /// </summary>
-    public class Idea
-    {
-        /// <summary>
-        /// 일련번호
-        /// </summary>
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        /// <summary>
-        /// 노트
-        /// </summary>
-        [Required]
-        public string Note { get; set; }
-    }
+    /// <summary>
+    /// 노트
+    /// </summary>
+    [Required]
+    public string Note { get; set; }
 }
