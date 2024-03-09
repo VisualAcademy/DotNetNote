1bf8b52da9b6732a70265b70eaae81176470a4cb
Park YongJune <redplus@redplus.net>
2024-03-10 07:25:18 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Data/MemberDbContext.cs
+++ DotNetNote/DotNetNote/Data/MemberDbContext.cs
@@ -1,19 +1,18 @@
-﻿namespace MemberManagement.Data
+﻿namespace MemberManagement.Data;
+
+public class MemberDbContext : DbContext
 {
-    public class MemberDbContext : DbContext
+    // Member 모델 클래스를 바탕으로 Members 테이블을 생성
+    public DbSet<Member> Members { get; set; }
+
+    public MemberDbContext(DbContextOptions<MemberDbContext> options)
+        : base(options)
     {
-        // Member 모델 클래스를 바탕으로 Members 테이블을 생성
-        public DbSet<Member> Members { get; set; }
 
-        public MemberDbContext(DbContextOptions<MemberDbContext> options)
-            : base(options)
-        {
+    }
 
-        }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-
-        }
     }
 }
