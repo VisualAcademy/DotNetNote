6a99c96eda168a307c10cb66b5536dc6ea9c6dcc
Park YongJune <redplus@redplus.net>
2024-03-14 07:32:10 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote.Models/Twelves/Twelve.cs
+++ DotNetNote/DotNetNote.Models/Twelves/Twelve.cs
@@ -1,28 +1,27 @@
-﻿namespace DotNetNote.Models
+﻿namespace DotNetNote.Models;
+
+/// <summary>
+/// Twelve 모델 클래스: 12개월 데이터 표시 및 요약
+/// </summary>
+public class Twelve
 {
     /// <summary>
-    /// Twelve 모델 클래스: 12개월 데이터 표시 및 요약
+    /// 일련번호
     /// </summary>
-    public class Twelve
-    {
-        /// <summary>
-        /// 일련번호
-        /// </summary>
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        /// <summary>
-        /// 참조번호 
-        /// </summary>
-        public int ParentId { get; set; }
+    /// <summary>
+    /// 참조번호 
+    /// </summary>
+    public int ParentId { get; set; }
 
-        /// <summary>
-        /// 월: 1부터 12까지
-        /// </summary>
-        public int MonthNumber { get; set; }
+    /// <summary>
+    /// 월: 1부터 12까지
+    /// </summary>
+    public int MonthNumber { get; set; }
 
-        /// <summary>
-        /// 이익
-        /// </summary>
-        public double? Profit { get; set; }
-    }
+    /// <summary>
+    /// 이익
+    /// </summary>
+    public double? Profit { get; set; }
 }
