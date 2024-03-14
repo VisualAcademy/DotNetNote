47a50875a5ff46d3b648226be108906550ea143c
Park YongJune <redplus@redplus.net>
2024-03-14 17:44:20 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Controllers/FluentUITestsController.cs
+++ DotNetNote/DotNetNote/Controllers/FluentUITestsController.cs
@@ -1,10 +1,9 @@
-﻿namespace DotNetNote.Controllers
+﻿namespace DotNetNote.Controllers;
+
+public class FluentUITestsController : Controller
 {
-    public class FluentUITestsController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        return View();
     }
 }
