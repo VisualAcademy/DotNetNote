24767b92de18f3fde806cefbaf86784bb4b042bd
Park YongJune <redplus@redplus.net>
2024-03-21 04:36:06 +0900

    Remove unnecessary usings

--- DotNetNote/DotNetNote/Models/AccountViewModels/ForgotPasswordViewModel.cs
+++ DotNetNote/DotNetNote/Models/AccountViewModels/ForgotPasswordViewModel.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace DotNetNote.Models.AccountViewModels;
+﻿namespace DotNetNote.Models.AccountViewModels;
 
 public class ForgotPasswordViewModel
 {
