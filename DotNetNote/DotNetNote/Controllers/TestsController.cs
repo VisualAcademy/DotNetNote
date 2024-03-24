f0550c95c66a0eaa04e5cb5edc304971e2be12d8
Park YongJune <redplus@redplus.net>
2024-03-25 08:16:40 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Controllers/TestsController.cs
+++ DotNetNote/DotNetNote/Controllers/TestsController.cs
@@ -1,18 +1,17 @@
 ﻿// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace DotNetNote.Controllers
-{
-    public class TestsController : Controller
-    {
-        // GET: /<controller>/
-        public IActionResult Index()
-        {
-            return View();
-        }
+namespace DotNetNote.Controllers;
 
-        public IActionResult DulGetNameTest()
-        {
-            return View(); 
-        }
+public class TestsController : Controller
+{
+    // GET: /<controller>/
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult DulGetNameTest()
+    {
+        return View(); 
     }
 }
