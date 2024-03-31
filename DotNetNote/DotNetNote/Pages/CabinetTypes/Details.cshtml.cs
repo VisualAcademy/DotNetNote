1753a90128e9bed5accb9fdd557e8f0e217cfabf
Park YongJune <redplus@redplus.net>
2024-04-01 02:25:05 +0900

    Use primary constructor (and remove fields)

--- DotNetNote/DotNetNote/Pages/CabinetTypes/Details.cshtml.cs
+++ DotNetNote/DotNetNote/Pages/CabinetTypes/Details.cshtml.cs
@@ -3,12 +3,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DotNetNote.Pages.CabinetTypes;
 
-public class DetailsModel : PageModel
+public class DetailsModel(Data.ApplicationDbContext context) : PageModel
 {
-    private readonly DotNetNote.Data.ApplicationDbContext _context;
-
-    public DetailsModel(Data.ApplicationDbContext context) => _context = context;
-
     public CabinetType CabinetType { get; set; }
 
     public async Task<IActionResult> OnGetAsync(long? id)
@@ -18,7 +14,7 @@ public class DetailsModel : PageModel
             return NotFound();
         }
 
-        CabinetType = await _context.CabinetTypes.FirstOrDefaultAsync(m => m.Id == id);
+        CabinetType = await context.CabinetTypes.FirstOrDefaultAsync(m => m.Id == id);
 
         if (CabinetType == null)
         {
