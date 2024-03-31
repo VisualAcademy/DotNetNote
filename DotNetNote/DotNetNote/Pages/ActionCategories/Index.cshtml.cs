5f778aeef9a5bd44b7c22a7fdd78ce0405489978
Park YongJune <redplus@redplus.net>
2024-03-31 23:58:58 +0900

    Remove unnecessary usings

--- DotNetNote/DotNetNote/Pages/ActionCategories/Index.cshtml.cs
+++ DotNetNote/DotNetNote/Pages/ActionCategories/Index.cshtml.cs
@@ -1,7 +1,5 @@
 ﻿#nullable disable
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 using Acts.Models;
 
 namespace Acts.Pages.ActionCategories;
