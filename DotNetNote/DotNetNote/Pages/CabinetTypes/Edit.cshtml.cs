61ab20f2ff46f2b653725d7ea36b02777668db57
Park YongJune <redplus@redplus.net>
2024-04-02 23:33:57 +0900

    Remove unnecessary usings

--- DotNetNote/DotNetNote/Pages/CabinetTypes/Edit.cshtml.cs
+++ DotNetNote/DotNetNote/Pages/CabinetTypes/Edit.cshtml.cs
@@ -1,9 +1,5 @@
 ﻿#nullable disable
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 
 namespace DotNetNote.Pages.CabinetTypes;
 
