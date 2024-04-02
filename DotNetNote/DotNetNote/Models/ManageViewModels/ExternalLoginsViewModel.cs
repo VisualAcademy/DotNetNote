dbe1607f8852024877427c4de82c8b9469c7b73e
Park YongJune <redplus@redplus.net>
2024-04-03 02:08:30 +0900

    Convert to file-scoped namesapce

--- DotNetNote/DotNetNote/Models/ManageViewModels/ExternalLoginsViewModel.cs
+++ DotNetNote/DotNetNote/Models/ManageViewModels/ExternalLoginsViewModel.cs
@@ -1,13 +1,12 @@
-﻿namespace DotNetNote.Models.ManageViewModels
+﻿namespace DotNetNote.Models.ManageViewModels;
+
+public class ExternalLoginsViewModel
 {
-    public class ExternalLoginsViewModel
-    {
-        public IList<UserLoginInfo> CurrentLogins { get; set; }
+    public IList<UserLoginInfo> CurrentLogins { get; set; }
 
-        public IList<AuthenticationScheme> OtherLogins { get; set; }
+    public IList<AuthenticationScheme> OtherLogins { get; set; }
 
-        public bool ShowRemoveButton { get; set; }
+    public bool ShowRemoveButton { get; set; }
 
-        public string StatusMessage { get; set; }
-    }
+    public string StatusMessage { get; set; }
 }
