157ce59d29eb6904f3cff8c1ebe6d2b0ffd33d3e
Park YongJune <redplus@redplus.net>
2024-04-07 14:02:13 +0900

    Remove unnecessary usings

--- DotNetNote/DotNetNote/Pages/Cascading/Sublocations/Index.cshtml.cs
+++ DotNetNote/DotNetNote/Pages/Cascading/Sublocations/Index.cshtml.cs
@@ -1,8 +1,5 @@
 ﻿#nullable disable
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using VisualAcademy.Models;
 
 namespace VisualAcademy.Pages.Cascading.Sublocations
 {
