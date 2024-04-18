d8316c3d3b06c1f2e7b301faa8c702999dc3adc2
Park YongJune <redplus@redplus.net>
2024-04-19 02:05:16 +0900

    Global usings update

--- DotNetNote/DotNetNote/Controllers/CkEditorDemoController.cs
+++ DotNetNote/DotNetNote/Controllers/CkEditorDemoController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Net.Http.Headers;
-using System.IO;
 
 namespace DotNetNote.Controllers;
 
--- DotNetNote/DotNetNote/GlobalUsings.cs
+++ DotNetNote/DotNetNote/GlobalUsings.cs
@@ -21,6 +21,7 @@ global using System;
 global using System.Collections.Generic;
 global using System.ComponentModel.DataAnnotations;
 global using System.Data;
+global using System.IO;
 global using System.Linq;
 global using System.Security.Claims;
 global using System.Text;
