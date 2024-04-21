f11bdf7da97cfe617e7d171cfe7db649bcf58d83
Park YongJune <redplus@redplus.net>
2024-04-21 21:43:57 +0900

    Use primary constructor

--- DotNetNote/DotNetNote/Data/MemberDbContext.cs
+++ DotNetNote/DotNetNote/Data/MemberDbContext.cs
@@ -1,15 +1,9 @@
 ﻿namespace MemberManagement.Data;
 
-public class MemberDbContext : DbContext
+public class MemberDbContext(DbContextOptions<MemberDbContext> options) : DbContext(options)
 {
     // Member 모델 클래스를 바탕으로 Members 테이블을 생성
     public DbSet<Member> Members { get; set; }
-
-    public MemberDbContext(DbContextOptions<MemberDbContext> options)
-        : base(options)
-    {
-
-    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
