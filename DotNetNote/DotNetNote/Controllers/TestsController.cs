8059d4974b6cc22d9de9412402c355810c1fa769
Park YongJune <redplus@redplus.net>
2024-04-23 17:05:42 +0900

    Code cleanup

--- DotNetNote/DotNetNote/Controllers/TestsController.cs
+++ DotNetNote/DotNetNote/Controllers/TestsController.cs
@@ -1,6 +1,4 @@
-﻿// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace DotNetNote.Controllers;
+﻿namespace DotNetNote.Controllers;
 
 public class TestsController : Controller
 {
