210aec4c76f6e7e6c90cd6313a92137a5d55a06c
Park YongJune <redplus@redplus.net>
2024-05-13 09:19:44 +0900

    Use primary constructor (and remove fields)

--- DotNetNote/DotNetNote/Controllers/LoggingDemoController.cs
+++ DotNetNote/DotNetNote/Controllers/LoggingDemoController.cs
@@ -1,22 +1,18 @@
 ﻿namespace DotNetNote.Controllers;
 
-public class LoggingDemoController : Controller
+public class LoggingDemoController(ILogger<LoggingDemoController> logger) : Controller
 {
-    private readonly ILogger<LoggingDemoController> _logger;
-
-    public LoggingDemoController(ILogger<LoggingDemoController> logger) => _logger = logger; // 생성자 주입 방식으로 ILogger 개체 사용
-
     public IActionResult Index()
     {
         // Index 페이지 실행시 로그의 Info 범주에 문자열과 시간 출력
-        _logger.LogInformation("Index View {time}", DateTime.Now);
+        logger.LogInformation("Index View {time}", DateTime.Now);
         return View();
     }
 
     public IActionResult About()
     {
         // About 페이지 실행시 로그의 Info 범주에 문자열과 시간 출력
-        _logger.LogInformation("About View {time}", DateTime.Now);
+        logger.LogInformation("About View {time}", DateTime.Now);
 
         return View();
     }
