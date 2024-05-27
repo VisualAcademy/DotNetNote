6bcfe3ba2b486b2484c47840de37e9ad85ba3ba9
Park YongJune <redplus@redplus.net>
2024-05-28 00:40:48 +0900

    Remove unnecessary usings

--- DotNetNote/DotNetNote/Controllers/WebAPI/WebApiNoteController.cs
+++ DotNetNote/DotNetNote/Controllers/WebAPI/WebApiNoteController.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace DotNetNote.Controllers;
+﻿namespace DotNetNote.Controllers;
 
 [Route("api/[controller]")]
 public class WebApiNoteController : Controller
