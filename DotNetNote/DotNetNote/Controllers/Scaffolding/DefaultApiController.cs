a4ad4524fe95121dce73005458676c490de2f505
Park YongJune <redplus@redplus.net>
2024-06-09 22:28:03 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Controllers/Scaffolding/DefaultApiController.cs
+++ DotNetNote/DotNetNote/Controllers/Scaffolding/DefaultApiController.cs
@@ -1,39 +1,38 @@
-﻿namespace ScaffoldingDemo.Controllers
-{
-    [Produces("application/json")]
-    [Route("api/DefaultApi")]
-    public class DefaultApiController : Controller
-    {
-        // GET: api/DefaultApi
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+﻿namespace ScaffoldingDemo.Controllers;
 
-        // GET: api/DefaultApi/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-        
-        // POST: api/DefaultApi
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-        
-        // PUT: api/DefaultApi/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-        
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+[Produces("application/json")]
+[Route("api/DefaultApi")]
+public class DefaultApiController : Controller
+{
+    // GET: api/DefaultApi
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
+
+    // GET: api/DefaultApi/5
+    [HttpGet("{id}", Name = "Get")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+    
+    // POST: api/DefaultApi
+    [HttpPost]
+    public void Post([FromBody]string value)
+    {
+    }
+    
+    // PUT: api/DefaultApi/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody]string value)
+    {
+    }
+    
+    // DELETE: api/ApiWithActions/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
     }
 }
