b2688a01ab6dbd7b220eef5941f4be79bb9ce0c6
Park YongJune <redplus@redplus.net>
2024-06-11 01:39:10 +0900

    .

--- DotNetNote/DotNetNote/Models/Site.cs
+++ DotNetNote/DotNetNote/Models/Site.cs
@@ -5,8 +5,23 @@
 /// </summary>
 public class Site
 {
+    /// <summary>
+    /// 일련번호
+    /// </summary>
     public int Id { get; set; }
+
+    /// <summary>
+    /// 제목, 사이트명
+    /// </summary>
     public string Title { get; set; }
+
+    /// <summary>
+    /// 사이트 URL
+    /// </summary>
     public string Url { get; set; }
+
+    /// <summary>
+    /// 사이트 간단 설명 
+    /// </summary>
     public string Description { get; set; }
 }
