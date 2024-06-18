0e37f67df5bbc0cbe2dc82a2d0964e024be3db7a
Park YongJune <redplus@redplus.net>
2024-06-19 02:00:09 +0900

    Use 'new(...)'

--- DotNetNote/DotNetNote/Controllers/DemoController.cs
+++ DotNetNote/DotNetNote/Controllers/DemoController.cs
@@ -40,7 +40,7 @@ public class DemoController : Controller
     /// </summary>
     public IActionResult ViewWithListOfDemo()
     {
-        List<DemoModel> models = new List<DemoModel>() {
+        List<DemoModel> models = new() {
             new DemoModel { Id = 1, Name = "홍길동" },
             new DemoModel { Id = 2, Name = "백두산" },
             new DemoModel { Id = 3, Name = "임꺽정" }
