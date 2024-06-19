0b21000ef97bb89a29a69dfae3f4f4c2e7ab10d5
Park YongJune <redplus@redplus.net>
2024-06-19 15:01:05 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/ViewComponents/RecentlyCommentList2ViewComponent.cs
+++ DotNetNote/DotNetNote/ViewComponents/RecentlyCommentList2ViewComponent.cs
@@ -1,14 +1,13 @@
 ﻿using DotNetNote.Models.Notes;
 
-namespace DotNetNote.ViewComponents
+namespace DotNetNote.ViewComponents;
+
+public class RecentlyCommentList2ViewComponent(
+    INoteCommentRepository repository) : ViewComponent
 {
-    public class RecentlyCommentList2ViewComponent(
-        INoteCommentRepository repository) : ViewComponent
+    public IViewComponentResult Invoke()
     {
-        public IViewComponentResult Invoke()
-        {
-            // 최근 댓글 리스트 전달
-            return View(repository.GetRecentCommentsNoCache());
-        }
+        // 최근 댓글 리스트 전달
+        return View(repository.GetRecentCommentsNoCache());
     }
 }
