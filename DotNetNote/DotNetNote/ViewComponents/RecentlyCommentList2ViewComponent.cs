b3334cc96d1ebd63fbbf3468b3a4650eaf90a3f8
Park YongJune <redplus@redplus.net>
2024-06-19 15:00:43 +0900

    Use primary constructor (and remove fields)

--- DotNetNote/DotNetNote/ViewComponents/RecentlyCommentList2ViewComponent.cs
+++ DotNetNote/DotNetNote/ViewComponents/RecentlyCommentList2ViewComponent.cs
@@ -2,21 +2,13 @@
 
 namespace DotNetNote.ViewComponents
 {
-    public class RecentlyCommentList2ViewComponent : ViewComponent
+    public class RecentlyCommentList2ViewComponent(
+        INoteCommentRepository repository) : ViewComponent
     {
-        // 댓글 리포지토리 개체
-        private INoteCommentRepository _repository;
-
-        public RecentlyCommentList2ViewComponent(
-            INoteCommentRepository repository)
-        {
-            _repository = repository;
-        }
-
         public IViewComponentResult Invoke()
         {
             // 최근 댓글 리스트 전달
-            return View(_repository.GetRecentCommentsNoCache());
+            return View(repository.GetRecentCommentsNoCache());
         }
     }
 }
