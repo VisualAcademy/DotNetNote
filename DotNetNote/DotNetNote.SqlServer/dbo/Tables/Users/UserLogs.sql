d5de7c15329ee2e5b7c73bf1bcdbdf4c25cd233f
Park YongJune <redplus@redplus.net>
2024-06-21 19:03:14 +0900

    .

--- DotNetNote/DotNetNote.SqlServer/dbo/Tables/Users/UserLogs.sql
+++ DotNetNote/DotNetNote.SqlServer/dbo/Tables/Users/UserLogs.sql
@@ -3,10 +3,13 @@ CREATE TABLE [dbo].[UserLogs]
 (
 	-- 일련번호
 	[Id] INT NOT NULL PRIMARY KEY Identity(1, 1),		
+
 	-- 사용자 아이디
 	Username NVarChar(50) Not Null,						
+
 	-- 로그인 실패 카운트
 	FailedPasswordAttemptCount Int Default(0),			
+
 	-- 로그인 실패 처음 생성일
 	FailedPasswordAttemptWindowStart 
 		DateTime Default(GetDate())						
