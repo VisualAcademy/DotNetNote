caa73c6a54ddd5669b14e91de8545e627d80c26e
Park YongJune <redplus@redplus.net>
2024-06-23 03:10:08 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Controllers/Articles/Comment.cs
+++ DotNetNote/DotNetNote/Controllers/Articles/Comment.cs
@@ -2,48 +2,47 @@ using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Security.Cryptography;
 
-namespace MemoEngineCore.Models
+namespace MemoEngineCore.Models;
+
+public class Comment
 {
-    public class Comment
+    [Required]
+    public string Author { get; set; } = string.Empty;
+
+    [Required]
+    public string Content { get; set; } = string.Empty;
+
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string ID { get; set; } = Guid.NewGuid().ToString();
+
+    public bool IsAdmin { get; set; } = false;
+
+    [Required]
+    public DateTime PubDate { get; set; } = DateTime.UtcNow;
+
+    [SuppressMessage(
+        "Security",
+        "CA5351:Do Not Use Broken Cryptographic Algorithms",
+        Justification = "We aren't using it for encryption so we don't care.")]
+    [SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "It is an email address.")]
+    public string GetGravatar()
     {
-        [Required]
-        public string Author { get; set; } = string.Empty;
+        using var md5 = MD5.Create();
+        var inputBytes = Encoding.UTF8.GetBytes(this.Email.Trim().ToLowerInvariant());
+        var hashBytes = md5.ComputeHash(inputBytes);
 
-        [Required]
-        public string Content { get; set; } = string.Empty;
-
-        [Required, EmailAddress]
-        public string Email { get; set; } = string.Empty;
-
-        [Required]
-        public string ID { get; set; } = Guid.NewGuid().ToString();
-
-        public bool IsAdmin { get; set; } = false;
-
-        [Required]
-        public DateTime PubDate { get; set; } = DateTime.UtcNow;
-
-        [SuppressMessage(
-            "Security",
-            "CA5351:Do Not Use Broken Cryptographic Algorithms",
-            Justification = "We aren't using it for encryption so we don't care.")]
-        [SuppressMessage("Globalization", "CA1308:Normalize strings to uppercase", Justification = "It is an email address.")]
-        public string GetGravatar()
+        // Convert the byte array to hexadecimal string
+        var sb = new StringBuilder();
+        for (var i = 0; i < hashBytes.Length; i++)
         {
-            using var md5 = MD5.Create();
-            var inputBytes = Encoding.UTF8.GetBytes(this.Email.Trim().ToLowerInvariant());
-            var hashBytes = md5.ComputeHash(inputBytes);
-
-            // Convert the byte array to hexadecimal string
-            var sb = new StringBuilder();
-            for (var i = 0; i < hashBytes.Length; i++)
-            {
-                sb.Append(hashBytes[i].ToString("X2", CultureInfo.InvariantCulture));
-            }
-
-            return $"https://www.gravatar.com/avatar/{sb.ToString().ToLowerInvariant()}?s=60&d=blank";
+            sb.Append(hashBytes[i].ToString("X2", CultureInfo.InvariantCulture));
         }
 
-        public string RenderContent() => this.Content;
+        return $"https://www.gravatar.com/avatar/{sb.ToString().ToLowerInvariant()}?s=60&d=blank";
     }
+
+    public string RenderContent() => this.Content;
 }
