c76935f243c9388e3c4e9a0029436eb9de4a5604
Park YongJune <redplus@redplus.net>
2024-06-24 04:38:58 +0900

    Remove unnecessary usings

--- DotNetNote/DotNetNote/Controllers/Users/UserServiceController.cs
+++ DotNetNote/DotNetNote/Controllers/Users/UserServiceController.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-
-namespace DotNetNote.Controllers;
+﻿namespace DotNetNote.Controllers;
 
 [Route("api/[controller]")]
 public class UserServiceController(IUserRepository repo) : Controller
