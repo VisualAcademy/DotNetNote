6d33c80a27ce47e17f29a14e8702ef61d97d4393
Park YongJune <redplus@redplus.net>
2024-06-30 08:11:44 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Services/Interfaces/IEmailManager.cs
+++ DotNetNote/DotNetNote/Services/Interfaces/IEmailManager.cs
@@ -1,11 +1,10 @@
-﻿namespace DotNetNoteCore.Services
-{
-    // Abstractions: Interfaces => IEmailSender
-    // Implementations: Classes => EmailSender, SendGridEmailSender, ...
-    public interface IEmailManager
-    {
-        Task SendEmailAsync(string email, string subject, string message, string recipient = "", string callbackUrl = "");
+﻿namespace DotNetNoteCore.Services;
 
-        Task SendEmailCodeAsync(string email, string subject, string message, string recipient = "", string callbackUrl = "");
-    }
+// Abstractions: Interfaces => IEmailSender
+// Implementations: Classes => EmailSender, SendGridEmailSender, ...
+public interface IEmailManager
+{
+    Task SendEmailAsync(string email, string subject, string message, string recipient = "", string callbackUrl = "");
+
+    Task SendEmailCodeAsync(string email, string subject, string message, string recipient = "", string callbackUrl = "");
 }
