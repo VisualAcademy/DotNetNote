332aa94668ea28a6bc87be5d992821c67f0cd927
Park YongJune <redplus@redplus.net>
2024-07-07 21:43:38 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/TagHelpers/UnixTimeConverterTagHelper.cs
+++ DotNetNote/DotNetNote/TagHelpers/UnixTimeConverterTagHelper.cs
@@ -1,27 +1,26 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
 
-namespace DotNetNote.TagHelpers
+namespace DotNetNote.TagHelpers;
+
+public class UnixTimeConverterTagHelper : TagHelper
 {
-    public class UnixTimeConverterTagHelper : TagHelper
+    //public override void Process(
+    //    TagHelperContext context, TagHelperOutput output)
+    //{
+    //    output.Content.SetContent("UNIX 시간 변경기");
+    //}
+
+    public override async Task ProcessAsync(
+        TagHelperContext context, TagHelperOutput output)
     {
-        //public override void Process(
-        //    TagHelperContext context, TagHelperOutput output)
-        //{
-        //    output.Content.SetContent("UNIX 시간 변경기");
-        //}
+        var childContent = (await output.GetChildContentAsync()).GetContent();
 
-        public override async Task ProcessAsync(
-            TagHelperContext context, TagHelperOutput output)
-        {
-            var childContent = (await output.GetChildContentAsync()).GetContent();
+        var unixTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
-            var unixTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        var currentTime = unixTime.AddSeconds(Convert.ToDouble(childContent));
 
-            var currentTime = unixTime.AddSeconds(Convert.ToDouble(childContent));
-
-            output.Content.SetContent(currentTime.ToString(Formatter));
-        }
-
-        public string Formatter { get; set; } = "yyyy-MM-dd hh:mm:ss";
+        output.Content.SetContent(currentTime.ToString(Formatter));
     }
+
+    public string Formatter { get; set; } = "yyyy-MM-dd hh:mm:ss";
 }
