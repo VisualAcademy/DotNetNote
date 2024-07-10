141e65c1393cfdfd06febbb61fc4cad5fea03926
Park YongJune <redplus@redplus.net>
2024-07-11 06:21:11 +0900

    불필요한 Using 제거 Remove unnecessary usings

--- DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
-using DotNetNote.Data;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace VisualAcademy.Areas.Identity.Pages.Account
 {
