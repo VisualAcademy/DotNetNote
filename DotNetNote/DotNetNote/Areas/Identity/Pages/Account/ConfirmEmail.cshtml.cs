33f75a54b8a6780241e2f8664a8aad5b6d5b2ada
Park YongJune <redplus@redplus.net>
2024-07-13 01:25:07 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ DotNetNote/DotNetNote/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -1,37 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace VisualAcademy.Areas.Identity.Pages.Account
+namespace VisualAcademy.Areas.Identity.Pages.Account;
+
+[AllowAnonymous]
+public class ConfirmEmailModel : PageModel
 {
-    [AllowAnonymous]
-    public class ConfirmEmailModel : PageModel
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public ConfirmEmailModel(UserManager<ApplicationUser> userManager)
     {
-        private readonly UserManager<ApplicationUser> _userManager;
+        _userManager = userManager;
+    }
 
-        public ConfirmEmailModel(UserManager<ApplicationUser> userManager)
+    public async Task<IActionResult> OnGetAsync(string userId, string code)
+    {
+        if (userId == null || code == null)
         {
-            _userManager = userManager;
+            return RedirectToPage("/Index");
         }
 
-        public async Task<IActionResult> OnGetAsync(string userId, string code)
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user == null)
         {
-            if (userId == null || code == null)
-            {
-                return RedirectToPage("/Index");
-            }
-
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null)
-            {
-                return NotFound($"Unable to load user with ID '{userId}'.");
-            }
-
-            var result = await _userManager.ConfirmEmailAsync(user, code);
-            if (!result.Succeeded)
-            {
-                throw new InvalidOperationException($"Error confirming email for user with ID '{userId}':");
-            }
-
-            return Page();
+            return NotFound($"Unable to load user with ID '{userId}'.");
         }
+
+        var result = await _userManager.ConfirmEmailAsync(user, code);
+        if (!result.Succeeded)
+        {
+            throw new InvalidOperationException($"Error confirming email for user with ID '{userId}':");
+        }
+
+        return Page();
     }
 }
