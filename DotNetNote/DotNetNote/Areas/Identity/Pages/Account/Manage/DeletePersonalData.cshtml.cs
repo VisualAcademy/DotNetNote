173ceb877bb831bc46393035abbfbff7fd15dc42
Park YongJune <redplus@redplus.net>
2024-07-14 08:34:38 +0900

    기본 생성자 사용(및 필드 제거)

--- DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/DeletePersonalData.cshtml.cs
+++ DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/DeletePersonalData.cshtml.cs
@@ -2,22 +2,11 @@
 
 namespace VisualAcademy.Areas.Identity.Pages.Account.Manage;
 
-public class DeletePersonalDataModel : PageModel
+public class DeletePersonalDataModel(
+    UserManager<ApplicationUser> userManager,
+    SignInManager<ApplicationUser> signInManager,
+    ILogger<DeletePersonalDataModel> logger) : PageModel
 {
-    private readonly UserManager<ApplicationUser> _userManager;
-    private readonly SignInManager<ApplicationUser> _signInManager;
-    private readonly ILogger<DeletePersonalDataModel> _logger;
-
-    public DeletePersonalDataModel(
-        UserManager<ApplicationUser> userManager,
-        SignInManager<ApplicationUser> signInManager,
-        ILogger<DeletePersonalDataModel> logger)
-    {
-        _userManager = userManager;
-        _signInManager = signInManager;
-        _logger = logger;
-    }
-
     [BindProperty]
     public InputModel Input { get; set; }
 
@@ -32,44 +21,44 @@ public class DeletePersonalDataModel : PageModel
 
     public async Task<IActionResult> OnGet()
     {
-        var user = await _userManager.GetUserAsync(User);
+        var user = await userManager.GetUserAsync(User);
         if (user == null)
         {
-            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
         }
 
-        RequirePassword = await _userManager.HasPasswordAsync(user);
+        RequirePassword = await userManager.HasPasswordAsync(user);
         return Page();
     }
 
     public async Task<IActionResult> OnPostAsync()
     {
-        var user = await _userManager.GetUserAsync(User);
+        var user = await userManager.GetUserAsync(User);
         if (user == null)
         {
-            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
         }
 
-        RequirePassword = await _userManager.HasPasswordAsync(user);
+        RequirePassword = await userManager.HasPasswordAsync(user);
         if (RequirePassword)
         {
-            if (!await _userManager.CheckPasswordAsync(user, Input.Password))
+            if (!await userManager.CheckPasswordAsync(user, Input.Password))
             {
                 ModelState.AddModelError(string.Empty, "Password not correct.");
                 return Page();
             }
         }
 
-        var result = await _userManager.DeleteAsync(user);
-        var userId = await _userManager.GetUserIdAsync(user);
+        var result = await userManager.DeleteAsync(user);
+        var userId = await userManager.GetUserIdAsync(user);
         if (!result.Succeeded)
         {
             throw new InvalidOperationException($"Unexpected error occurred deleteing user with ID '{userId}'.");
         }
 
-        await _signInManager.SignOutAsync();
+        await signInManager.SignOutAsync();
 
-        _logger.LogInformation("User with ID '{UserId}' deleted themselves.", userId);
+        logger.LogInformation("User with ID '{UserId}' deleted themselves.", userId);
 
         return Redirect("~/");
     }
