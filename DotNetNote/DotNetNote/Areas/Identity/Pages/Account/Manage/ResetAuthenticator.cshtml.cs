1c8dc6564407b0be03edc8a67929d07c0f4e30dc
Park YongJune <redplus@redplus.net>
2024-07-19 09:45:05 +0900

    기본 생성자 사용(및 필드 제거) Use primary constructor (and remove fields)

--- DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/ResetAuthenticator.cshtml.cs
+++ DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/ResetAuthenticator.cshtml.cs
@@ -2,31 +2,20 @@
 
 namespace VisualAcademy.Areas.Identity.Pages.Account.Manage;
 
-public class ResetAuthenticatorModel : PageModel
+public class ResetAuthenticatorModel(
+    UserManager<ApplicationUser> userManager,
+    SignInManager<ApplicationUser> signInManager,
+    ILogger<ResetAuthenticatorModel> logger) : PageModel
 {
-    UserManager<ApplicationUser> _userManager;
-    private readonly SignInManager<ApplicationUser> _signInManager;
-    ILogger<ResetAuthenticatorModel> _logger;
-
-    public ResetAuthenticatorModel(
-        UserManager<ApplicationUser> userManager,
-        SignInManager<ApplicationUser> signInManager,
-        ILogger<ResetAuthenticatorModel> logger)
-    {
-        _userManager = userManager;
-        _signInManager = signInManager;
-        _logger = logger;
-    }
-
     [TempData]
     public string StatusMessage { get; set; }
 
     public async Task<IActionResult> OnGet()
     {
-        var user = await _userManager.GetUserAsync(User);
+        var user = await userManager.GetUserAsync(User);
         if (user == null)
         {
-            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
         }
 
         return Page();
@@ -34,17 +23,17 @@ public class ResetAuthenticatorModel : PageModel
 
     public async Task<IActionResult> OnPostAsync()
     {
-        var user = await _userManager.GetUserAsync(User);
+        var user = await userManager.GetUserAsync(User);
         if (user == null)
         {
-            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
         }
 
-        await _userManager.SetTwoFactorEnabledAsync(user, false);
-        await _userManager.ResetAuthenticatorKeyAsync(user);
-        _logger.LogInformation("User with ID '{UserId}' has reset their authentication app key.", user.Id);
+        await userManager.SetTwoFactorEnabledAsync(user, false);
+        await userManager.ResetAuthenticatorKeyAsync(user);
+        logger.LogInformation("User with ID '{UserId}' has reset their authentication app key.", user.Id);
 
-        await _signInManager.RefreshSignInAsync(user);
+        await signInManager.RefreshSignInAsync(user);
         StatusMessage = "Your authenticator app key has been reset, you will need to configure your authenticator app using the new key.";
 
         return RedirectToPage("./EnableAuthenticator");
