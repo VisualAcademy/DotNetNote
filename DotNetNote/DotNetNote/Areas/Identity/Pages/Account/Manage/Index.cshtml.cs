0899c2a164dc65d91ac872701d35b8dde6143399
Park YongJune <redplus@redplus.net>
2024-07-21 15:51:52 +0900

    Use primary constructor (and remove fields)

--- DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -2,22 +2,11 @@
 
 namespace VisualAcademy.Areas.Identity.Pages.Account.Manage;
 
-public partial class IndexModel : PageModel
+public partial class IndexModel(
+    UserManager<ApplicationUser> userManager,
+    SignInManager<ApplicationUser> signInManager,
+    IEmailSender emailSender) : PageModel
 {
-    private readonly UserManager<ApplicationUser> _userManager;
-    private readonly SignInManager<ApplicationUser> _signInManager;
-    private readonly IEmailSender _emailSender;
-
-    public IndexModel(
-        UserManager<ApplicationUser> userManager,
-        SignInManager<ApplicationUser> signInManager,
-        IEmailSender emailSender)
-    {
-        _userManager = userManager;
-        _signInManager = signInManager;
-        _emailSender = emailSender;
-    }
-
     public string Username { get; set; }
 
     public bool IsEmailConfirmed { get; set; }
@@ -41,15 +30,15 @@ public partial class IndexModel : PageModel
 
     public async Task<IActionResult> OnGetAsync()
     {
-        var user = await _userManager.GetUserAsync(User);
+        var user = await userManager.GetUserAsync(User);
         if (user == null)
         {
-            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
         }
 
-        var userName = await _userManager.GetUserNameAsync(user);
-        var email = await _userManager.GetEmailAsync(user);
-        var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+        var userName = await userManager.GetUserNameAsync(user);
+        var email = await userManager.GetEmailAsync(user);
+        var phoneNumber = await userManager.GetPhoneNumberAsync(user);
 
         Username = userName;
 
@@ -59,7 +48,7 @@ public partial class IndexModel : PageModel
             PhoneNumber = phoneNumber
         };
 
-        IsEmailConfirmed = await _userManager.IsEmailConfirmedAsync(user);
+        IsEmailConfirmed = await userManager.IsEmailConfirmedAsync(user);
 
         return Page();
     }
@@ -71,35 +60,35 @@ public partial class IndexModel : PageModel
             return Page();
         }
 
-        var user = await _userManager.GetUserAsync(User);
+        var user = await userManager.GetUserAsync(User);
         if (user == null)
         {
-            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
         }
 
-        var email = await _userManager.GetEmailAsync(user);
+        var email = await userManager.GetEmailAsync(user);
         if (Input.Email != email)
         {
-            var setEmailResult = await _userManager.SetEmailAsync(user, Input.Email);
+            var setEmailResult = await userManager.SetEmailAsync(user, Input.Email);
             if (!setEmailResult.Succeeded)
             {
-                var userId = await _userManager.GetUserIdAsync(user);
+                var userId = await userManager.GetUserIdAsync(user);
                 throw new InvalidOperationException($"Unexpected error occurred setting email for user with ID '{userId}'.");
             }
         }
 
-        var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+        var phoneNumber = await userManager.GetPhoneNumberAsync(user);
         if (Input.PhoneNumber != phoneNumber)
         {
-            var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+            var setPhoneResult = await userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
             if (!setPhoneResult.Succeeded)
             {
-                var userId = await _userManager.GetUserIdAsync(user);
+                var userId = await userManager.GetUserIdAsync(user);
                 throw new InvalidOperationException($"Unexpected error occurred setting phone number for user with ID '{userId}'.");
             }
         }
 
-        await _signInManager.RefreshSignInAsync(user);
+        await signInManager.RefreshSignInAsync(user);
         StatusMessage = "Your profile has been updated";
         return RedirectToPage();
     }
@@ -111,22 +100,22 @@ public partial class IndexModel : PageModel
             return Page();
         }
 
-        var user = await _userManager.GetUserAsync(User);
+        var user = await userManager.GetUserAsync(User);
         if (user == null)
         {
-            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
         }
 
 
-        var userId = await _userManager.GetUserIdAsync(user);
-        var email = await _userManager.GetEmailAsync(user);
-        var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+        var userId = await userManager.GetUserIdAsync(user);
+        var email = await userManager.GetEmailAsync(user);
+        var code = await userManager.GenerateEmailConfirmationTokenAsync(user);
         var callbackUrl = Url.Page(
             "/Account/ConfirmEmail",
             pageHandler: null,
             values: new { userId = userId, code = code },
             protocol: Request.Scheme);
-        await _emailSender.SendEmailAsync(
+        await emailSender.SendEmailAsync(
             email,
             "Confirm your email",
             $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
