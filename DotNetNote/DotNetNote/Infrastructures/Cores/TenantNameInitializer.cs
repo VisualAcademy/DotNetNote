4c33147a83e4bcfa1e6316bdc894d16494c3fac7
Park YongJune <redplus@redplus.net>
2024-07-24 03:42:37 +0900

    TenantNameInitializer class add

---
+++ DotNetNote/DotNetNote/Infrastructures/Cores/TenantNameInitializer.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Dalbodre.Infrastructures
+{
+    public class TenantNameInitializer
+    {
+        private readonly string _connectionString;
+
+        public TenantNameInitializer(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void InitializeEmptyTenantNames()
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                SqlCommand cmdUpdate = new SqlCommand(@"
+                    UPDATE ApplicantsTransfers
+                    SET TenantName = 'VisualAcademy'
+                    WHERE TenantName IS NULL OR TenantName = ''", connection);
+
+                cmdUpdate.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+    }
+}
