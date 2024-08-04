d24d27a1cc98af29fe4523f51af4214f02bbc4ef
Park YongJune <redplus@redplus.net>
2024-08-04 09:22:27 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote.Models/Urls/Url.cs
+++ DotNetNote/DotNetNote.Models/Urls/Url.cs
@@ -1,37 +1,36 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace DotNetNote.Models
+namespace DotNetNote.Models;
+
+/// <summary>
+/// Url 모델 클래스: Urls 테이블과 일대일
+/// </summary>
+public class Url
 {
     /// <summary>
-    /// Url 모델 클래스: Urls 테이블과 일대일
+    /// 일련번호
     /// </summary>
-    public class Url
-    {
-        /// <summary>
-        /// 일련번호
-        /// </summary>
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        /// <summary>
-        /// 도메인
-        /// </summary>
-        [Required]
-        public string SiteUrl { get; set; }
+    /// <summary>
+    /// 도메인
+    /// </summary>
+    [Required]
+    public string SiteUrl { get; set; }
 
-        /// <summary>
-        /// 비고
-        /// </summary>
-        public string Content { get; set; }
+    /// <summary>
+    /// 비고
+    /// </summary>
+    public string Content { get; set; }
 
-        /// <summary>
-        /// 등록자
-        /// </summary>
-        public string UserName { get; set; }
+    /// <summary>
+    /// 등록자
+    /// </summary>
+    public string UserName { get; set; }
 
-        /// <summary>
-        /// 등록일: Created, DateReleased, CreatedAt, ...
-        /// </summary>
-        public DateTime Created { get; set; }
-    }
+    /// <summary>
+    /// 등록일: Created, DateReleased, CreatedAt, ...
+    /// </summary>
+    public DateTime Created { get; set; }
 }
