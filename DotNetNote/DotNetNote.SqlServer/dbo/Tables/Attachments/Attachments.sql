2c6bd7fd08e89d936d2012725867d749fcb5c6dd
Park YongJune <redplus@redplus.net>
2024-08-16 02:31:50 +0900

    Attachments table script add

---
+++ DotNetNote/DotNetNote.SqlServer/dbo/Tables/Attachments/Attachments.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Attachments] (
+    ID bigint IDENTITY(1,1) PRIMARY KEY,
+    DateCreated datetimeoffset(7) NOT NULL,
+    FileName nvarchar(max) NULL,
+    EmployeeID bigint NULL
+);
