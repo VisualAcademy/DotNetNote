a470db9ecc8d4d7270a19a115252141d14b24f0f
Park YongJune <redplus@redplus.net>
2024-09-12 19:56:06 +0900

    Remove unnecessary usings

--- DotNetNote/DotNetNote.Models/Books/Book.cs
+++ DotNetNote/DotNetNote.Models/Books/Book.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DotNetNote.Models.Books;
+﻿namespace DotNetNote.Models.Books;
 
 internal class Book
 {
