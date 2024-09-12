7c63f7baf81708419974529ba4a990fa2d4fe361
Park YongJune <redplus@redplus.net>
2024-09-12 19:55:51 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote.Models/Books/Book.cs
+++ DotNetNote/DotNetNote.Models/Books/Book.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DotNetNote.Models.Books
+namespace DotNetNote.Models.Books;
+
+internal class Book
 {
-    internal class Book
-    {
-        public int Id { get; set; }
-    }
+    public int Id { get; set; }
 }
