098d9d9ddf758d85342fb190ac7aab74159b9fb3
Park YongJune <redplus@redplus.net>
2024-09-12 19:31:02 +0900

    Book class add

---
+++ DotNetNote/DotNetNote.Models/Books/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetNote.Models.Books
+{
+    internal class Book
+    {
+        public int Id { get; set; }
+    }
+}
