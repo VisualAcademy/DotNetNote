936db2c85be2e57e7a0686720d4d6600cc7d2d28
Park YongJune <redplus@redplus.net>
2024-09-17 06:36:52 +0900

    .

---
+++ DotNetNote/DotNetNote/Areas/Identity/Pages/Account/GenerateTempPassword.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DotNetNote.Areas.Identity.Pages.Account
+{
+    public class GenerateTempPasswordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
