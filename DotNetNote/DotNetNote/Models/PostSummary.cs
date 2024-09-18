8763d1005ea5fbdd9d2f21e3109b95dcdda4e1b3
Park YongJune <redplus@redplus.net>
2024-09-19 08:31:21 +0900

    PostSummary class add

---
+++ DotNetNote/DotNetNote/Models/PostSummary.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetNote.Models
+{
+    public class PostSummary
+    {
+        public string BlogName { get; set; }
+        public string PostTitle { get; set; }
+        public DateOnly PublishedOn { get; set; }
+    }
+}
