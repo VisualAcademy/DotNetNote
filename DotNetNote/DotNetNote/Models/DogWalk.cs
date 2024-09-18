a82beacfbe822f9bced146b117bdd174a070eda5
Park YongJune <redplus@redplus.net>
2024-09-19 08:30:03 +0900

    DogWalk class add

---
+++ DotNetNote/DotNetNote/Models/DogWalk.cs
@@ -0,0 +1,21 @@
+﻿namespace DotNetNote.Models
+{
+    public class DogWalk
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public Terrain Terrain { get; set; }
+        public List<DateOnly> DaysVisited { get; private set; } = new();
+        public Pub? ClosestPub { get; set; }
+    }
+
+    public enum Terrain
+    {
+        Forest,
+        River,
+        Hills,
+        Village,
+        Park,
+        Beach,
+    }
+}
