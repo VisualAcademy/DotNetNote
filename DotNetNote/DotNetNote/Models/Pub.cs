cce7f488a2674942f3a015f924df0ad761bc361b
Park YongJune <redplus@redplus.net>
2024-09-19 08:29:23 +0900

    Pub class add

---
+++ DotNetNote/DotNetNote/Models/Pub.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetNote.Models
+{
+    public class Pub
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string[] Beers { get; set; }
+        public List<DateOnly> DaysVisited { get; private set; } = new();
+    }
+}
