dfe1fde478c14d030c7ad68b316ceb97cab66214
Park YongJune <redplus@redplus.net>
2024-09-19 08:27:59 +0900

    Address class add

---
+++ DotNetNote/DotNetNote/Models/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNetNote.Models
+{
+    public class Address
+    {
+        public required string Line1 { get; set; }
+        public string? Line2 { get; set; }
+        public required string City { get; set; }
+        public required string Country { get; set; }
+        public required string PostCode { get; set; }
+    }
+}
