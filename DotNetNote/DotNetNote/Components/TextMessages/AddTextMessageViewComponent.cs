69c29b3ffb180a1d2cf4168dbc28da8453f043a3
Park YongJune <redplus@redplus.net>
2024-10-08 12:06:44 +0900

    AddTextMessageViewComponent ViewComponent Add

---
+++ DotNetNote/DotNetNote/Components/TextMessages/AddTextMessageViewComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetNote.Components.TextMessages;
+
+public class AddTextMessageViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return await Task.FromResult(View());
+    }
+}
