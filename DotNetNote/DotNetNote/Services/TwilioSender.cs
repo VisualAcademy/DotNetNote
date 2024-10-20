75239b9f5ce89372ccf35940b528c236b81d47f9
Park YongJune <redplus@redplus.net>
2024-10-20 21:27:59 +0900

    TwilioSender initial create

---
+++ DotNetNote/DotNetNote/Services/TwilioSender.cs
@@ -0,0 +1,15 @@
+﻿namespace DotNetNote.Services
+{
+    public interface ITwilioSender
+    {
+        Task SendSmsAsync(string phoneNumber, string message);
+    }
+
+    public class TwilioSender : ITwilioSender
+    {
+        public Task SendSmsAsync(string phoneNumber, string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
