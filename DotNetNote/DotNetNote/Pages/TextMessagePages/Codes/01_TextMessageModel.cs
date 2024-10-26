b51e8ce581eb3f16387982d004e61158386015dc
Park YongJune <redplus@redplus.net>
2024-10-27 01:34:16 +0900

    TextMessageModel class body complete

--- DotNetNote/DotNetNote/Pages/TextMessagePages/Codes/01_TextMessageModel.cs
+++ DotNetNote/DotNetNote/Pages/TextMessagePages/Codes/01_TextMessageModel.cs
@@ -1,6 +1,24 @@
-﻿namespace DotNetNote.Pages.TextMessagePages.Codes
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VisualAcademy.Models.TextMessages
 {
+    [Table("TextMessages")]
     public class TextMessageModel
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Required]
+        public long EmployeeID { get; set; }
+
+        [Required]
+        public string Message { get; set; } = string.Empty;
+
+        public DateTimeOffset DateSent { get; set; } = DateTimeOffset.Now;
+
+        public int? TextMessageType { get; set; } // 1: Vendor, 2: Vendor Employee
     }
 }
