1cb9dace15b096e2406e21c0132705c61109dfa4
Park YongJune <redplus@redplus.net>
2024-11-03 06:27:21 +0900

    DeleteDialog component UI and Code Behind code add

--- DotNetNote/DotNetNote/Pages/TextMessagePages/Components/DeleteDialog.razor.cs
+++ DotNetNote/DotNetNote/Pages/TextMessagePages/Components/DeleteDialog.razor.cs
@@ -1,7 +1,35 @@
-﻿namespace DotNetNote.Pages.TextMessagePages.Components
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace VisualAcademy.Pages.TextMessages.Components
 {
     public partial class DeleteDialog
     {
+        #region Parameters
+        /// <summary>
+        /// 부모에서 OnClickCallback 속성에 지정한 이벤트 처리기 실행
+        /// </summary>
+        [Parameter]
+        public EventCallback<MouseEventArgs> OnClickCallback { get; set; }
+        #endregion
 
+        #region Properties
+        /// <summary>
+        /// 모달 다이얼로그를 표시할건지 여부 
+        /// </summary>
+        public bool IsShow { get; set; } = false;
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// 폼 보이기 
+        /// </summary>
+        public void Show() => IsShow = true;
+
+        /// <summary>
+        /// 폼 닫기
+        /// </summary>
+        public void Hide() => IsShow = false;
+        #endregion
     }
 }
