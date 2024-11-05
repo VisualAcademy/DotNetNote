e79c2aef00e7e37443a223dec0a0f498ec6f64da
Park YongJune <redplus@redplus.net>
2024-11-05 14:30:12 +0900

    IUserService interface code add

--- DotNetNote/DotNetNote/Services/Interfaces/IUserService.cs
+++ DotNetNote/DotNetNote/Services/Interfaces/IUserService.cs
@@ -2,5 +2,28 @@
 {
     public interface IUserService
     {
+        ApplicationUser GetUser();
+
+        ApplicationUser GetUserNotCached();
+
+        ApplicationUser GetUser(string email);
+
+        ApplicationUser GetUserById(string id);
+
+        ApplicationUser GetUserByIdCached(string id);
+
+        Task<ApplicationUser> GetUserAsync();
+
+        Task<ApplicationUser> GetUserAsyncCached();
+
+        Task<ApplicationUser> GetUserAsyncWithRoleInfo();
+
+        Task<TimeZoneInfo> GetTimeZoneInfoAsync();
+
+        void ConfirmEmail(string email);
+
+        bool HasAccessTo(string packageFeature);
+
+        Task RemoveUserCacheAsync();
     }
 }
