0fcc5980db68fe8540e9874bd285f07727216da6
Park YongJune <redplus@redplus.net>
2024-11-18 16:43:43 +0900

    BroadcastMessageController api controller class file add

---
+++ DotNetNote/DotNetNote/Apis/ApplicationsNotifications/BroadcastMessageController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Apis.ApplicationsNotifications
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BroadcastMessageController : ControllerBase
+    {
+    }
+}
