bc910d372eccb8b1b34d617a009b3284e5bc318a
Park YongJune <redplus@redplus.net>
2024-12-14 02:25:25 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Infrastructures/Cores/DocumentSchemaEnhancer.cs
+++ DotNetNote/DotNetNote/Infrastructures/Cores/DocumentSchemaEnhancer.cs
@@ -1,61 +1,61 @@
-﻿namespace Dalbodre.Infrastructures
+﻿namespace Dalbodre.Infrastructures;
+
+public class DocumentSchemaEnhancer
 {
-    public class DocumentSchemaEnhancer
+    private readonly string _connectionString;
+
+    public DocumentSchemaEnhancer(string connectionString)
     {
-        private readonly string _connectionString;
+        _connectionString = connectionString;
+    }
 
-        public DocumentSchemaEnhancer(string connectionString)
+    public void EnsureDocumentsTableAndAlterTitleColumn()
+    {
+        using (SqlConnection connection = new SqlConnection(_connectionString))
         {
-            _connectionString = connectionString;
-        }
+            connection.Open();
 
-        public void EnsureDocumentsTableAndAlterTitleColumn()
-        {
-            using (SqlConnection connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
-
-                SqlCommand cmdCheckTable = new SqlCommand(@"
+            SqlCommand cmdCheckTable = new SqlCommand(@"
                     SELECT COUNT(*) 
                     FROM INFORMATION_SCHEMA.TABLES 
                     WHERE TABLE_SCHEMA = 'dbo' 
                     AND TABLE_NAME = 'Documents'", connection);
 
-                int tableCount = (int)cmdCheckTable.ExecuteScalar();
+            int tableCount = (int)cmdCheckTable.ExecuteScalar();
 
-                if (tableCount > 0)
-                {
-                    SqlCommand cmdCheckColumn = new SqlCommand(@"
+            if (tableCount > 0)
+            {
+                SqlCommand cmdCheckColumn = new SqlCommand(@"
                         SELECT CHARACTER_MAXIMUM_LENGTH
                         FROM INFORMATION_SCHEMA.COLUMNS 
                         WHERE TABLE_NAME = 'Documents' 
                         AND COLUMN_NAME = 'Title'", connection);
 
-                    int? columnLength = (int?)cmdCheckColumn.ExecuteScalar();
+                int? columnLength = (int?)cmdCheckColumn.ExecuteScalar();
 
-                    if (columnLength != 512)
-                    {
-                        SqlCommand cmdAlterColumn = new SqlCommand(@"
+                if (columnLength != 512)
+                {
+                    SqlCommand cmdAlterColumn = new SqlCommand(@"
                             ALTER TABLE dbo.Documents 
                             ALTER COLUMN Title NVARCHAR(512) NOT NULL", connection);
 
-                        cmdAlterColumn.ExecuteNonQuery();
-                    }
+                    cmdAlterColumn.ExecuteNonQuery();
                 }
-
-                connection.Close();
             }
+
+            connection.Close();
         }
+    }
 
-        // Method to add the Language column if it doesn't exist
-        public void AddLanguageColumnIfNotExists()
+    // Method to add the Language column if it doesn't exist
+    public void AddLanguageColumnIfNotExists()
+    {
+        using (SqlConnection connection = new SqlConnection(_connectionString))
         {
-            using (SqlConnection connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
+            connection.Open();
 
-                // Check if the Language column exists in the Documents table
-                SqlCommand cmdCheckColumn = new SqlCommand(@"
+            // Check if the Language column exists in the Documents table
+            SqlCommand cmdCheckColumn = new SqlCommand(@"
                     IF NOT EXISTS (
                         SELECT * FROM INFORMATION_SCHEMA.COLUMNS 
                         WHERE TABLE_NAME = 'Documents' 
@@ -65,29 +65,28 @@
                         ALTER TABLE dbo.Documents ADD Language NVARCHAR(255) NULL DEFAULT 'ko-KR';
                     END", connection);
 
-                cmdCheckColumn.ExecuteNonQuery();
+            cmdCheckColumn.ExecuteNonQuery();
 
-                connection.Close();
-            }
+            connection.Close();
         }
+    }
 
-        // Method to update Language column values to 'en-US' if they are NULL
-        public void UpdateNullLanguageValuesToDefault()
+    // Method to update Language column values to 'en-US' if they are NULL
+    public void UpdateNullLanguageValuesToDefault()
+    {
+        using (SqlConnection connection = new SqlConnection(_connectionString))
         {
-            using (SqlConnection connection = new SqlConnection(_connectionString))
-            {
-                connection.Open();
+            connection.Open();
 
-                // Update Language column to 'en-US' where it is NULL
-                SqlCommand cmdUpdate = new SqlCommand(@"
+            // Update Language column to 'en-US' where it is NULL
+            SqlCommand cmdUpdate = new SqlCommand(@"
                     UPDATE dbo.Documents
                     SET Language = 'en-US'
                     WHERE Language IS NULL", connection);
 
-                cmdUpdate.ExecuteNonQuery();
+            cmdUpdate.ExecuteNonQuery();
 
-                connection.Close();
-            }
+            connection.Close();
         }
     }
 }
