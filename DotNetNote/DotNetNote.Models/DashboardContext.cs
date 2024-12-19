32cbc7ffb845c12876da674a958f95c5886e48a6
Park YongJune <redplus@redplus.net>
2024-12-19 20:40:54 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote.Models/DashboardContext.cs
+++ DotNetNote/DotNetNote.Models/DashboardContext.cs
@@ -1,33 +1,32 @@
 ﻿using DotNetNote.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace DotNetNote
+namespace DotNetNote;
+
+public class DashboardContext : DbContext
 {
-    public class DashboardContext : DbContext
+    public DbSet<Twelve> Twelves { get; set; }
+
+    public DashboardContext()
     {
-        public DbSet<Twelve> Twelves { get; set; }
+        // Empty
+    }
 
-        public DashboardContext()
+    /// <summary>
+    /// 생성자 매개변수에 옵션값 전달
+    /// </summary>
+    public DashboardContext(DbContextOptions<DashboardContext> options)
+        : base(options)
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
         {
-            // Empty
-        }
-
-        /// <summary>
-        /// 생성자 매개변수에 옵션값 전달
-        /// </summary>
-        public DashboardContext(DbContextOptions<DashboardContext> options)
-            : base(options)
-        {
-
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
-                optionsBuilder.UseSqlServer(connectionString);
-            }
+            string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            optionsBuilder.UseSqlServer(connectionString);
         }
     }
 }
