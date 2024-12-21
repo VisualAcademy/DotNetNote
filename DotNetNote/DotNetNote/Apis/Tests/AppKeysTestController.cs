db962123c47cda3e861276c5bfc58ddccc1910f6
Park YongJune <redplus@redplus.net>
2024-12-21 12:54:42 +0900

    AppKeysTestController comment

--- DotNetNote/DotNetNote/Apis/Tests/AppKeysTestController.cs
+++ DotNetNote/DotNetNote/Apis/Tests/AppKeysTestController.cs
@@ -1,36 +1,36 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetNote.Apis.Tests
-{
-    [Authorize(Roles = "Administrators")]
-    [ApiController]
-    [Route("api/[controller]")]
-    public class AppKeysTestController : ControllerBase
-    {
-        private readonly IConfiguration _configuration;
+//namespace DotNetNote.Apis.Tests
+//{
+//    [Authorize(Roles = "Administrators")]
+//    [ApiController]
+//    [Route("api/[controller]")]
+//    public class AppKeysTestController : ControllerBase
+//    {
+//        private readonly IConfiguration _configuration;
 
-        public AppKeysTestController(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
+//        public AppKeysTestController(IConfiguration configuration)
+//        {
+//            _configuration = configuration;
+//        }
 
-        [HttpGet("GetAppKeys")]
-        public IActionResult GetAppKeys()
-        {
-            var azureStorageAccount = _configuration["AppKeys:AzureStorageAccount"];
-            var azureStorageAccessKey = _configuration["AppKeys:AzureStorageAccessKey"];
+//        [HttpGet("GetAppKeys")]
+//        public IActionResult GetAppKeys()
+//        {
+//            var azureStorageAccount = _configuration["AppKeys:AzureStorageAccount"];
+//            var azureStorageAccessKey = _configuration["AppKeys:AzureStorageAccessKey"];
 
-            if (string.IsNullOrEmpty(azureStorageAccount) || string.IsNullOrEmpty(azureStorageAccessKey))
-            {
-                return NotFound("AppKeys 데이터가 설정되지 않았습니다.");
-            }
+//            if (string.IsNullOrEmpty(azureStorageAccount) || string.IsNullOrEmpty(azureStorageAccessKey))
+//            {
+//                return NotFound("AppKeys 데이터가 설정되지 않았습니다.");
+//            }
 
-            return Ok(new
-            {
-                AzureStorageAccount = azureStorageAccount,
-                AzureStorageAccessKey = azureStorageAccessKey
-            });
-        }
-    }
-}
+//            return Ok(new
+//            {
+//                AzureStorageAccount = azureStorageAccount,
+//                AzureStorageAccessKey = azureStorageAccessKey
+//            });
+//        }
+//    }
+//}
