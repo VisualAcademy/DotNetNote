2510a4015de3bbc2a094098b06d2c72404575ae4
Park YongJune <redplus@redplus.net>
2024-12-27 17:08:26 +0900

    Mission page add

---
+++ DotNetNote/DotNetNote.Web/Controllers/MissionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Web.Controllers
+{
+    public class MissionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
