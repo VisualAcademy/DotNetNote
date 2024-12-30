ef1a3cbb0a2e8fc4da49d60305e3edef7c24cb9a
Park YongJune <redplus@redplus.net>
2024-12-31 04:24:24 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote.Web/Pages/Index.cshtml.cs
+++ DotNetNote/DotNetNote.Web/Pages/Index.cshtml.cs
@@ -1,12 +1,11 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace DotNetNote.Web.Pages
-{
-    public class IndexModel(ILogger<IndexModel> logger) : PageModel
-    {
-        public void OnGet()
-        {
+namespace DotNetNote.Web.Pages;
 
-        }
+public class IndexModel(ILogger<IndexModel> logger) : PageModel
+{
+    public void OnGet()
+    {
+
     }
 }
