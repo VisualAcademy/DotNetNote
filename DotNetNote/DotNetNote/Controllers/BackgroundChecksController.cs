d5f906748e521060e3b641c39e99cec42abd092a
Park YongJune <redplus@redplus.net>
2025-01-14 16:42:53 +0900

    파일 범위 네임스페이스로 변환

--- DotNetNote/DotNetNote/Controllers/BackgroundChecksController.cs
+++ DotNetNote/DotNetNote/Controllers/BackgroundChecksController.cs
@@ -1,12 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace DotNetNote.Controllers
+namespace DotNetNote.Controllers;
+
+public class BackgroundChecksController : Controller
 {
-    public class BackgroundChecksController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        return View();
     }
 }
