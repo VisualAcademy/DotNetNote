7dd1332c3ec3455e107ac1507a4d582c79be85cd
Park YongJune <redplus@redplus.net>
2025-02-23 14:54:07 +0900

    Default files add.

---
+++ DotNetNote/All/Models/AccountViewModels/RegisterViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace All.Models.AccountViewModels
+{
+    class RegisterViewModel
+    {
+    }
+}
---
+++ DotNetNote/All/Services/Interfaces/IEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace All.Services.Interfaces
+{
+    interface IEmailSender
+    {
+    }
+}
