aeac8c30cc04f458eafa130a6a4c2401a1a6019d
Park YongJune <redplus@redplus.net>
2025-02-28 11:05:57 +0900

    Add RoleViewModel class files.

---
+++ DotNetNote/All/Models/RoleViewModels/ApplicationRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace All.Models.RoleViewModels
+{
+    public class ApplicationRoleViewModel
+    {
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Please provide a role name.")]
+        public string RoleName { get; set; }
+    }
+}
---
+++ DotNetNote/All/Models/RoleViewModels/IdentityRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace All.Models.RoleViewModels
+{
+    public class IdentityRoleViewModel
+    {
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Please provide a role name.")]
+        public string RoleName { get; set; }
+    }
+}
