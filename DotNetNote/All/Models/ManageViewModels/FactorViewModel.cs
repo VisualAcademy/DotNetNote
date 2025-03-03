5dd3cb09da54f8488c484d60b20e84a13000957d
Park YongJune <redplus@redplus.net>
2025-03-03 19:11:56 +0900

    Add FactorViewModel class file.

---
+++ DotNetNote/All/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace All.Models.ManageViewModels
+{
+    /// <summary>
+    /// 이 클래스는 인증 요소(Factor)에 대한 정보를 저장하는 ViewModel입니다.
+    /// </summary>
+    public class FactorViewModel
+    {
+        /// <summary>
+        /// 인증 요소의 목적을 나타내는 문자열입니다.
+        /// </summary>
+        public string Purpose { get; set; }
+    }
+}
