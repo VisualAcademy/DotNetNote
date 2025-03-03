4f303c7cdce84efe96606c85f2384b9b6ca1a330
Park YongJune <redplus@redplus.net>
2025-03-03 22:21:01 +0900

    Add OwnershipTypeEnum enumeration file.

---
+++ DotNetNote/All/Models/Enums/OwnershipTypeEnum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace All.Models.Enums
+{
+    public enum OwnershipTypeEnum
+    {
+        Employees, // 기본값(우선순위 높음)
+        Vendors
+    }
+
+    public static class OwnershipTypeHelper
+    {
+        /// <summary>
+        /// 문자열 값을 OwnershipTypeEnum으로 변환(기본값: Employees)
+        /// </summary>
+        public static OwnershipTypeEnum Parse(string value)
+        {
+            if (Enum.TryParse(value, true, out OwnershipTypeEnum result))
+            {
+                return result;
+            }
+            return OwnershipTypeEnum.Employees; // 기본값 Employees
+        }
+
+        /// <summary>
+        /// OwnershipTypeEnum을 문자열로 변환
+        /// </summary>
+        public static string ToStringValue(this OwnershipTypeEnum ownershipType)
+        {
+            return ownershipType.ToString();
+        }
+    }
+}
