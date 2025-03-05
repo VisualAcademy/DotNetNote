0b5bc21097868cc1fccecdc228a8e1965b942bcc
Park YongJune <redplus@redplus.net>
2025-03-06 04:15:48 +0900

    Add IStorageServiceBase interface file.

---
+++ DotNetNote/All/Services/Interfaces/IStorageServiceBase.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace All.Services.Interfaces
+{
+    public interface IStorageServiceBase
+    {
+        Task<bool> CreateDirectory(string folderPath);
+
+        Task<bool> DeleteAsync(string fileName, string folderPath);
+
+        Task<bool> DeleteDirectoryAsync(string folderPath);
+
+        Task<byte[]> DownloadAsync(string fileName, string folderPath);
+
+        Task<string> DownloadPathAsync(string fileName, string folderPath);
+
+        Task<MemoryStream> DownloadToMemoryStreamAsync(string fileName, string folderPath);
+
+        Task<MemoryStream> DownloadToMemoryStreamAsync(string fileName, string folderPath, string accountId);
+
+        string GetFolderPath(string ownerType, long ownerId, string fileType);
+
+        string GetFolderPath(string ownerType, string ownerId, string fileType);
+
+        Task<IDictionary<string, string>> GetMetadataAsync(string fileName, string folderPath);
+
+        Task<string> UploadAsync(byte[] bytes, string fileName, string folderPath, bool overwrite);
+
+        Task<string> UploadAsync(byte[] bytes, string fileName, string folderPath, bool overwrite, string accountId);
+
+        Task<string> UploadAsync(Stream stream, string fileName, string folderPath, bool overwrite);
+
+        Task<string> UploadAsync(Stream stream, string fileName, string folderPath, bool overwrite, string accountId);
+    }
+}
