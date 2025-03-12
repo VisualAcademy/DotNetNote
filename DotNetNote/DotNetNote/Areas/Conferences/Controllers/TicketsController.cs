a157348eed242cd48705cd91328125f47e00caf0
Park YongJune <redplus@redplus.net>
2025-03-12 22:41:53 +0900

    TicketsController initial create.

---
+++ DotNetNote/DotNetNote/Areas/Conferences/Controllers/TicketsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Areas.Conferences.Controllers
+{
+    public class TicketsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
