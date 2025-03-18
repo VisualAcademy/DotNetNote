4b2a4295d5a79be8b82021b8906abbf6eb05c48f
Park YongJune <redplus@redplus.net>
2025-03-18 22:17:55 +0900

    UserDetails - 사용자 기본 정보를 저장하는 DTO 클래스

---
+++ DotNetNote/All/Models/UserViewModels/UserDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace All.Models.UserViewModels
+{
+    /// <summary>
+    /// 사용자 기본 정보를 저장하는 DTO 클래스
+    /// </summary>
+    public class UserDetails
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
