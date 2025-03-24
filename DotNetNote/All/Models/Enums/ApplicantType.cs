80ad4acef675faee84a82026a581fc41fb79e0f4
Park YongJune <redplus@redplus.net>
2025-03-24 15:10:45 +0900

    Create ApplicantType enumeration file.

---
+++ DotNetNote/All/Models/Enums/ApplicantType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace All.Models.Enums
+{
+    [Flags]
+    public enum ApplicantType
+    {
+        [Description("Vendor")]
+        Vendor = 1, // 0001
+
+        [Description("Employee")]
+        Employee = 2 // 0010
+    }
+}
