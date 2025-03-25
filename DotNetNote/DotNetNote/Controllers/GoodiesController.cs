7eb85edbbe33a461f8b9dad3cde0be01c348fdf8
Park YongJune <redplus@redplus.net>
2025-03-26 03:09:49 +0900

    Add countdowwn timer for Microsoft BUILD 2025 event

---
+++ DotNetNote/DotNetNote/Controllers/GoodiesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Controllers
+{
+    public class GoodiesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
