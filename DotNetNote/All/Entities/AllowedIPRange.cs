a76418c13c92dd6dbf7446c2167e9e67c125fb87
Park YongJune <redplus@redplus.net>
2025-03-27 06:05:57 +0900

    Add AllowedIPRange class file.

---
+++ DotNetNote/All/Entities/AllowedIPRange.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace All.Entities
+{
+    public class AllowedIPRange
+    {
+        public int Id { get; set; }
+        public string StartIPRange { get; set; }
+        public string EndIPRange { get; set; }
+        public string Description { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now; // 기본값 설정
+        public long TenantId { get; set; }
+    }
+}
