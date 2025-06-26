073072da3222d3f440b7c5616f686a3a0d360570
Park YongJune <redplus@redplus.net>
2025-06-27 05:57:25 +0900

    Add Divisions table

---
+++ DotNetNote/DotNetNote.SqlServer/dbo/Tables/Divisions.sql
@@ -0,0 +1,9 @@
+﻿--[0][0] 디비전: Divisions 
+CREATE TABLE [dbo].[Divisions]
+(
+    [Id]        BIGINT             IDENTITY (1, 1) NOT NULL PRIMARY KEY,    -- 디비전 고유 아이디, 자동 증가
+    [Active]    BIT                DEFAULT ((1)) NULL,                      -- 활성 상태 표시, 기본값 1 (활성)
+    [CreatedAt] DATETIMEOFFSET NULL DEFAULT SYSDATETIMEOFFSET(),            -- 레코드 생성 시간
+    [CreatedBy] NVARCHAR (255)     NULL,                                    -- 레코드 생성자 이름
+    [Name]      NVARCHAR (MAX)     NULL                                     -- 이름
+);
