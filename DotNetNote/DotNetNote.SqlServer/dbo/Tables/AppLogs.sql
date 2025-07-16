9f8145c54140ba02e348b9357c4304e18ebc6858
Park YongJune <redplus@redplus.net>
2025-07-16 22:14:35 +0900

    Add AppLogs table

---
+++ DotNetNote/DotNetNote.SqlServer/dbo/Tables/AppLogs.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE AppLogs (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Message NVARCHAR(MAX),
+    MessageTemplate NVARCHAR(MAX),
+    Level NVARCHAR(128),
+    TimeStamp DATETIMEOFFSET,
+    Exception NVARCHAR(MAX),
+    Properties NVARCHAR(MAX)
+);
