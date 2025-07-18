afc2c8875d9f486c5e08de479149a87b98963d24
Park YongJune <redplus@redplus.net>
2025-07-18 14:24:32 +0900

    Add KnownUser model class

---
+++ DotNetNote/DotNetNote/Models/KnownUser.cs
@@ -0,0 +1,15 @@
+﻿namespace DotNetNote.Models
+{
+    public class KnownUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = default!;
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
