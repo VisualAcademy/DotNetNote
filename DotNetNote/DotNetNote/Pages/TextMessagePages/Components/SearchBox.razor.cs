9fef98aa4288a97cd28f1b34af9c5a014efad745
Park YongJune <redplus@redplus.net>
2025-08-18 00:35:15 +0900

    Code Cleanup

--- DotNetNote/DotNetNote/Pages/TextMessagePages/Components/SearchBox.razor.cs
+++ DotNetNote/DotNetNote/Pages/TextMessagePages/Components/SearchBox.razor.cs
@@ -6,13 +6,13 @@ namespace VisualAcademy.Pages.TextMessages.Components
     public partial class SearchBox : ComponentBase, IDisposable
     {
         #region Fields
-        private string searchQuery;
-        private System.Timers.Timer debounceTimer;
+        private string searchQuery = "";
+        private System.Timers.Timer? debounceTimer;
         #endregion
 
         #region Parameters
         [Parameter(CaptureUnmatchedValues = true)]
-        public IDictionary<string, object> AdditionalAttributes { get; set; }
+        public IDictionary<string, object> AdditionalAttributes { get; set; } = new Dictionary<string, object>();
 
         // 자식 컴포넌트에서 발생한 정보를 부모 컴포넌트에게 전달
         [Parameter]
@@ -29,8 +29,8 @@ namespace VisualAcademy.Pages.TextMessages.Components
             set
             {
                 searchQuery = value;
-                debounceTimer.Stop(); // 텍스트박스에 값을 입력하는 동안 타이머 중지
-                debounceTimer.Start(); // 타이머 실행(300밀리초 후에 딱 한 번 실행)
+                debounceTimer?.Stop(); // 텍스트박스에 값을 입력하는 동안 타이머 중지
+                debounceTimer?.Start(); // 타이머 실행(300밀리초 후에 딱 한 번 실행)
             }
         }
         #endregion
@@ -44,7 +44,7 @@ namespace VisualAcademy.Pages.TextMessages.Components
             debounceTimer = new System.Timers.Timer();
             debounceTimer.Interval = Debounce;
             debounceTimer.AutoReset = false; // 딱 한번 실행 
-            debounceTimer.Elapsed += SearchHandler;
+            debounceTimer.Elapsed += SearchHandler!;
         }
         #endregion
 
@@ -55,7 +55,7 @@ namespace VisualAcademy.Pages.TextMessages.Components
         #endregion
 
         #region Public Methods
-        public void Dispose() => debounceTimer.Dispose();
+        public void Dispose() => debounceTimer?.Dispose();
         #endregion
     }
 }
