0913126241b0a2e367fd672beada1d447c517b54
Park YongJune <redplus@redplus.net>
2025-08-22 04:16:04 +0900

    Add SignIns and AnonymousSignIns table script

---
+++ DotNetNote/DotNetNote.SqlServer/dbo/Tables/AnonymousSignIns.sql
@@ -0,0 +1,9 @@
+﻿-- 익명 사용자 로그인 이력 테이블
+CREATE TABLE [dbo].[AnonymousSignIns] (
+    [ID]               BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY,  -- 기본 키(자동 증가)
+    [DateTimeSignedIn] DATETIMEOFFSET(0) NOT NULL,                 -- 로그인 시각
+    [Email]            NVARCHAR(MAX) NOT NULL,                     -- 입력된 이메일
+    [Result]           NVARCHAR(MAX) NOT NULL,                     -- 로그인 결과(성공/실패 등)
+    [IPAddress]        NVARCHAR(MAX) NULL,                         -- 클라이언트 IP
+    [Note]             NVARCHAR(MAX) NULL                          -- 비고
+);
---
+++ DotNetNote/DotNetNote.SqlServer/dbo/Tables/SignIns.sql
@@ -0,0 +1,12 @@
+﻿-- 사용자 로그인 이력 테이블
+CREATE TABLE [dbo].[SignIns] ( 
+    [ID] [bigint] IDENTITY(1,1) NOT NULL PRIMARY KEY, -- 기본 키(자동 증가)
+    [DateTimeSignedIn] [datetimeoffset](0) NOT NULL,  -- 로그인 시각
+    [UserID] [nvarchar](450) NULL,                    -- 사용자 ID
+    [Email] [nvarchar](max) NOT NULL,                 -- 로그인에 사용된 이메일
+    [FirstName] [nvarchar](max) NULL,                 -- 이름
+    [LastName] [nvarchar](max) NULL,                  -- 성
+    [Result] [nvarchar](max) NOT NULL,                -- 로그인 결과(예: 성공/실패)
+    [IPAddress] [nvarchar](max) NULL,                 -- 클라이언트 IP 주소
+    [Note] [nvarchar](max) NULL                       -- 비고/메모
+);
