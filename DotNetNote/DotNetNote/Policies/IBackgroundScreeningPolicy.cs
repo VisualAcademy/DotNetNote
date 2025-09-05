a77f468573414beee8ab1a1dd8cf86a61f786177
Park YongJune <redplus@redplus.net>
2025-09-06 05:04:20 +0900

    Add IBackgroundScreeningPolicy interface

---
+++ DotNetNote/DotNetNote/Policies/IBackgroundScreeningPolicy.cs
@@ -0,0 +1,9 @@
+﻿namespace Azunt.Web.Policies;
+
+public interface IBackgroundScreeningPolicy
+{
+    /// <summary>
+    /// 지정한 Provider가 현재 tenantName에서 노출/사용 가능한지 여부
+    /// </summary>
+    bool IsProviderVisible(string provider, string? tenantName);
+}
