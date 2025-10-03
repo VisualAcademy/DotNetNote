2ef129cfa239c38470994347346b414738c44ee6
Park YongJune <redplus@redplus.net>
2025-10-03 23:54:28 +0900

    .

---
+++ DotNetNote/DotNetNote/Areas/Employees/Controllers/TemplatesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Areas.Employees.Controllers
+{
+    public class TemplatesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Employee Area");
+        }
+    }
+}
