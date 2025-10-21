0ee473fad5948fcb72103fe242d5ad6de9deeb2b
Park YongJune <redplus@redplus.net>
2025-10-21 21:06:18 +0900

    AppLog.cs

---
+++ DotNetNote/DotNetNote/Models/AppLog.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetNote.Models
+{
+    // Serilog 표준 테이블 스키마 (Id 없음)
+    public class AppLog
+    {
+        public string? Message { get; set; }
+        public string? MessageTemplate { get; set; }
+        public string? Level { get; set; }
+        public DateTimeOffset? TimeStamp { get; set; }
+        public string? Exception { get; set; }
+        public string? Properties { get; set; }
+    }
+}
