b8329f8786c7cf95b04043562f70bddaf433dcbc
Park YongJune <redplus@redplus.net>
2025-10-21 21:07:00 +0900

    LogsDbContext.cs

---
+++ DotNetNote/DotNetNote/Data/LogsDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace DotNetNote.Data
+{
+    public class LogsDbContext : DbContext
+    {
+        public LogsDbContext(DbContextOptions<LogsDbContext> options) : base(options) { }
+        public DbSet<AppLog> AppLogs => Set<AppLog>();
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<AppLog>(e =>
+            {
+                e.ToTable("AppLogs");   // dbo.AppLogs
+                e.HasNoKey();           // 키 없음
+                e.HasIndex(nameof(AppLog.TimeStamp));
+                e.HasIndex(nameof(AppLog.Level));
+            });
+        }
+    }
+}
