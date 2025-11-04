ce88052914d576b2f4c9380730b2b58a63476641
Park YongJune <redplus@redplus.net>
2025-11-04 14:32:39 +0900

    Remove unnecessary usings

--- DotNetNote/DotNetNote/Controllers/Administrations/UserRoleManagement/UserRoleManagementController.cs
+++ DotNetNote/DotNetNote/Controllers/Administrations/UserRoleManagement/UserRoleManagementController.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace DotNetNote.Controllers.Administrations.UserRoleManagement
+﻿namespace DotNetNote.Controllers.Administrations.UserRoleManagement
 {
     public class UserRoleManagementController : Controller
     {
