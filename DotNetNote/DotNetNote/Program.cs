2e187bb137bfe2776351900bf5a505a94c67e0fc
Park YongJune <redplus@redplus.net>
2025-11-16 16:14:42 +0900

    DotNetNote 프로젝트에 Web API 기본 템플릿의 샘플 Web API를 적용하고 실행하기

--- DotNetNote/DotNetNote/Program.cs
+++ DotNetNote/DotNetNote/Program.cs
@@ -163,9 +163,16 @@ public partial class Program
         builder.Services.AddDbContext<LogsDbContext>(opt =>
             opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
         var app = builder.Build();
 
-
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
 
 
         // <TodoComponent>
@@ -336,6 +343,8 @@ public partial class Program
         // Minimal APIs
         app.MapAzuntMinimalApis();
 
+        app.MapControllers();
+
         app.Run();
     }
 
@@ -347,7 +356,9 @@ public partial class Program
 
         //services.AddDbContext<DotNetNote.Components.TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
         services.AddHttpContextAccessor();
+
         services.AddControllersWithViews();
+
         services.AddRazorPages();
         services.AddServerSideBlazor();
 
