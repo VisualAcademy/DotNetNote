8b8a2dc176f6b8ab2d0cdeecd14fac0fe7f485d6
Park YongJune <redplus@redplus.net>
2025-11-27 09:53:48 +0900

    Add CheckoutController class file

---
+++ temp/DotNetNote/DotNetNote/Controllers/CheckoutController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        // GET: CheckoutController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CheckoutController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CheckoutController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CheckoutController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CheckoutController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CheckoutController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CheckoutController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CheckoutController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
