60edb226bb163e79cf70e5adb50b544a9a47a98c
Park YongJune <redplus@redplus.net>
2025-11-28 02:10:36 +0900

    Code Cleanup

--- DotNetNote/DotNetNote/Models/AccountViewModels/LoginViewModel.cs
+++ DotNetNote/DotNetNote/Models/AccountViewModels/LoginViewModel.cs
@@ -6,11 +6,11 @@ public class LoginViewModel
 {
     [Required]
     [EmailAddress]
-    public string Email { get; set; }
+    public string Email { get; set; } = string.Empty;
 
     [Required]
     [DataType(DataType.Password)]
-    public string Password { get; set; }
+    public string Password { get; set; } = string.Empty;
 
     [Display(Name = "Remember me?")]
     public bool RememberMe { get; set; }
