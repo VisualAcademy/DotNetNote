6bb182b511fca2db5fbadd16cec3b0972edd25ff
Park YongJune <redplus@redplus.net>
2025-12-18 12:50:34 +0900

    Add Clients table

---
+++ DotNetNote/DotNetNote.SqlServer/dbo/Tables/Clients.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Clients]
+(
+    [Id]               BIGINT IDENTITY(1,1) NOT NULL,
+    [TenantId]         NVARCHAR(64)  NOT NULL,
+    [OrganizationName] NVARCHAR(200) NOT NULL,
+    [BillingEmail]     NVARCHAR(200) NOT NULL,
+    [Domain]           NVARCHAR(200) NULL,
+    [Type]             INT NOT NULL,
+
+    CONSTRAINT [PK_Clients] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+GO
