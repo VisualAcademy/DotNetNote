6f78b3adb5ba84414012ad8b5509bc33fe245189
Park YongJune <redplus@redplus.net>
2025-12-19 11:45:30 +0900

    Add InvoiceNumberSequences script

---
+++ DotNetNote/DotNetNote.SqlServer/dbo/Tables/InvoiceNumberSequences.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[InvoiceNumberSequences]
+(
+    -- EF Core convention상 string PK 길이 제한이 없으면 보통 NVARCHAR(450)로 잡힙니다.
+    [TenantId]   NVARCHAR(450) NOT NULL,
+    [NextValue]  BIGINT NOT NULL,
+
+    CONSTRAINT [PK_InvoiceNumberSequences] PRIMARY KEY CLUSTERED ([TenantId] ASC)
+);
+GO
