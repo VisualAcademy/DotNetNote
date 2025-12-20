0e76d9cda66f0d88d476935ec0785bfedef5d451
Park YongJune <redplus@redplus.net>
2025-12-20 11:52:23 +0900

    Add InvoiceItems table script

---
+++ DotNetNote/DotNetNote.SqlServer/dbo/Tables/InvoiceItems.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[InvoiceItems]
+(
+    [Id]          BIGINT IDENTITY(1,1) NOT NULL,
+    [InvoiceId]   BIGINT NOT NULL,
+    [Description] NVARCHAR(200) NOT NULL,
+    [Quantity]    DECIMAL(18,2) NOT NULL,
+    [UnitPrice]   DECIMAL(18,2) NOT NULL,
+
+    CONSTRAINT [PK_InvoiceItems] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+GO
