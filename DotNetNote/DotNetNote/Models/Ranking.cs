9eee774994bb1275837fcc3720c5b6c0bc88eff3
Park YongJune <redplus@redplus.net>
2026-01-02 16:36:21 +0900

    Code Cleanup

--- DotNetNote/DotNetNote/Models/Ranking.cs
+++ DotNetNote/DotNetNote/Models/Ranking.cs
@@ -3,7 +3,7 @@
 public class Ranking
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = "";
     public int Rank { get; set; }
-    public string RankImage { get; set; }
+    public string RankImage { get; set; } = "";
 }
