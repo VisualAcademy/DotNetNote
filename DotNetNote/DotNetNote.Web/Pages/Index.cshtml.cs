fb34d05cd984d7b347a2eb6a16fd12a97258b034
Park YongJune <redplus@redplus.net>
2026-01-18 05:01:53 +0900

    Store logger as a field and use it in Index page model

--- DotNetNote/DotNetNote.Web/Pages/Index.cshtml.cs
+++ DotNetNote/DotNetNote.Web/Pages/Index.cshtml.cs
@@ -2,10 +2,17 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DotNetNote.Web.Pages;
 
-public class IndexModel(ILogger<IndexModel> logger) : PageModel
+public class IndexModel : PageModel
 {
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
     public void OnGet()
     {
-
+        _logger.LogInformation("Index page accessed");
     }
 }
