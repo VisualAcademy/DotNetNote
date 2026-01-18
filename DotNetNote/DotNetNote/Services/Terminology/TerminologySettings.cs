f49f2bcff76b3620cfaa4ab5b6aa36651bc80fe2
Park YongJune <redplus@redplus.net>
2026-01-19 05:36:26 +0900

    Add TerminologySettings model

---
+++ DotNetNote/DotNetNote/Services/Terminology/TerminologySettings.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetNote.Services.Terminology;
+
+public sealed class TerminologySettings
+{
+    public string AppMode { get; set; } = "VisualAcademy";
+
+    // Terminology[Mode][Key] = Value
+    public Dictionary<string, Dictionary<string, string>> Terminology { get; set; } = new();
+}
