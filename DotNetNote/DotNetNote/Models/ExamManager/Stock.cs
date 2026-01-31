bcafe316ea6e959762ad83ce2d768d7379f416e3
Park YongJune <redplus@redplus.net>
2026-01-31 15:13:23 +0900

    Resolve nullable warning by ensuring the non-nullable Name property is properly initialized.

--- DotNetNote/DotNetNote/Models/ExamManager/Stock.cs
+++ DotNetNote/DotNetNote/Models/ExamManager/Stock.cs
@@ -2,7 +2,7 @@
 {
     public class Stock
     {
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public double Price { get; set; }
         public double LastPrice { get; set; }
     }
