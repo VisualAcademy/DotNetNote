fa991d02d537282720a0388a22998ad0886bfb6a
Park YongJune <redplus@redplus.net>
2026-02-17 02:19:33 +0900

    Initialize Email and Password with default empty strings to ensure null-safety in SignViewModel

--- DotNetNote/DotNetNote/Models/Sign/SignViewModel.cs
+++ DotNetNote/DotNetNote/Models/Sign/SignViewModel.cs
@@ -2,7 +2,7 @@
 
 public class SignViewModel
 {
-    public string Email { get; set; }
+    public string Email { get; set; } = string.Empty;
 
-    public string Password { get; set; }
+    public string Password { get; set; } = string.Empty;
 }
