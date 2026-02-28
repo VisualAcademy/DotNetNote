5767ee982b7d9758dedbb9774dfe17b2d4bee36a
Park YongJune <redplus@redplus.net>
2026-02-28 23:24:47 +0900

    Apply string.Empty default initialization to non-nullable propertie

--- DotNetNote/DotNetNote/Models/TodoItem.cs
+++ DotNetNote/DotNetNote/Models/TodoItem.cs
@@ -3,15 +3,15 @@
     public class TodoItem
     {
         public long Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public bool IsComplete { get; set; }
-        public string Secret { get; set; }
+        public string Secret { get; set; } = string.Empty;
     }
 
     public class TodoItemDTO
     {
         public long Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public bool IsComplete { get; set; }
     }
 }
