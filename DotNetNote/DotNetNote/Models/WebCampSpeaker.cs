1b0abed7dfda28367629d1e78555b84894d78f2a
Park YongJune <redplus@redplus.net>
2026-03-05 16:33:51 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Models/WebCampSpeaker.cs
+++ DotNetNote/DotNetNote/Models/WebCampSpeaker.cs
@@ -1,11 +1,10 @@
-﻿namespace DotNetNote.Models
+﻿namespace DotNetNote.Models;
+
+public class WebCampSpeaker
 {
-    public class WebCampSpeaker
-    {
-        public int Id { get; set; }                 // 번호
-        public string Name { get; set; } = string.Empty;            // 이름
-        public string Photo { get; set; } = string.Empty;           // 사진 파일명
-        public string Title { get; set; } = string.Empty;           // 회사 정보, 직책
-        public string Description { get; set; } = string.Empty;     // 소개
-    }
+    public int Id { get; set; }                 // 번호
+    public string Name { get; set; } = string.Empty;            // 이름
+    public string Photo { get; set; } = string.Empty;           // 사진 파일명
+    public string Title { get; set; } = string.Empty;           // 회사 정보, 직책
+    public string Description { get; set; } = string.Empty;     // 소개
 }
