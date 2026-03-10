c28f9ae645fe6cbb6e4f37e8ef435f027639b55e
Park YongJune <redplus@redplus.net>
2026-03-10 22:12:01 +0900

    Initialize string properties with default empty values

--- DotNetNote/DotNetNote/Models/Article.cs
+++ DotNetNote/DotNetNote/Models/Article.cs
@@ -4,9 +4,9 @@ public class Article
 {
     public int Id { get; set; }
 
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 
-    public string Title { get; set; }
+    public string Title { get; set; } = string.Empty;
 
     public DateTime PostDate { get; set; }
 }
