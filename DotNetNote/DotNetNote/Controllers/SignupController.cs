e24e97c5ed85471313975fc5239d9c81f4ec7466
Park YongJune <redplus@redplus.net>
2026-03-12 20:41:28 +0900

    Add dedicated SignupController for /signup route without modifying AccountController

---
+++ DotNetNote/DotNetNote/Controllers/SignupController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VisualAcademy.Controllers
+{
+    public class SignupController : Controller
+    {
+        [HttpGet("/signup")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("/signup")]
+        public IActionResult Index(string username, string email, string password)
+        {
+            // TODO: 회원가입 처리
+            // 예: DB 저장, Identity User 생성 등
+
+            return Content($"가입 완료: {username}, {email}");
+        }
+    }
+}
