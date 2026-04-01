afbe2573242f13b5dbc2919587e554f1aeb61fac
Park YongJune <redplus@redplus.net>
2026-04-01 21:14:02 +0900

    Handle nullable configuration values in AppSettingsDemo

--- DotNetNote/DotNetNote/Controllers/AppSettingsDemo.cs
+++ DotNetNote/DotNetNote/Controllers/AppSettingsDemo.cs
@@ -1,18 +1,21 @@
-﻿namespace DotNetNote.Controllers;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Controllers;
 
 [Authorize(Roles = "Administrators")]
 public class AppSettingsDemo(IConfiguration configuration) : Controller
 {
     public IActionResult Index()
     {
-        string con1 = configuration.GetSection("StorageConnectionString1").Value;
+        string con1 = configuration.GetSection("StorageConnectionString1").Value ?? "";
 
         string site1 = configuration
             .GetSection("BlobStorageConnectionString")
-            .GetSection("Site1").Value;
+            .GetSection("Site1").Value ?? "";
 
         string site2 = configuration
-            .GetValue<string>("BlobStorageConnectionString:Site2");
+            .GetValue<string>("BlobStorageConnectionString:Site2") ?? "";
 
         return Content($"{con1}, {site1}, {site2}");
     }
