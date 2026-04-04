1c0c7667ba2bfcec238b47375530465a9bb2a6c2
Park YongJune <redplus@redplus.net>
2026-04-05 03:05:12 +0900

    Make TempData properties nullable in GenerateRecoveryCodesModel Safely handle possible null recovery codes before converting to an array

--- DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/GenerateRecoveryCodes.cshtml.cs
+++ DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/GenerateRecoveryCodes.cshtml.cs
@@ -7,10 +7,10 @@ public class GenerateRecoveryCodesModel(
     ILogger<GenerateRecoveryCodesModel> logger) : PageModel
 {
     [TempData]
-    public string[] RecoveryCodes { get; set; }
+    public string[]? RecoveryCodes { get; set; }
 
     [TempData]
-    public string StatusMessage { get; set; }
+    public string? StatusMessage { get; set; }
 
     public async Task<IActionResult> OnGetAsync()
     {
@@ -24,7 +24,8 @@ public class GenerateRecoveryCodesModel(
         if (!isTwoFactorEnabled)
         {
             var userId = await userManager.GetUserIdAsync(user);
-            throw new InvalidOperationException($"Cannot generate recovery codes for user with ID '{userId}' because they do not have 2FA enabled.");
+            throw new InvalidOperationException(
+                $"Cannot generate recovery codes for user with ID '{userId}' because they do not have 2FA enabled.");
         }
 
         return Page();
@@ -40,16 +41,20 @@ public class GenerateRecoveryCodesModel(
 
         var isTwoFactorEnabled = await userManager.GetTwoFactorEnabledAsync(user);
         var userId = await userManager.GetUserIdAsync(user);
+
         if (!isTwoFactorEnabled)
         {
-            throw new InvalidOperationException($"Cannot generate recovery codes for user with ID '{userId}' as they do not have 2FA enabled.");
+            throw new InvalidOperationException(
+                $"Cannot generate recovery codes for user with ID '{userId}' as they do not have 2FA enabled.");
         }
 
         var recoveryCodes = await userManager.GenerateNewTwoFactorRecoveryCodesAsync(user, 10);
-        RecoveryCodes = recoveryCodes.ToArray();
+        RecoveryCodes = recoveryCodes?.ToArray() ?? Array.Empty<string>();
 
         logger.LogInformation("User with ID '{UserId}' has generated new 2FA recovery codes.", userId);
+
         StatusMessage = "You have generated new recovery codes.";
+
         return RedirectToPage("./ShowRecoveryCodes");
     }
 }
