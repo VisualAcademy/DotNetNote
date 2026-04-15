1cf4bc0aefa6b9356755995cc78903af4d3b4981
Park YongJune <redplus@redplus.net>
2026-04-15 10:15:06 +0900

    Fix nullablity warnings in SetPassword page model

--- DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/SetPassword.cshtml.cs
+++ DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/SetPassword.cshtml.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace VisualAcademy.Areas.Identity.Pages.Account.Manage;
 
@@ -7,10 +10,10 @@ public class SetPasswordModel(
     SignInManager<ApplicationUser> signInManager) : PageModel
 {
     [BindProperty]
-    public InputModel Input { get; set; }
+    public InputModel Input { get; set; } = new();
 
     [TempData]
-    public string StatusMessage { get; set; }
+    public string? StatusMessage { get; set; }
 
     public class InputModel
     {
@@ -18,12 +21,12 @@ public class SetPasswordModel(
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "New password")]
-        public string NewPassword { get; set; }
+        public string? NewPassword { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm new password")]
         [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
+        public string? ConfirmPassword { get; set; }
     }
 
     public async Task<IActionResult> OnGetAsync()
@@ -57,13 +60,14 @@ public class SetPasswordModel(
             return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
         }
 
-        var addPasswordResult = await userManager.AddPasswordAsync(user, Input.NewPassword);
+        var addPasswordResult = await userManager.AddPasswordAsync(user, Input.NewPassword!);
         if (!addPasswordResult.Succeeded)
         {
             foreach (var error in addPasswordResult.Errors)
             {
                 ModelState.AddModelError(string.Empty, error.Description);
             }
+
             return Page();
         }
 
