3abaa151c6dfd4981ed2903e7586f681773b51e8
Park YongJune <redplus@redplus.net>
2026-04-15 10:19:05 +0900

    Remove unnecessary usings

--- DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/TwoFactorAuthentication.cshtml.cs
+++ DotNetNote/DotNetNote/Areas/Identity/Pages/Account/Manage/TwoFactorAuthentication.cshtml.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace VisualAcademy.Areas.Identity.Pages.Account.Manage;
 
