7d68b30673cb2be504b78216d7b521518dc8ad5e
Park YongJune <redplus@redplus.net>
2026-04-29 10:58:49 +0900

    Handle Missing Persisted Choice During Exam Grading

--- DotNetNote/DotNetNote/Models/ExamManager/ExamService.cs
+++ DotNetNote/DotNetNote/Models/ExamManager/ExamService.cs
@@ -6,63 +6,66 @@ public class ExamService
 {
     public Exam GetExam()
     {
-        var exam = new Exam() { Id=100, Name = "중간 고사"};
+        var exam = new Exam() { Id = 100, Name = "중간 고사" };
         exam.AddQuestion(GetQuestions());
 
-        return exam; 
+        return exam;
     }
 
     private IList<Question> GetQuestions()
     {
         var questions = new List<Question>()
-            {
-                new Question() { Title = "What is 2+2", Point = 10, Id = 1, OrderNumber = 0}, 
-                new Question() { Title = "What is 5+2", Point = 10, Id =2, OrderNumber = 1},
-                new Question() { Title="What is 10+2", Point =5, Id=3, OrderNumber = 2}
-            };
+        {
+            new Question() { Title = "What is 2+2", Point = 10, Id = 1, OrderNumber = 0 },
+            new Question() { Title = "What is 5+2", Point = 10, Id = 2, OrderNumber = 1 },
+            new Question() { Title = "What is 10+2", Point = 5, Id = 3, OrderNumber = 2 }
+        };
 
-        questions[0].AddChoice(new Choice() { IsAnswer = false, ChoiceText = "2", Id = 1});
-        questions[0].AddChoice(new Choice(){ IsAnswer = true, ChoiceText = "4",Id=2});
+        questions[0].AddChoice(new Choice() { IsAnswer = false, ChoiceText = "2", Id = 1 });
+        questions[0].AddChoice(new Choice() { IsAnswer = true, ChoiceText = "4", Id = 2 });
 
-        questions[1].AddChoice(new Choice() { IsAnswer = false, ChoiceText = "12", Id=3});
-        questions[1].AddChoice(new Choice() { IsAnswer = true, ChoiceText = "7", Id=4});
+        questions[1].AddChoice(new Choice() { IsAnswer = false, ChoiceText = "12", Id = 3 });
+        questions[1].AddChoice(new Choice() { IsAnswer = true, ChoiceText = "7", Id = 4 });
 
         questions[2].AddChoice(new Choice() { IsAnswer = true, ChoiceText = "12", Id = 5 });
         questions[2].AddChoice(new Choice() { IsAnswer = false, ChoiceText = "15", Id = 6 });
-        
-        return questions; 
+
+        return questions;
     }
 
     public Grade Grade(Exam toBeGradedExam)
     {
         var persistedExam = GetExam();
-        var grade = new Grade() { Exam = persistedExam}; 
+        var grade = new Grade() { Exam = persistedExam };
 
-        foreach(var question in toBeGradedExam.Questions)
+        foreach (var question in toBeGradedExam.Questions)
         {
-            var persistedQuestion = (from q in persistedExam.Questions
-                                    where q.Id == question.Id
-                                    select q).SingleOrDefault(); 
+            var persistedQuestion = persistedExam.Questions
+                .SingleOrDefault(q => q.Id == question.Id);
 
-            if(persistedQuestion != null)
+            if (persistedQuestion != null)
             {
-                foreach(var choice in question.Choices)
+                foreach (var choice in question.Choices)
                 {
-                    var persistedChoice = (from c in persistedQuestion.Choices
-                                           where c.Id == choice.Id
-                                           select c).SingleOrDefault();
+                    var persistedChoice = persistedQuestion.Choices
+                        .SingleOrDefault(c => c.Id == choice.Id);
 
-                    // sets the user choice in the actual exam fetched from database! 
-                    persistedChoice.IsSelected = true; 
-
-                    if(persistedChoice.IsAnswer)
+                    if (persistedChoice == null)
                     {
-                        grade.Score += persistedQuestion.Point; 
+                        continue;
+                    }
+
+                    // sets the user choice in the actual exam fetched from database!
+                    persistedChoice.IsSelected = true;
+
+                    if (persistedChoice.IsAnswer)
+                    {
+                        grade.Score += persistedQuestion.Point;
                     }
                 }
             }
         }
 
-        return grade; 
+        return grade;
     }
 }
