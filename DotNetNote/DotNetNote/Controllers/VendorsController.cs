6a7cefee2156646918283545f139404fe3f34015
Park YongJune <redplus@redplus.net>
2026-05-24 09:57:25 +0900

    Add Vendors controller and view file

---
+++ DotNetNote/DotNetNote/Controllers/VendorsController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Namespace
+{
+    public class VendorsController : Controller
+    {
+        // GET: VendorsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
