f1cd7965f0dab64dce8e5da094fc24f80377c7d2
Park YongJune <redplus@redplus.net>
2026-06-02 04:04:03 +0900

    Add Sessions controller and view

---
+++ DotNetNote/DotNetNote/Controllers/SessionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Controllers
+{
+    public class SessionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
