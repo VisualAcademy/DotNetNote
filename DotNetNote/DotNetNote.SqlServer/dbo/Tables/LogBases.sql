626b516b27a3c1b0b96f94e86a9b52abf5a3fc84
Park YongJune <redplus@redplus.net>
2026-06-01 22:39:10 +0900

    Add LogBases table script

---
+++ DotNetNote/DotNetNote.SqlServer/dbo/Tables/LogBases.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[LogBases]
+(
+    [Id] INT Identity(1, 1) NOT NULL PRIMARY KEY,               -- 일련번호
+    [Message] NVARCHAR(MAX) NOT NULL,                           -- 로그 메시지
+    [Timestamp] DATETIMEOFFSET(7) DEFAULT (SYSDATETIMEOFFSET()) -- 시간
+)
+GO
