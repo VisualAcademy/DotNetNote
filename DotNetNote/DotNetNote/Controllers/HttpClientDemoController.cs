e2c86c930df38c138af814725edd2797ccc17fce
Park YongJune <redplus@redplus.net>
2026-06-10 05:48:23 +0900

    Add HttpClientDemo controller and view files

---
+++ DotNetNote/DotNetNote/Controllers/HttpClientDemoController.cs
@@ -0,0 +1,38 @@
+﻿using DotNetNote.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace DotNetNote.Controllers;
+
+public class HttpClientDemoController : Controller
+{
+    private readonly HttpClient _httpClient;
+
+    public HttpClientDemoController(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var model = new HttpClientDemoResult
+        {
+            Url = "https://jsonplaceholder.typicode.com/todos/1"
+        };
+
+        try
+        {
+            string responseText = await _httpClient.GetStringAsync(model.Url);
+
+            model.ResponseText = responseText;
+            model.IsSuccess = true;
+        }
+        catch (Exception ex)
+        {
+            model.IsSuccess = false;
+            model.ErrorMessage = ex.Message;
+        }
+
+        return View(model);
+    }
+}
