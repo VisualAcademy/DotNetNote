5b62456174575937f55b23fd8a9affcb6fb727ea
Park YongJune <redplus@redplus.net>
2026-06-10 05:44:59 +0900

    Add HttpClientDemoResult class file

---
+++ DotNetNote/DotNetNote/Models/HttpClientDemoResult.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetNote.Models;
+
+public class HttpClientDemoResult
+{
+    public string Url { get; set; } = "";
+    public string ResponseText { get; set; } = "";
+    public bool IsSuccess { get; set; }
+    public string ErrorMessage { get; set; } = "";
+}
