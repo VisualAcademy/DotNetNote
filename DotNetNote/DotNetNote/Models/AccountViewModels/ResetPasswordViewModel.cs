74c8216dbeaf46082d8fb700f9e0154845df3352
Park YongJune <redplus@redplus.net>
2026-06-14 20:19:01 +0900

    Fix nullable warnings in reset password view model

--- DotNetNote/DotNetNote/Models/AccountViewModels/ResetPasswordViewModel.cs
+++ DotNetNote/DotNetNote/Models/AccountViewModels/ResetPasswordViewModel.cs
@@ -6,17 +6,17 @@ public class ResetPasswordViewModel
 {
     [Required]
     [EmailAddress]
-    public string Email { get; set; }
+    public string Email { get; set; } = string.Empty;
 
     [Required]
     [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
     [DataType(DataType.Password)]
-    public string Password { get; set; }
+    public string Password { get; set; } = string.Empty;
 
     [DataType(DataType.Password)]
     [Display(Name = "Confirm password")]
     [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-    public string ConfirmPassword { get; set; }
+    public string ConfirmPassword { get; set; } = string.Empty;
 
-    public string Code { get; set; }
+    public string Code { get; set; } = string.Empty;
 }
