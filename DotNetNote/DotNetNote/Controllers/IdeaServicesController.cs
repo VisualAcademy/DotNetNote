7b7ed19c16ac935fbbfcc2e1b51a1933a2377f19
Park YongJune <redplus@redplus.net>
2026-06-19 01:57:29 +0900

    Resolve nullable return warning in IdeaServicesController

--- DotNetNote/DotNetNote/Controllers/IdeaServicesController.cs
+++ DotNetNote/DotNetNote/Controllers/IdeaServicesController.cs
@@ -1,35 +1,37 @@
 ﻿using DotNetNote.Models.Ideas;
-using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetNote.Controllers;
 
 /// <summary>
-/// [5] Web API 컨트롤러 클래스 
+/// [5] Web API 컨트롤러 클래스
 /// </summary>
 [Route("api/[controller]")]
 public class IdeaServicesController(IIdeaRepository repository) : Controller
 {
     /// <summary>
-    /// /api/IdeaServices 
+    /// /api/IdeaServices
     /// </summary>
     [HttpGet]
     public IEnumerable<Idea> Get() =>
         // cRud
-        //return _repository.GetAll().AsEnumerable();
+        // return _repository.GetAll().AsEnumerable();
         repository.GetAll().ToList();
 
     /// <summary>
     /// /api/IdeaServices/1234
     /// </summary>
     [HttpGet("{id}")]
-    public Idea Get(int id)
+    public ActionResult<Idea> Get(int id)
     {
-        var idea = repository.GetAll().Where(m => m.Id == id).SingleOrDefault();
+        var idea = repository.GetAll().SingleOrDefault(m => m.Id == id);
+
         if (idea == null)
         {
-            Response.StatusCode = (int)HttpStatusCode.NotFound;
-            return null;
+            return NotFound();
         }
+
         return idea;
     }
 
@@ -44,13 +46,13 @@ public class IdeaServicesController(IIdeaRepository repository) : Controller
             // Crud
             var m = repository.Add(model);
 
-            Response.StatusCode = (int)HttpStatusCode.Created;
+            Response.StatusCode = StatusCodes.Status201Created;
             return Json(m);
         }
         else
         {
-            Response.StatusCode = (int)HttpStatusCode.BadRequest;
-            return Json(new { Message = "실패", ModelState = ModelState });
+            Response.StatusCode = StatusCodes.Status400BadRequest;
+            return Json(new { Message = "실패", ModelState });
         }
     }
 }
