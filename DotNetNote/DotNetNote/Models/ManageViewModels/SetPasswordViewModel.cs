cea15722907b5cff250fbe000df4da9b7ce9723a
Park YongJune <redplus@redplus.net>
2026-06-21 21:34:13 +0900

    Convert to file-scoped namespace

--- DotNetNote/DotNetNote/Models/ManageViewModels/SetPasswordViewModel.cs
+++ DotNetNote/DotNetNote/Models/ManageViewModels/SetPasswordViewModel.cs
@@ -1,18 +1,17 @@
-﻿namespace DotNetNote.Models.ManageViewModels
+﻿namespace DotNetNote.Models.ManageViewModels;
+
+public class SetPasswordViewModel
 {
-    public class SetPasswordViewModel
-    {
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-        [DataType(DataType.Password)]
-        [Display(Name = "New password")]
-        public string NewPassword { get; set; } = string.Empty;
+    [Required]
+    [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+    [DataType(DataType.Password)]
+    [Display(Name = "New password")]
+    public string NewPassword { get; set; } = string.Empty;
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; } = string.Empty;
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm new password")]
+    [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; } = string.Empty;
 
-        public string StatusMessage { get; set; } = string.Empty;
-    }
+    public string StatusMessage { get; set; } = string.Empty;
 }
