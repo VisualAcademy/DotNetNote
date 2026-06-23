63c1fdb7d6396ba8a1277fffe031d4a35b63754c
Park YongJune <redplus@redplus.net>
2026-06-23 22:22:42 +0900

    Fix nullable warnings in IndexViewModel

--- DotNetNote/DotNetNote/Models/ManageViewModels/IndexViewModel.cs
+++ DotNetNote/DotNetNote/Models/ManageViewModels/IndexViewModel.cs
@@ -4,17 +4,17 @@ namespace DotNetNote.Models.ManageViewModels;
 
 public class IndexViewModel
 {
-    public string Username { get; set; }
+    public string Username { get; set; } = string.Empty;
 
     public bool IsEmailConfirmed { get; set; }
 
     [Required]
     [EmailAddress]
-    public string Email { get; set; }
+    public string Email { get; set; } = string.Empty;
 
     [Phone]
     [Display(Name = "Phone number")]
-    public string PhoneNumber { get; set; }
+    public string? PhoneNumber { get; set; }
 
-    public string StatusMessage { get; set; }
+    public string? StatusMessage { get; set; }
 }
