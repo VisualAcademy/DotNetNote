775aefbda721c61f8a323439575d1f60cbe409d4
Park YongJune <redplus@redplus.net>
2026-07-06 04:55:57 +0900

    Fix nullable warnings by initializing required Applicant string properties

--- DotNetNote/DotNetNote/Models/Applicant.cs
+++ DotNetNote/DotNetNote/Models/Applicant.cs
@@ -1,4 +1,6 @@
-﻿namespace DotNetNote.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetNote.Models;
 
 public class Applicant
 {
@@ -7,18 +9,18 @@ public class Applicant
     [Display(Name = "이름")]
     [Required(ErrorMessage = "이름을 입력하시오.")]
     [StringLength(25, MinimumLength = 1, ErrorMessage = "이름은 1자 이상 25자 이하")]
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 
-    [Required]
-    public string FatherName { get; set; }
+    [Required(ErrorMessage = "아버지 이름을 입력하시오.")]
+    public string FatherName { get; set; } = string.Empty;
 
-    public string FatherDob { get; set; }
+    public string? FatherDob { get; set; }
 
-    [Required]
-    public string IsFatherMember { get; set; }
+    [Required(ErrorMessage = "아버지 회원 여부를 선택하시오.")]
+    public string IsFatherMember { get; set; } = string.Empty;
 
-    [Required]
-    public string MotherName { get; set; }
+    [Required(ErrorMessage = "어머니 이름을 입력하시오.")]
+    public string MotherName { get; set; } = string.Empty;
 
-    public string MotherDob { get; set; }
+    public string? MotherDob { get; set; }
 }
