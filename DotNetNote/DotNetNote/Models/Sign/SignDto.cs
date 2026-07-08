6530531e4024f7db368485545306ab0f2369da4f
Park YongJune <redplus@redplus.net>
2026-07-08 15:11:42 +0900

    Initialize SignDto string properties to fix nullable warnings

--- DotNetNote/DotNetNote/Models/Sign/SignDto.cs
+++ DotNetNote/DotNetNote/Models/Sign/SignDto.cs
@@ -7,8 +7,8 @@
     {
         public int SignId { get; set; }
 
-        public string Token { get; set; }
+        public string Token { get; set; } = string.Empty;
 
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
     }
 }
