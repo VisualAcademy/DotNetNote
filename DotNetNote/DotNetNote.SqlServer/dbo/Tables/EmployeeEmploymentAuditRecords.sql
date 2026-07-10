1180e721b607f6fb1196227f89ffb5542a0fc4ae
Park YongJune <redplus@redplus.net>
2026-07-10 15:34:00 +0900

    Add EmployeeEmploymentAuditRecords table script

---
+++ DotNetNote/DotNetNote.SqlServer/dbo/Tables/EmployeeEmploymentAuditRecords.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE dbo.EmployeeEmploymentAuditRecords
+(
+    ID BIGINT IDENTITY(1, 1) NOT NULL
+        CONSTRAINT PK_EmployeeEmploymentAuditRecords PRIMARY KEY,
+
+    AuditTrailRecordID BIGINT NOT NULL,
+    EmployeeID BIGINT NOT NULL,
+    EmployeeName NVARCHAR(300) NOT NULL,
+
+    ChangedByUserID NVARCHAR(450) NULL,
+    ChangedByUserName NVARCHAR(256) NOT NULL,
+    ChangedByEmail NVARCHAR(256) NULL,
+    ChangedByRole NVARCHAR(100) NULL,
+
+    ChangedAt DATETIMEOFFSET NOT NULL,
+    ChangeCount INT NOT NULL,
+
+    TraceIdentifier NVARCHAR(100) NULL,
+    RequestPath NVARCHAR(500) NULL,
+    RemoteIpAddress NVARCHAR(64) NULL,
+
+    Active BIT NOT NULL
+        CONSTRAINT DF_EmployeeEmploymentAuditRecords_Active
+        DEFAULT (1)
+);
