0b24ca5e2401845568984e19e9a296e16bf01f15
Park YongJune <redplus@redplus.net>
2026-07-12 21:47:39 +0900

    Initialize community camp member string properties

--- DotNetNote/DotNetNote/Models/CommunityCamp/01_CommunityCampJoinMember.cs
+++ DotNetNote/DotNetNote/Models/CommunityCamp/01_CommunityCampJoinMember.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DotNetNote.Models;
 
@@ -6,23 +7,23 @@ public class CommunityCampJoinMember
 {
     public int Id { get; set; }
 
-    [Display(Name="커뮤니티")]
-    public string CommunityName { get; set; }
+    [Display(Name = "커뮤니티")]
+    public string CommunityName { get; set; } = string.Empty;
 
-    [Display(Name="이름")]
-    [Required(ErrorMessage ="이름을 입력하시오.")]
+    [Display(Name = "이름")]
+    [Required(ErrorMessage = "이름을 입력하시오.")]
     [StringLength(25, MinimumLength = 1, ErrorMessage = "이름을 확인하세요.")]
-    public string Name { get; set; }
-    
-    [Display(Name="연락처")]
-    [Required(ErrorMessage ="연락처를 입력하시오.")]
-    public string Mobile { get; set; }
-    
-    [Required(ErrorMessage ="이메일을 입력하시오.")]
-    public string Email { get; set; }
-    
-    [Display(Name="티셔츠 사이즈")]
-    public string Size { get; set; }
-    
+    public string Name { get; set; } = string.Empty;
+
+    [Display(Name = "연락처")]
+    [Required(ErrorMessage = "연락처를 입력하시오.")]
+    public string Mobile { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "이메일을 입력하시오.")]
+    public string Email { get; set; } = string.Empty;
+
+    [Display(Name = "티셔츠 사이즈")]
+    public string Size { get; set; } = string.Empty;
+
     public DateTime CreationDate { get; set; }
 }
