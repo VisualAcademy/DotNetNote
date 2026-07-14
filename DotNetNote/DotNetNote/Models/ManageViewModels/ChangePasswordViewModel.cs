41e8cecb9399a201fc3678b1d89fe724c5171f69
Park YongJune <redplus@redplus.net>
2026-07-14 19:01:03 +0900

    Initialize change password view model string properties

--- DotNetNote/DotNetNote/Models/ManageViewModels/ChangePasswordViewModel.cs
+++ DotNetNote/DotNetNote/Models/ManageViewModels/ChangePasswordViewModel.cs
@@ -7,18 +7,23 @@ public class ChangePasswordViewModel
     [Required]
     [DataType(DataType.Password)]
     [Display(Name = "Current password")]
-    public string OldPassword { get; set; }
+    public string OldPassword { get; set; } = string.Empty;
 
     [Required]
-    [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+    [StringLength(
+        100,
+        ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.",
+        MinimumLength = 6)]
     [DataType(DataType.Password)]
     [Display(Name = "New password")]
-    public string NewPassword { get; set; }
+    public string NewPassword { get; set; } = string.Empty;
 
     [DataType(DataType.Password)]
     [Display(Name = "Confirm new password")]
-    [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
-    public string ConfirmPassword { get; set; }
+    [Compare(
+        nameof(NewPassword),
+        ErrorMessage = "The new password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; } = string.Empty;
 
-    public string StatusMessage { get; set; }
+    public string StatusMessage { get; set; } = string.Empty;
 }
