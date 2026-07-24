8da19f5b7fd214f10eb18643abfa0016651305bc
Park YongJune <redplus@redplus.net>
2026-07-25 03:32:49 +0900

    Add InvestigationsViewComponent class file

---
+++ DotNetNote/DotNetNote/ViewComponents/InvestigationsViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.ViewComponents;
+
+/// <summary>
+/// Investigations 내용을 출력하는 ViewComponent입니다.
+/// </summary>
+public class InvestigationsViewComponent : ViewComponent
+{
+    /// <summary>
+    /// Investigations 컴포넌트를 실행합니다.
+    /// </summary>
+    public IViewComponentResult Invoke()
+    {
+        string message = "Investigations ViewComponent 데모입니다.";
+
+        return View("Default", message);
+    }
+}
