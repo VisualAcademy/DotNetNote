ccdcde3b67bece4329964924db3cdb68d76c8dea
Park YongJune <redplus@redplus.net>
2026-07-25 03:27:25 +0900

    Add InvestigationsController class file

---
+++ DotNetNote/DotNetNote/Controllers/InvestigationsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Controllers;
+
+/// <summary>
+/// Investigations 페이지를 제공하는 MVC 컨트롤러입니다.
+/// </summary>
+public class InvestigationsController : Controller
+{
+    /// <summary>
+    /// /Investigations 경로를 처리합니다.
+    /// </summary>
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
