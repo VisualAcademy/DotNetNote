05f9b288c0dcb844f7fc6a8f261e18b9fe098c2f
Park YongJune <redplus@redplus.net>
2026-07-26 04:30:48 +0900

    Fix nullable warning in LoginWithRecoveryCodeViewModel
    다음은 Copilot가 만들어 준 커밋 메시지:
    
    System.ComponentModel.DataAnnotations 명시적 추가 및 기본값 수정
    
    LoginWithRecoveryCodeViewModel.cs에 System.ComponentModel.DataAnnotations 네임스페이스를 명시적으로 추가하였고, RecoveryCode 속성의 기본값을 null에서 string.Empty로 변경하였습니다.

--- DotNetNote/DotNetNote/Models/AccountViewModels/LoginWithRecoveryCodeViewModel.cs
+++ DotNetNote/DotNetNote/Models/AccountViewModels/LoginWithRecoveryCodeViewModel.cs
@@ -1,9 +1,11 @@
-﻿namespace DotNetNote.Models.AccountViewModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetNote.Models.AccountViewModels;
 
 public class LoginWithRecoveryCodeViewModel
 {
     [Required]
     [DataType(DataType.Text)]
     [Display(Name = "Recovery Code")]
-    public string RecoveryCode { get; set; }
+    public string RecoveryCode { get; set; } = string.Empty;
 }
