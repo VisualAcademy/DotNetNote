6a845020543b03ced7d448dd5c752c5c432f3bf0
Park YongJune <redplus@redplus.net>
2026-07-28 13:06:25 +0900

    Fix nullable warnings in DnnPagingHelperTagHelper

--- DotNetNote/DotNetNote/TagHelpers/DotNetNote/DnnPagingHelperTagHelper.cs
+++ DotNetNote/DotNetNote/TagHelpers/DotNetNote/DnnPagingHelperTagHelper.cs
@@ -10,24 +10,28 @@ public class DnnPagingHelperTagHelper : TagHelper
     /// <summary>
     /// 기본 리스트면 false, 검색 결과에 대한 페이징 리스트면 true
     /// </summary>
-    public bool SearchMode { get; set; } = false;
+    public bool SearchMode { get; set; }
+
     /// <summary>
     /// 검색할 필드: Name, Title, Content
     /// </summary>
-    public string SearchField { get; set; }
+    public string SearchField { get; set; } = string.Empty;
+
     /// <summary>
     /// 검색할 내용
     /// </summary>
-    public string SearchQuery { get; set; }
+    public string SearchQuery { get; set; } = string.Empty;
 
     /// <summary>
-    /// 현재 보여줄 페이지 인덱스: 0, 1, 2 : CurrentPage 
+    /// 현재 보여줄 페이지 인덱스: 0, 1, 2
     /// </summary>
-    public int PageIndex { get; set; } = 0;
+    public int PageIndex { get; set; }
+
     /// <summary>
     /// 총 페이지 개수
     /// </summary>
     public int PageCount { get; set; }
+
     /// <summary>
     /// 한 페이지에 보여줄 아티클 개수
     /// </summary>
@@ -36,20 +40,23 @@ public class DnnPagingHelperTagHelper : TagHelper
     /// <summary>
     /// 페이징 헬퍼가 실행될 URL
     /// </summary>
-    public string Url { get; set; }
+    public string Url { get; set; } = string.Empty;
 
-    private int _RecordCount;
+    private int _recordCount;
+
     /// <summary>
     /// 총 레코드 수
     /// </summary>
     public int RecordCount
     {
-        get { return _RecordCount; }
+        get => _recordCount;
         set
         {
-            _RecordCount = value;
-            PageCount = ((_RecordCount - 1) / PageSize) + 1; // 계산식
-            //PageCount = (int)Math.Ceiling(_RecordCount / (double)PageSize);
+            _recordCount = value;
+
+            PageCount = PageSize > 0
+                ? ((_recordCount - 1) / PageSize) + 1
+                : 0;
         }
     }
 
@@ -58,77 +65,96 @@ public class DnnPagingHelperTagHelper : TagHelper
     /// </summary>
     public int PagerButtonCount { get; set; } = 5;
 
-    public override void Process(TagHelperContext context, TagHelperOutput output)
+    public override void Process(
+        TagHelperContext context,
+        TagHelperOutput output)
     {
         output.TagName = "ul";
-        output.Attributes.Add("class", "pagination pagination-sm mx-auto");
+        output.Attributes.SetAttribute(
+            "class",
+            "pagination pagination-sm mx-auto");
 
-        if (PageIndex == 0)
+        if (PageIndex <= 0)
         {
             PageIndex = 1;
         }
 
-        int i = 0;
-
-        string strPage = "";
+        var page = 0;
+        var pageHtml = string.Empty;
 
         if (PageIndex > 10)
         {
+            var previousPage = ((PageIndex - 1) / 10) * 10;
+
             if (!SearchMode)
             {
-                strPage += "<li><a href=\"" + Url + "?Page=" + Convert.ToString(((PageIndex - 1) / (int)10) * 10) + "\">◀</a></li>";
+                pageHtml +=
+                    $"<li><a href=\"{Url}?Page={previousPage}\">◀</a></li>";
             }
             else
             {
-                strPage += "<li><a href=\"" + Url + "?Page=" + Convert.ToString(((PageIndex - 1) / (int)10) * 10) + "&SearchField=" + SearchField + "&SearchQuery=" + SearchQuery + "\">◀</a></li>";
+                pageHtml +=
+                    $"<li><a href=\"{Url}?Page={previousPage}" +
+                    $"&SearchField={SearchField}" +
+                    $"&SearchQuery={SearchQuery}\">◀</a></li>";
             }
         }
         else
         {
-            strPage += "<li class='disabled'><a>◁</a></li>";
+            pageHtml += "<li class=\"disabled\"><a>◁</a></li>";
         }
 
-        for (i = (((PageIndex - 1) / (int)10) * 10 + 1); i <= ((((PageIndex - 1) / (int)10) + 1) * 10); i++)
+        var firstPage = ((PageIndex - 1) / 10) * 10 + 1;
+        var lastPage = (((PageIndex - 1) / 10) + 1) * 10;
+
+        for (page = firstPage; page <= lastPage; page++)
         {
-            if (i > PageCount)
+            if (page > PageCount)
             {
-                break; // 페이지 수보다 크면 종료 
+                break;
             }
 
-            // 현재 보고 있는 페이지면 링크 제거 
-            if (i == PageIndex)
+            if (page == PageIndex)
             {
-                strPage += " <li class='active'><a href='#'>" + i.ToString() + "</a></li>";
+                pageHtml +=
+                    $"<li class=\"active\"><a href=\"#\">{page}</a></li>";
+            }
+            else if (!SearchMode)
+            {
+                pageHtml +=
+                    $"<li><a href=\"{Url}?Page={page}\">{page}</a></li>";
             }
             else
             {
-                if (!SearchMode)
-                {
-                    strPage += "<li><a href=\"" + Url + "?Page=" + i.ToString() + "\">" + i.ToString() + "</a></li>";
-                }
-                else
-                {
-                    strPage += "<li><a href=\"" + Url + "?Page=" + i.ToString() + "&SearchField=" + SearchField + "&SearchQuery=" + SearchQuery + "\">" + i.ToString() + "</a></li>";
-                }
+                pageHtml +=
+                    $"<li><a href=\"{Url}?Page={page}" +
+                    $"&SearchField={SearchField}" +
+                    $"&SearchQuery={SearchQuery}\">{page}</a></li>";
             }
         }
 
-        if (i < PageCount)
+        if (page < PageCount)
         {
+            var nextPage = ((PageIndex - 1) / 10) * 10 + 11;
+
             if (!SearchMode)
             {
-                strPage += "<li><a href=\"" + Url + "?Page=" + Convert.ToString(((PageIndex - 1) / (int)10) * 10 + 11) + "\">▶</a></li>";
+                pageHtml +=
+                    $"<li><a href=\"{Url}?Page={nextPage}\">▶</a></li>";
             }
             else
             {
-                strPage += "<li><a href=\"" + Url + "?Page=" + Convert.ToString(((PageIndex - 1) / (int)10) * 10 + 11) + "&SearchField=" + SearchField + "&SearchQuery=" + SearchQuery + "\">▶</a></li>";
+                pageHtml +=
+                    $"<li><a href=\"{Url}?Page={nextPage}" +
+                    $"&SearchField={SearchField}" +
+                    $"&SearchQuery={SearchQuery}\">▶</a></li>";
             }
         }
         else
         {
-            strPage += "<li class='disabled'><a>▷</a></li>";
+            pageHtml += "<li class=\"disabled\"><a>▷</a></li>";
         }
 
-        output.Content.AppendHtml(strPage);
+        output.Content.SetHtmlContent(pageHtml);
     }
 }
