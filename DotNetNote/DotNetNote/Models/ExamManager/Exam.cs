804311dec80ffd51031615c0781f5711bfe92606
Park YongJune <redplus@redplus.net>
2026-08-01 21:53:24 +0900

    Fix nullable warning in Exam model

--- DotNetNote/DotNetNote/Models/ExamManager/Exam.cs
+++ DotNetNote/DotNetNote/Models/ExamManager/Exam.cs
@@ -1,48 +1,45 @@
-﻿namespace DotNetNote.Models
+﻿namespace DotNetNote.Models;
+
+/// <summary>
+/// 시험 클래스
+/// </summary>
+public class Exam
 {
     /// <summary>
-    /// 시험 클래스 
+    /// 고유 번호
     /// </summary>
-    public class Exam
+    public int Id { get; set; }
+
+    /// <summary>
+    /// 시험 이름
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    private IList<Question> _questions = new List<Question>();
+
+    public IList<Question> Questions
     {
-        /// <summary>
-        /// 고유 번호
-        /// </summary>
-        public int Id { get; set; }
-
-        /// <summary>
-        /// 시험 이름 
-        /// </summary>
-        public string Name { get; set; }
-
-
-
-
-        private IList<Question> _questions = new List<Question>();
-        public IList<Question> Questions
-        {
-            get { return _questions;  }
-            set { _questions = value;  }
-        }
-
-        public void AddQuestion(IList<Question> questions)
-        {
-            foreach(var question in questions)
-            {
-                AddQuestion(question);
-            }
-        }
-
-        public void AddQuestion(Question question) => _questions.Add(question);
-
-        public double TotalPoints
-        {
-            get
-            {
-                return (from q in _questions
-                        select q.Point).Sum(); 
-            }
-        }
-       
+        get => _questions;
+        set => _questions = value ?? new List<Question>();
     }
+
+    public void AddQuestion(IList<Question> questions)
+    {
+        ArgumentNullException.ThrowIfNull(questions);
+
+        foreach (var question in questions)
+        {
+            AddQuestion(question);
+        }
+    }
+
+    public void AddQuestion(Question question)
+    {
+        ArgumentNullException.ThrowIfNull(question);
+
+        _questions.Add(question);
+    }
+
+    public double TotalPoints =>
+        _questions.Sum(question => question.Point);
 }
