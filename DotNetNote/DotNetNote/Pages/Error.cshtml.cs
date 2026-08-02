14ac18acd45a31add408803409cb7d9be128d001
Park YongJune <redplus@redplus.net>
2026-08-02 20:05:01 +0900

    Fix nullable warning in ErrorModel request ID

--- DotNetNote/DotNetNote/Pages/Error.cshtml.cs
+++ DotNetNote/DotNetNote/Pages/Error.cshtml.cs
@@ -2,18 +2,23 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace DotNetNote.Pages
+namespace DotNetNote.Pages;
+
+[ResponseCache(
+    Duration = 0,
+    Location = ResponseCacheLocation.None,
+    NoStore = true)]
+public class ErrorModel : PageModel
 {
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public class ErrorModel : PageModel
+    public string RequestId { get; private set; } = string.Empty;
+
+    public bool ShowRequestId =>
+        !string.IsNullOrEmpty(RequestId);
+
+    public void OnGet()
     {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
-        public void OnGet()
-        {
-            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-        }
+        RequestId =
+            Activity.Current?.Id
+            ?? HttpContext.TraceIdentifier;
     }
 }
