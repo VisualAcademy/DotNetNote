452600d056c1f91e6d7523ce8483944675f8a5cc
Park YongJune <redplus@redplus.net>
2026-08-20 11:41:27 +0900

    Add PublicAttachmentUploadsController.cs class file

---
+++ DotNetNote/DotNetNote/Controllers/PublicAttachmentUploadsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Controllers
+{
+    public class PublicAttachmentUploadsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
