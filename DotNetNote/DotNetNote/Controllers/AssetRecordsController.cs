308e75930d9d9794614470698a9196901ce7a2d6
Park YongJune <redplus@redplus.net>
2026-09-07 15:47:16 +0900

    Add AssetRecords controller

---
+++ DotNetNote/DotNetNote/Controllers/AssetRecordsController.cs
@@ -0,0 +1,39 @@
+﻿using DotNetNote.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetNote.Controllers;
+
+public class AssetRecordsController : Controller
+{
+    private readonly IAssetRecordRepository _repository;
+
+    public AssetRecordsController(IAssetRecordRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public IActionResult Index()
+    {
+        return View(_repository.GetAll());
+    }
+
+    public IActionResult Details(int id)
+    {
+        return View(_repository.GetById(id));
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    public IActionResult Edit(int id)
+    {
+        return View(_repository.GetById(id));
+    }
+
+    public IActionResult Delete(int id)
+    {
+        return View(_repository.GetById(id));
+    }
+}
