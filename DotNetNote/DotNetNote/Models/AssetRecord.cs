6c86be2d482b90c673351a2f9914134dbd6d9615
Park YongJune <redplus@redplus.net>
2026-09-07 15:42:32 +0900

    Add AssetRecord model

---
+++ DotNetNote/DotNetNote/Models/AssetRecord.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNetNote.Models;
+
+public class AssetRecord
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Category { get; set; } = "";
+    public string Status { get; set; } = "";
+}
