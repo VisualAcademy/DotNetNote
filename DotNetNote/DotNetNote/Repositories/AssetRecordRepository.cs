b78179bdde5f4f1bfa20bfcbab038edc18ac4514
Park YongJune <redplus@redplus.net>
2026-09-07 15:44:01 +0900

    Add AssetRecord repository

---
+++ DotNetNote/DotNetNote/Repositories/AssetRecordRepository.cs
@@ -0,0 +1,16 @@
+﻿using DotNetNote.Models;
+
+namespace DotNetNote.Repositories;
+
+public class AssetRecordRepository : IAssetRecordRepository
+{
+    private static readonly List<AssetRecord> _items =
+    [
+        new AssetRecord { Id = 1, Name = "Sample Asset", Category = "Machine", Status = "Active" }
+    ];
+
+    public IEnumerable<AssetRecord> GetAll() => _items;
+
+    public AssetRecord? GetById(int id) =>
+        _items.FirstOrDefault(x => x.Id == id);
+}
