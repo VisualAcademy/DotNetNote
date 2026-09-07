895be27aa7ac33a7a8210119b832cf1c949c566a
Park YongJune <redplus@redplus.net>
2026-09-07 15:43:31 +0900

    Add AssetRecord repository interface

---
+++ DotNetNote/DotNetNote/Repositories/IAssetRecordRepository.cs
@@ -0,0 +1,9 @@
+﻿using DotNetNote.Models;
+
+namespace DotNetNote.Repositories;
+
+public interface IAssetRecordRepository
+{
+    IEnumerable<AssetRecord> GetAll();
+    AssetRecord? GetById(int id);
+}
