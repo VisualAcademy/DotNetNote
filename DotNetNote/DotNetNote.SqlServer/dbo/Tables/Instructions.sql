24e7110ec52b960e7a544775156789df11d2445f
Park YongJune <redplus@redplus.net>
2026-09-08 14:36:51 +0900

    Add Instructions table script

---
+++ DotNetNote/DotNetNote.SqlServer/dbo/Tables/Instructions.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Instructions]
+(
+    [Id]        BIGINT         IDENTITY (1, 1) NOT NULL PRIMARY KEY,
+    [Active]    BIT            DEFAULT ((1)) NULL,
+    [CreatedAt] DATETIMEOFFSET NULL DEFAULT SYSDATETIMEOFFSET(),
+    [CreatedBy] NVARCHAR (255) NULL,
+    [Name]      NVARCHAR (MAX) NULL,
+    [Content]   NVARCHAR (MAX) NULL
+);
