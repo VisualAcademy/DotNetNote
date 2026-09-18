e3454e9ca2ac393823316eb26669b2dd1b101f21
Park YongJune <redplus@redplus.net>
2026-09-18 19:19:08 +0900

    Add InventoryCategories table script

---
+++ DotNetNote/DotNetNote.SqlServer/dbo/Tables/InventoryCategories.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[InventoryCategories]
+(
+    [Id] INT IDENTITY(1,1) NOT NULL CONSTRAINT [PK_InventoryCategories] PRIMARY KEY,
+    [Code] NVARCHAR(50) NOT NULL,
+    [Name] NVARCHAR(150) NOT NULL,
+    [Description] NVARCHAR(500) NULL,
+    [SortOrder] INT NOT NULL CONSTRAINT [DF_InventoryCategories_SortOrder] DEFAULT(0),
+    [IsActive] BIT NOT NULL CONSTRAINT [DF_InventoryCategories_IsActive] DEFAULT(1),
+    [CreatedBy] NVARCHAR(255) NULL,
+    [Created] DATETIME2(0) NULL CONSTRAINT [DF_InventoryCategories_Created] DEFAULT(SYSDATETIME()),
+    [ModifiedBy] NVARCHAR(255) NULL,
+    [Modified] DATETIME2(0) NULL
+);
+GO
+CREATE UNIQUE INDEX [UX_InventoryCategories_Code] ON [dbo].[InventoryCategories]([Code]);
+GO
